5848d319e267a3f8a208188732e84f08b9fe26ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 21:04:34 +0000

    Add package saas-common-lib-473815/0.6/saas_common_lib_473815-0.6.tar.gz

---
+++ packages/saas-common-lib-473815/saas_common_lib_473815-0.6.tar.gz/saas_common_lib_473815-0.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "saas_common_lib_473815",
+    version = "0.6",
+    author = "Srinidhi Nagarajan",
+    author_email = "Srinidhinsn@gmail.com",
+    description = ("Packaging all common functionalities"),
+    license = "BSD",
+    keywords = "Saas application",
+    url = "",
+    packages=['database', 'models', 'utils', 'config', 'entity'],
+    #packages = find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+)
