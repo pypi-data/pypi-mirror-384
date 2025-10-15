faa3fc89e511ba71666c077fd2e32de694df5f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 22:04:37 +0000

    Add package fam/4.0.1/fam-4.0.1.tar.gz

---
+++ packages/fam/fam-4.0.1.tar.gz/fam-4.0.1/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+
+setup(name='fam',
+    version='4.0.1',
+    description="Simple Python ORM for CouchDB, Firebase and Sync Gateway",
+    url="https://github.com/paulharter/fam",
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Natural Language :: English',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python :: 3.13',
+      'License :: OSI Approved :: MIT License'
+    ],
+    author='Paul Harter',
+    author_email='paul@glowinthedark.co.uk',
+    license="LICENSE",
+    install_requires=['requests', 'simplejson', 'jsonschema', 'mock', 'pytz',
+                    'firebase_admin', 'grpcio'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False
+)
