e10e6e440c858fb34c4e62b5b406b353871c7ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 15:03:54 +0000

    Add package fastlib-py/0.3.5/fastlib_py-0.3.5.tar.gz

---
+++ packages/fastlib-py/fastlib_py-0.3.5.tar.gz/fastlib_py-0.3.5/fastlib/cache/manager.py
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: MIT
+"""Cache Client manager to instantiate the appropriate cache client"""
+
+from fastlib.cache.base import Cache
+from fastlib.config.manager import ConfigManager
+
+
+async def get_cache_client() -> Cache:
+    """Initialize and return the appropriate cache client based on configuration.
+
+    Returns:
+        Cache: Redis client if Redis is enabled in config, otherwise returns page cache.
+    """
+    from fastlib.cache._redis_cache import RedisManager
+
+    config = ConfigManager.get_database_config()
+    if config.enable_redis:
+        from fastlib.cache._redis_cache import RedisCache
+
+        redis_client = await RedisManager.get_instance()
+        return RedisCache(redis_client)
+    else:
+        from fastlib.cache._disk_cache import PageCache
+
+        return PageCache()
