109a9ac4338f20e2e3e6795e9d5f05aa5c9c3eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 14:05:11 +0000

    Add package edat-utils/1.3.22/edat_utils-1.3.22-py3-none-any.whl

---
+++ packages/edat-utils/edat_utils-1.3.22-py3-none-any.whl/edat_utils/v2/__init__.py
@@ -0,0 +1,25 @@
+from edat_utils.v2.errors_handler import register_all_errors
+from edat_utils.v2.crud_base import CRUDBase
+from edat_utils.v2.graphql_service import GraphQLService
+from edat_utils.v2.models import (
+    Usuario,
+    TipoUsuario,
+    Papel,
+    UnidadeSchema,
+    CursoSchema,
+    CursoDacSchema,
+    CursoTecnicoSchema,
+)
+
+__all__ = [
+    "register_all_errors",
+    "CRUDBase",
+    "GraphQLService",
+    "Usuario",
+    "TipoUsuario",
+    "Papel",
+    "UnidadeSchema",
+    "CursoSchema",
+    "CursoDacSchema",
+    "CursoTecnicoSchema",
+]
