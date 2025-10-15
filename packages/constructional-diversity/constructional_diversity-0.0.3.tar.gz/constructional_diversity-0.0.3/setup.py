475502cd090c2ba2dfc8ec06f2177966a19c1b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 22:05:13 +0000

    Add package constructional-diversity/0.0.3/constructional_diversity-0.0.3.tar.gz

---
+++ packages/constructional-diversity/constructional_diversity-0.0.3.tar.gz/constructional_diversity-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open('README.md', encoding='utf-8') as f:
+  long_description = f.read()
+
+setup(
+  name='constructional-diversity',
+  version='0.0.3',
+  description='This is a package with which users can analyze constructional diversity, constructional complexity, and verbal diversity.', 
+  long_description=long_description, 
+  long_description_content_type = 'text/markdown', 
+  author='Haerim Hwang',
+  author_email='yayhaerim@gmail.com',
+  url='https://pypi.org/project/constructional-diversity', 
+  license='MIT', 
+  python_requires='>=3.9', 
+  install_requires=[    "more_itertools",
+    "spacy",
+    "numpy",
+    "pandas",
+    "torch"], 
+  packages=[], 
+  package_data={'':['*']}, 
+  keywords=[], 
+
+)
