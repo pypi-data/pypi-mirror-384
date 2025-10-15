8b8d0430fbb616dcfde5479015da726939b80076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 15:04:24 +0000

    Add package pydantic-cwe/0.1.1/pydantic_cwe-0.1.1-py3-none-any.whl

---
+++ packages/pydantic-cwe/pydantic_cwe-0.1.1-py3-none-any.whl/pydantic_cwe/models/catalog.py
@@ -0,0 +1,26 @@
+from typing import Iterator, List
+from pydantic import Field, BaseModel
+
+from .categories import Categories, Category
+from .weaknesses import Weaknesses, Weakness
+
+
+class Catalog(BaseModel):
+    """The root CWE catalog model"""
+    name: str = Field(..., alias="Name")
+    version: str = Field(..., alias="Version")
+    date: str = Field(..., alias="Date")
+    weaknesses: Weaknesses = Field(..., alias="Weaknesses")
+    categories: Categories = Field(..., alias="Categories")
+    # TODO: add the resto of the fields (Views, and External References);
+
+    def __iter__(self) -> Iterator[Weakness]:
+        return iter(self.weaknesses.weaknesses)
+
+    def get_ordered_weaknesses(self) -> List[Weakness]:
+        """Return an ordered list of weaknesses"""
+        return sorted(self.weaknesses.weaknesses, key=lambda weakness: weakness.id)
+
+    def get_ordered_categories(self) -> List[Category]:
+        """Return an ordered list of categories"""
+        return sorted(self.categories.categories, key=lambda category: category.id)
