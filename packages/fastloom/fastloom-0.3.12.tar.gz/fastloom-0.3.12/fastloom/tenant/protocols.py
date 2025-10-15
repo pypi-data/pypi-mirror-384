37eb08ac514a8ea7ff8e587650bd5fde4b97a5f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 14:05:12 +0000

    Add package fastloom/0.3.12/fastloom-0.3.12.tar.gz

---
+++ packages/fastloom/fastloom-0.3.12.tar.gz/fastloom-0.3.12/fastloom/tenant/protocols.py
@@ -0,0 +1,15 @@
+from typing import Protocol
+
+from pydantic import AnyHttpUrl
+
+
+class TenantNameSchema(Protocol):
+    name: str
+
+
+class TenantHostSchema(TenantNameSchema, Protocol):
+    website_url: AnyHttpUrl | list[AnyHttpUrl]
+
+
+class TenantMonitoringSchema(Protocol):
+    PROJECT_NAME: str
