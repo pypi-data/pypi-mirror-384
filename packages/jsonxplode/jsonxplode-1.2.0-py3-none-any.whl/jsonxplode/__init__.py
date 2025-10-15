bf635a13546768ead175ee6f40dacc34671a6193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 19:05:11 +0000

    Add package jsonxplode/1.2.0/jsonxplode-1.2.0-py3-none-any.whl

---
+++ packages/jsonxplode/jsonxplode-1.2.0-py3-none-any.whl/jsonxplode/__init__.py
@@ -0,0 +1,8 @@
+"""
+Flatson - Advanced JSON flattening with conflict resolution
+"""
+
+from .core import FlattenJson, flatten, to_dataframe
+
+__version__ = "1.2.0"
+__all__ = ["FlattenJson", "flatten", "to_dataframe"]
