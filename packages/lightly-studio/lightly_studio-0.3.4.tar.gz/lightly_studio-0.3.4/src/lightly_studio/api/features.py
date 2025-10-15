f78c7428a7ba74570ff8a6ba7972866adb1036fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 14:04:11 +0000

    Add package lightly-studio/0.3.4/lightly_studio-0.3.4.tar.gz

---
+++ packages/lightly-studio/lightly_studio-0.3.4.tar.gz/lightly_studio-0.3.4/src/lightly_studio/api/features.py
@@ -0,0 +1,7 @@
+"""Global list fo the active features."""
+
+# TODO(Kondrat 04/25): Pass the feature flag to app
+# https://linear.app/lightly/issue/LIG-6708/introduce-apifeatures-endpoint
+from typing import List
+
+lightly_studio_active_features: List[str] = []
---
+++ packages/lightly-studio/lightly_studio-0.3.4.tar.gz/lightly_studio-0.3.4/src/lightly_studio/dist_lightly_studio_view_app/_app/immutable/entry/start.SNvc2nrm.js
@@ -0,0 +1,1 @@
+import{l as o,e as r}from"../chunks/DQ8aZ1o-.js";export{o as load_css,r as start};
