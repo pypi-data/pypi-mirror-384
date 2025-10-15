8f9fdf0406585fdd60841bb53a471520d992e887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 22:04:48 +0000

    Add package turftopic/0.19.0/turftopic-0.19.0.tar.gz

---
+++ packages/turftopic/turftopic-0.19.0.tar.gz/turftopic-0.19.0/turftopic/__init__.py
@@ -0,0 +1,29 @@
+from turftopic._datamapplot import build_datamapplot
+from turftopic.base import ContextualModel
+from turftopic.error import NotInstalled
+from turftopic.models.cluster import BERTopic, ClusteringTopicModel, Top2Vec
+from turftopic.models.decomp import SemanticSignalSeparation
+from turftopic.models.fastopic import FASTopic
+from turftopic.models.gmm import GMM
+from turftopic.models.keynmf import KeyNMF
+from turftopic.serialization import load_model
+
+try:
+    from turftopic.models.ctm import AutoEncodingTopicModel
+except ModuleNotFoundError:
+    AutoEncodingTopicModel = NotInstalled("AutoEncodingTopicModel", "pyro-ppl")
+
+
+__all__ = [
+    "ClusteringTopicModel",
+    "SemanticSignalSeparation",
+    "GMM",
+    "KeyNMF",
+    "AutoEncodingTopicModel",
+    "ContextualModel",
+    "FASTopic",
+    "Top2Vec",
+    "BERTopic",
+    "load_model",
+    "build_datamapplot",
+]
