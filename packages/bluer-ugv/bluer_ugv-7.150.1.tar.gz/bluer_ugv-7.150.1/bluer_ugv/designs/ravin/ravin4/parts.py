610b481af03e960ae052bfe24c4b255d070be543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 22:05:00 +0000

    Add package bluer-ugv/7.150.1/bluer_ugv-7.150.1.tar.gz

---
+++ packages/bluer-ugv/bluer_ugv-7.150.1.tar.gz/bluer_ugv-7.150.1/bluer_ugv/designs/ravin/ravin4/parts.py
@@ -0,0 +1,24 @@
+dict_of_parts = {
+    "4-ch-transceiver": "",
+    "2xAA-battery-holder": "",
+    "4xAA-battery-holder": "",
+    "yellow-gearbox-dc-motor": "4 x 270 RPM",
+    "yellow-wheels": "x 4",
+    "L-1x2": "x 8",
+    "nuts-bolts-spacers": "M3: ({})".format(
+        " + ".join(
+            [
+                "10 x 6 mm bolt",
+                "8 x 25 mm bolt",
+                "2 x 5 mm spacer",
+                "20 x nut",
+            ]
+        )
+    ),
+    "ni-mh-battery": "x 4",
+    "plexiglass": "14 cm x 9.5 cm",
+    "PCB-single-14x9_5": "1 x",
+    "mt-3608": "",
+    "arduino-nano": "",
+    "tb6612": "",
+}
