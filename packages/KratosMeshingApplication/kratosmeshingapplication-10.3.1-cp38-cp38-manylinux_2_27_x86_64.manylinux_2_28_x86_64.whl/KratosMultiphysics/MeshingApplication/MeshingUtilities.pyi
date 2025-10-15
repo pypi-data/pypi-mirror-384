066324d6bd35f83a802900d31d207743882c69c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 15:04:37 +0000

    Add package KratosMeshingApplication/10.3.1/kratosmeshingapplication-10.3.1-cp38-cp38-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/KratosMeshingApplication/kratosmeshingapplication-10.3.1-cp38-cp38-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/KratosMultiphysics/MeshingApplication/MeshingUtilities.pyi
@@ -0,0 +1,6 @@
+import Kratos
+
+def BlockThresholdSizeElements(arg0: Kratos.ModelPart, arg1: Kratos.Parameters) -> None:
+    """BlockThresholdSizeElements(arg0: Kratos.ModelPart, arg1: Kratos.Parameters) -> None"""
+def ComputeElementsSize(arg0: Kratos.ModelPart) -> None:
+    """ComputeElementsSize(arg0: Kratos.ModelPart) -> None"""
