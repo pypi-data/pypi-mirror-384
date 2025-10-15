e9dbce3e94b1df6585fe25de57d663b02eb0471f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 16:05:52 +0000

    Add package critical-es-value/0.2.1/critical_es_value-0.2.1-py3-none-any.whl

---
+++ packages/critical-es-value/critical_es_value-0.2.1-py3-none-any.whl/critical_es_value/__init__.py
@@ -0,0 +1,13 @@
+from .corrtest import critical_for_correlation_test
+from .linreg import (
+    critical_for_linear_regression,
+    critical_for_linear_regression_se_coefficients,
+)
+from .ttest import (
+    critical_for_one_sample_ttest,
+    critical_for_two_sample_ttest,
+)
+from .utils import (
+    get_alpha,
+    get_bias_correction_factor_J,
+)
