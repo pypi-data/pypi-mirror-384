d2ab0313edf9dd24b49a9e09486f4c14a1867025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 21:05:16 +0000

    Add package TikExt/1.0.0/tikext-1.0.0-py3-none-any.whl

---
+++ packages/TikExt/tikext-1.0.0-py3-none-any.whl/TikExt/__init__.py
@@ -0,0 +1,11 @@
+"""
+TikExt - مكتبة شاملة للتفاعل مع TikTok API
+مطور بواسطة Mustafa - @D_B_HH
+"""
+
+from .core import TikExt
+
+__version__ = "1.0.0"
+__author__ = "Mustafa"
+__email__ = "tofey.amrie@gmail.com"
+__all__ = ['TikExt']
