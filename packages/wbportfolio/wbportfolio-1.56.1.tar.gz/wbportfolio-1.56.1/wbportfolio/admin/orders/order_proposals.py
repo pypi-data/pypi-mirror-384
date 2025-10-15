736997a7d87b3b60b511bb349e75b845eebf1eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 21:04:52 +0000

    Add package wbportfolio/1.56.1/wbportfolio-1.56.1.tar.gz

---
+++ packages/wbportfolio/wbportfolio-1.56.1.tar.gz/wbportfolio-1.56.1/wbportfolio/admin/orders/order_proposals.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from wbportfolio.models import OrderProposal
+
+from .orders import OrderTabularInline
+
+
+@admin.register(OrderProposal)
+class OrderProposalAdmin(admin.ModelAdmin):
+    search_fields = ["portfolio__name", "comment"]
+
+    list_display = ("portfolio", "rebalancing_model", "trade_date", "status")
+    autocomplete_fields = ["portfolio", "rebalancing_model"]
+    inlines = [OrderTabularInline]
