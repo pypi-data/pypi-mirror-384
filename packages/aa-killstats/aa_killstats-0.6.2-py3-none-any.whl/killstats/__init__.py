1bc68ca43f74fbe4ada8b9435ce4c312796fb174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 14:03:43 +0000

    Add package aa-killstats/0.6.2/aa_killstats-0.6.2-py3-none-any.whl

---
+++ packages/aa-killstats/aa_killstats-0.6.2-py3-none-any.whl/killstats/__init__.py
@@ -0,0 +1,11 @@
+"""Initialize the app"""
+
+__version__ = "0.6.2"
+__title__ = "Killstats"
+
+__package_name__ = "aa-killstats"
+__app_name__ = "killstats"
+__app_name_useragent__ = "AA-Killstats"
+__github_url__ = f"https://github.com/Geuthur/{__package_name__}"
+
+USER_AGENT_TEXT = f"{__app_name_useragent__} v{__version__} @ {__github_url__}"
