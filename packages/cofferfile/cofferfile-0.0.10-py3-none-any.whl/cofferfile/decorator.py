eb268bddbdaa342cd9001c3d59a4edfda3f67dba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 21:05:08 +0000

    Add package cofferfile/0.0.10/cofferfile-0.0.10-py3-none-any.whl

---
+++ packages/cofferfile/cofferfile-0.0.10-py3-none-any.whl/cofferfile/decorator.py
@@ -0,0 +1,60 @@
+# *-* encoding: utf-8 *-*
+""" Decorators
+
+From pyramid : https://github.com/Pylons/pyramid/blob/main/src/pyramid/decorator.py
+
+"""
+__author__ = 'bibi21000 aka Sébastien GALLET'
+__email__ = 'bibi21000@gmail.com'
+
+class reify:
+    """Use as a class method decorator.  It operates almost exactly like the
+    Python ``@property`` decorator, but it puts the result of the method it
+    decorates into the instance dict after the first call, effectively
+    replacing the function it decorates with an instance variable.  It is, in
+    Python parlance, a non-data descriptor.  The following is an example and
+    its usage:
+
+    .. doctest::
+
+        >>> from cofferfile.decorator import reify
+
+        >>> class Foo:
+        ...     @reify
+        ...     def jammy(self):
+        ...         print('jammy called')
+        ...         return 1
+
+        >>> f = Foo()
+        >>> v = f.jammy
+        jammy called
+        >>> print(v)
+        1
+        >>> f.jammy
+        1
+        >>> # jammy func not called the second time; it replaced itself with 1
+        >>> # Note: reassignment is possible
+        >>> f.jammy = 2
+        >>> f.jammy
+        2
+    """
+
+    def __init__(self, wrapped):
+        """
+        """
+        self.wrapped = wrapped
+        self.__name__ = wrapped.__name__
+        self.__doc__ = wrapped.__doc__
+
+    def __get__(self, inst, objtype=None):
+        """
+        """
+        if inst is None:
+            return self
+        val = self.wrapped(inst)
+        # reify is a non-data-descriptor which is leveraging the fact
+        # that it is not invoked if the equivalent attribute is defined in the
+        # object's dict, so the setattr here effectively hides this descriptor
+        # from subsequent lookups
+        setattr(inst, self.wrapped.__name__, val)
+        return val
