2ac1d1876c4e6de3be66ae62d075703f671e774b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 14:05:42 +0000

    Add package pyexiv2/2.15.5/pyexiv2-2.15.5-cp39-none-win_amd64.whl

---
+++ packages/pyexiv2/pyexiv2-2.15.5-cp39-none-win_amd64.whl/pyexiv2/__init__.py
@@ -0,0 +1,36 @@
+"""
+Read and write image metadata, including EXIF, IPTC, XMP, ICC Profile.
+"""
+
+
+from .core import *
+
+
+__version__ = '2.15.5'
+__exiv2_version__ = exiv2api.version()
+
+
+__all__ = [
+  '__version__',
+  '__exiv2_version__',
+  'lib',
+
+  # from core.py
+  'Image',
+  'ImageData',
+  'registerNs',
+  'enableBMFF',
+  'set_log_level',
+
+  # from convert.py
+  'convert_exif_to_xmp',
+  'convert_iptc_to_xmp',
+  'convert_xmp_to_exif',
+  'convert_xmp_to_iptc',
+]
+
+
+def ImageMetadata(*args, **kwargs):
+    raise NameError('ImageMetadata() is the API of py3exiv2, see https://pypi.org/project/py3exiv2 . ' +
+              'However, the library you imported is pyexiv2, see https://pypi.org/project/pyexiv2 .')
+
