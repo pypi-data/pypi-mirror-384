66444cdd5bdf6e1bf5d38d028e5ff73cd4e4c04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 16:05:48 +0000

    Add package webu/0.5.1/webu-0.5.1.tar.gz

---
+++ packages/webu/webu-0.5.1.tar.gz/webu-0.5.1/src/webu/__init__.py
@@ -0,0 +1,5 @@
+from .browsers.chrome import ChromeClientConfigType, ChromeClient
+from .llm import LLMConfigsType, LLMClient, LLMClientByConfig
+from .embed import EmbedConfigsType, EmbedClient, EmbedClientByConfig
+from .searches.google import GoogleSearchConfigType, GoogleSearcher
+from .searches.weibo import WeiboSearchConfigType, WeiboSearcher
