53ecf19010ceb9df0bdd1c3a62d327b147e8a016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 22:04:46 +0000

    Add package univocalx-dj/2.0.2a1/univocalx_dj-2.0.2a1-py3-none-any.whl

---
+++ packages/univocalx-dj/univocalx_dj-2.0.2a1-py3-none-any.whl/dj/exceptions.py
@@ -0,0 +1,42 @@
+class DatasetExist(Exception):
+    pass
+
+
+class DatasetNotFound(Exception):
+    pass
+
+
+class FileRecordNotFound(Exception):
+    pass
+
+
+class FileRecordExist(Exception):
+    pass
+
+
+class TagNotFound(Exception):
+    pass
+
+
+class FailedToGatherFiles(Exception):
+    pass
+
+
+class UnsuffiecentPermissions(Exception):
+    pass
+
+
+class TagExist(Exception):
+    pass
+
+
+class S3BucketNotFound(Exception):
+    pass
+
+
+class S3KeyNotFound(Exception):
+    pass
+
+
+class InvalidSetting(Exception):
+    pass
