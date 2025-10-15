2b20406622574e805d1d6414b3dd8b1a1960bab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 21:05:04 +0000

    Add package django-entangled/0.7/django_entangled-0.7.tar.gz

---
+++ packages/django-entangled/django_entangled-0.7.tar.gz/django_entangled-0.7/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from entangled import __version__
+
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13',
+    'Framework :: Django :: 4.2',
+    'Framework :: Django :: 5.0',
+    'Framework :: Django :: 5.1',
+    'Framework :: Django :: 5.2',
+]
+
+setup(
+    name='django-entangled',
+    version=__version__,
+    description='Edit JSON field using Django Model Form',
+    author='Jacob Rief',
+    author_email='jacob.rief@gmail.com',
+    url='https://github.com/jrief/django-entangled',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'django>=2.1',
+    ],
+    license='MIT',
+    platforms=['OS Independent'],
+    keywords=['Django Forms', 'JSON'],
+    classifiers=CLASSIFIERS,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    zip_safe=False
+)
