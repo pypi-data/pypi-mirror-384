ce6365223ff3201fec934807368278771bf63879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 17:04:38 +0000

    Add package prophecy-deploy-cli-executable/1.0.0/prophecy_deploy_cli_executable-1.0.0-py3-none-any.whl

---
+++ packages/prophecy-deploy-cli-executable/prophecy_deploy_cli_executable-1.0.0-py3-none-any.whl/prophecy_deploy_cli_executable/__init__.py
@@ -0,0 +1,6 @@
+def placeholder_function():
+    """Placeholder function - this package will be replaced with the full executable."""
+    raise NotImplementedError(
+        "This is a placeholder package. The full prophecy-deploy-cli-executable "
+        "package will be uploaded once PyPI approves the file size limit increase."
+    )
