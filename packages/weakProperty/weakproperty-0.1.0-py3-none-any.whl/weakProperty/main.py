a9f6a02749033e2f1c1a91215c457ce4409828b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 22:04:58 +0000

    Add package weakProperty/0.1.0/weakproperty-0.1.0-py3-none-any.whl

---
+++ packages/weakProperty/weakproperty-0.1.0-py3-none-any.whl/weakProperty/main.py
@@ -0,0 +1,21 @@
+import weakref
+
+def weakProperty(func):
+    attr_name = f"_weak_{func.__name__}"
+    @property
+    def wrapper(self):
+        ref = getattr(self, attr_name, None)
+        return None if ref is None else ref()
+
+    @wrapper.setter
+    def wrapper(self, value):
+        if value is None:
+            setattr(self, attr_name, None)
+        else:
+            setattr(self, attr_name, weakref.ref(value))
+
+    @wrapper.deleter
+    def wrapper(self):
+        setattr(self, attr_name, None)
+
+    return wrapper
