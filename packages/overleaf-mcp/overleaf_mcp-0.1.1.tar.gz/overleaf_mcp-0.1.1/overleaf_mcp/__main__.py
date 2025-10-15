632a62e50c1b7ad0dd68cd35425667cb006157a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 16:05:17 +0000

    Add package overleaf-mcp/0.1.1/overleaf_mcp-0.1.1.tar.gz

---
+++ packages/overleaf-mcp/overleaf_mcp-0.1.1.tar.gz/overleaf_mcp-0.1.1/overleaf_mcp/__main__.py
@@ -0,0 +1,40 @@
+from mcp.server.fastmcp import FastMCP
+from mh_overleaf import MHOverleaf
+import logging as log
+import os
+
+mcp = FastMCP("overleaf_mcp")
+project_id = os.getenv("PROJECT_ID")
+token = os.getenv("OVERLEAF_TOKEN")
+log.info(f"Project ID: {project_id}")
+log.info(f"Overleaf Token: {token}")
+
+overleaf = MHOverleaf(project_id, token)
+
+@mcp.tool()
+def list_of_files():
+    """Returns a list of files in the Overleaf project.
+
+    Returns:
+        list: A list of file paths in the Overleaf project.
+    """
+    return overleaf.list_files()
+
+@mcp.tool()
+def read_file(filename):
+    """Reads the content of a specific file in the Overleaf project.
+
+    Args:
+        filename (str): The name (path) of the file to read. The name is relative to the project root.
+        For example, "main.tex" or "sections/chapter1.tex".
+
+    Returns:
+        str: The content of the specified file.
+    """
+    return overleaf.read_file(filename)
+
+def main():
+    mcp.run(transport='stdio')
+
+if __name__ == "__main__":
+    main()
