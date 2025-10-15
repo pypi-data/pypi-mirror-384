934a903e2ee993db8768950a40dbd6d616af349b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 15:05:16 +0000

    Add package viur-shop/0.8.2/viur_shop-0.8.2-py3-none-any.whl

---
+++ packages/viur-shop/viur_shop-0.8.2-py3-none-any.whl/viur/shop/payment_providers/__init__.py
@@ -0,0 +1,21 @@
+from .abstract import PaymentProviderAbstract
+from .amazon_pay import AmazonPay
+from .invoice import Invoice
+from .paypal_plus import PayPalPlus
+from .prepayment import PrePayment, Prepayment
+
+try:
+    import unzer
+except ImportError:
+    # The unzer extra was not enabled, we don't import the related providers
+    ...
+else:
+    del unzer
+    from .unzer_abstract import UnzerAbstract
+    from .unzer_applepay import UnzerApplepay
+    from .unzer_bancontact import UnzerBancontact
+    from .unzer_card import UnzerCard
+    from .unzer_googlepay import UnzerGooglepay
+    from .unzer_ideal import UnzerIdeal
+    from .unzer_paypal import UnzerPayPal
+    from .unzer_sofort import UnzerSofort
