344a9da29513ef374636c64917a7c2980a17eed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 19:05:27 +0000

    Add package financegy/1.3/financegy-1.3-py3-none-any.whl

---
+++ packages/financegy/financegy-1.3-py3-none-any.whl/financegy/__init__.py
@@ -0,0 +1,30 @@
+"""FinanceGY - a Python library for accessing data from the Guyana Stock Exchange."""
+
+from importlib.metadata import version, PackageNotFoundError
+
+from financegy.modules.securities import(
+    get_securities,
+    get_security_by_symbol,
+    get_recent_trade,
+    get_security_recent_year,
+    get_session_trades,
+    get_security_session_trade,
+    search_securities,
+    get_trades_for_year,
+    get_historical_trades
+)
+
+from financegy.cache.cache_manager import clear_cache
+
+__all__ = [
+    "get_securities",
+    "get_security_by_symbol",
+    "get_recent_trade",
+    "get_security_recent_year",
+    "get_session_trades",
+    "get_security_session_trade",
+    "search_securities",
+    "get_trades_for_year",
+    "get_historical_trades",
+    "clear_cache"
+]
---
+++ packages/financegy/financegy-1.3-py3-none-any.whl/financegy/config.py
@@ -0,0 +1,7 @@
+__version__ = "0.1.0"
+
+BASE_URL = "https://guyanastockexchangeinc.com"
+HEADERS = {
+    "User-Agent": f"FinanceGY/{__version__} (https://github.com/xbze3/financegy)"
+}
+REQUEST_TIMEOUT = 10
