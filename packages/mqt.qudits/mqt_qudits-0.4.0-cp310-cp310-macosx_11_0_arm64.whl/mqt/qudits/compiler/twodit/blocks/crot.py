3f77415bc0e1aa249feaa62c63bad8add3b1de8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 19:04:32 +0000

    Add package mqt.qudits/0.4.0/mqt_qudits-0.4.0-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/mqt.qudits/mqt_qudits-0.4.0-cp310-cp310-macosx_11_0_arm64.whl/mqt/qudits/compiler/twodit/blocks/crot.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+# Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+# Copyright (c) 2025 Munich Quantum Software Company GmbH
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+from __future__ import annotations
+
+import typing
+
+import numpy as np
+
+from mqt.qudits.quantum_circuit import gates
+
+if typing.TYPE_CHECKING:
+    from mqt.qudits.quantum_circuit import QuantumCircuit
+    from mqt.qudits.quantum_circuit.gate import Gate
+
+CEX_SEQUENCE: list[Gate] | None = None  # list of numpy arrays
+
+
+class CRotGen:
+    def __init__(self, circuit: QuantumCircuit, indices: list[int]) -> None:
+        self.circuit: QuantumCircuit = circuit
+        self.indices: list[int] = indices
+
+    def crot_101_as_list(self, theta: float, phi: float) -> list[Gate]:
+        phi = -phi
+        # Assuming that 0 was control and 1 was target
+        index_target = self.indices[1]
+        dim_target = self.circuit.dimensions[index_target]
+
+        # Possible solution to improve of decomposition
+        single_excitation = gates.VirtRz(self.circuit, "vR", index_target, [0, -np.pi], dim_target)
+        single_excitation_back = gates.VirtRz(self.circuit, "vR", index_target, [0, np.pi], dim_target)
+        #######################
+
+        frame_back = gates.R(self.circuit, "R", index_target, [0, 1, -np.pi / 2, -phi - np.pi / 2], dim_target)
+        # on1(R(-np.pi / 2, -phi - np.pi / 2, 0, 1, d).matrix, d)
+
+        tminus = gates.Rz(self.circuit, "Rz", index_target, [0, 1, -theta / 2], dim_target)
+        # on1(ZditR(-theta / 2, 0, 1, d).matrix, d))
+
+        tplus = gates.Rz(self.circuit, "Rz", index_target, [0, 1, +theta / 2], dim_target)
+        # on1(ZditR(theta / 2, 0, 1, d).matrix, d)
+
+        frame_there = gates.R(self.circuit, "R", index_target, [0, 1, np.pi / 2, -phi - np.pi / 2], dim_target)
+        # on1(R(np.pi / 2, -phi - np.pi / 2, 0, 1, d).matrix, d)
+
+        if CEX_SEQUENCE is None:
+            cex = gates.CEx(
+                self.circuit,
+                "CEx" + str([self.circuit.dimensions[i] for i in self.indices]),
+                self.indices,
+                None,
+                [self.circuit.dimensions[i] for i in self.indices],
+                None,
+            )
+            # Cex().cex_101(d, 0)
+        else:
+            cex_s = CEX_SEQUENCE
+
+        #############
+
+        compose: list[Gate] = [frame_there]
+
+        if CEX_SEQUENCE is None:
+            compose.append(cex)
+        else:
+            compose += cex_s
+        compose.append(single_excitation)
+        compose.append(tminus)
+
+        if CEX_SEQUENCE is None:
+            compose.append(cex)
+        else:
+            compose += cex_s
+
+        compose.append(tplus)
+        ####################################
+        compose.append(single_excitation_back)
+        #####################################
+        compose.append(frame_back)
+
+        return compose
+
+    def permute_crot_101_as_list(self, i: int, theta: float, phase: float) -> list[Gate]:
+        index_ctrl = self.indices[0]
+        dim_ctrl = self.circuit.dimensions[index_ctrl]
+        index_target = self.indices[1]
+        dim_target = self.circuit.dimensions[index_target]
+
+        q0_i = int(np.floor(i / dim_target))  # finds the control block (level) of control line
+        q1_i = int(i - (dim_target * q0_i))  # finds lev_a or rotated subspace on target
+
+        rot_there = []
+        rot_back = []
+
+        rotation = self.crot_101_as_list(theta, phase)
+
+        if q0_i == 1 and q1_i == 0:
+            return rotation
+
+        if q1_i != 0:
+            permute_there_10 = gates.R(self.circuit, "R", index_target, [0, q1_i, np.pi, -np.pi / 2], dim_target)
+            # on1(R(np.pi, -np.pi / 2, 0, q1_i, d).matrix, d)
+            permute_there_11 = gates.R(self.circuit, "R", index_target, [1, q1_i + 1, -np.pi, np.pi / 2], dim_target)
+            # on1(R(-np.pi, np.pi / 2, 1, q1_i + 1, d).matrix, d)
+
+            permute_there_10_dag = gates.R(
+                self.circuit, "R", index_target, [0, q1_i, np.pi, -np.pi / 2], dim_target
+            ).dag()
+            permute_there_11_dag = gates.R(
+                self.circuit, "R", index_target, [1, q1_i + 1, -np.pi, np.pi / 2], dim_target
+            ).dag()
+
+            perm = [permute_there_10, permute_there_11]  # matmul(permute_there_10, permute_there_11)
+            perm_back = [permute_there_11_dag, permute_there_10_dag]  # perm.conj().T
+
+            rot_there += perm
+            rot_back += perm_back
+
+        if q0_i != 1:
+            permute_there_00 = gates.R(self.circuit, "R", index_ctrl, [1, q0_i, np.pi, -np.pi / 2], dim_ctrl)
+            # on0(R(np.pi, -np.pi / 2, 1, q0_i, d).matrix, d)
+            permute_back_00 = gates.R(self.circuit, "R", index_ctrl, [1, q0_i, np.pi, np.pi / 2], dim_ctrl)
+            # on0(R(np.pi, np.pi / 2, 1, q0_i, d).matrix, d)
+
+            rot_there.append(permute_there_00)
+            rot_back.insert(0, permute_back_00)
+
+        return rot_there + rotation + rot_back
---
+++ packages/mqt.qudits/mqt_qudits-0.4.0-cp310-cp310-macosx_11_0_arm64.whl/mqt/qudits/compiler/twodit/variational_twodit_compilation/opt/optimizer.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+# Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+# Copyright (c) 2025 Munich Quantum Software Company GmbH
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+from __future__ import annotations
+
+import typing
+
+import numpy as np
+from scipy.optimize import dual_annealing
+
+from mqt.qudits.compiler.twodit.variational_twodit_compilation.ansatz import cu_ansatz, ls_ansatz, ms_ansatz, reindex
+from mqt.qudits.exceptions import FidelityReachError
+
+from ..ansatz.ansatz_gen_utils import bound_1, bound_2, bound_3
+from .distance_measures import fidelity_on_unitares
+
+if typing.TYPE_CHECKING:
+    import multiprocessing
+
+    from numpy.typing import NDArray
+
+
+class Optimizer:
+    timer_var: bool = False
+    OBJ_FIDELITY: float = 1e-4
+    SINGLE_DIM_0: int = 0
+    SINGLE_DIM_1: int = 0
+    TARGET_GATE: NDArray[np.complex128] = np.array([])
+    MAX_NUM_LAYERS: int = 0  # (2 * SINGLE_DIM_0 * SINGLE_DIM_1)
+    X_SOLUTION: NDArray[np.float64] = np.array([])
+    FUN_SOLUTION: float = 0.0
+
+    @classmethod
+    def set_class_variables(
+        cls,
+        target: NDArray[np.complex128],
+        obj_fid: float = 1e-4,
+        dim_0: int = 0,
+        dim_1: int = 0,
+        layers: int = 0,
+    ) -> None:
+        cls.OBJ_FIDELITY = obj_fid
+        cls.SINGLE_DIM_0 = dim_0
+        cls.SINGLE_DIM_1 = dim_1
+        cls.TARGET_GATE = target
+        cls.MAX_NUM_LAYERS = layers if layers > 0 else (2 * dim_0 * dim_1 if dim_0 > 0 and dim_1 > 0 else 0)
+        cls.X_SOLUTION = np.array([])
+        cls.FUN_SOLUTION = 0.0
+
+    @staticmethod
+    def bounds_assigner(
+        b1: tuple[float, float], b2: tuple[float, float], b3: tuple[float, float], num_params_single: int, d: int
+    ) -> list[tuple[float, float]]:
+        assignment: list[tuple[float, float]] = [(0.0, 0.0)] * (num_params_single + 1)
+
+        for m in range(d):
+            for n in range(d):
+                if m == n:
+                    assignment[reindex(m, n, d)] = b3
+                elif m > n:
+                    assignment[reindex(m, n, d)] = b1
+                else:
+                    assignment[reindex(m, n, d)] = b2
+
+        return assignment[:-1]  # dont return last eleement which is just a global phase
+
+    @classmethod
+    def return_bounds(cls, num_layer_search: int = 1) -> list[tuple[float, float]]:
+        num_params_single_unitary_line_0 = -1 + Optimizer.SINGLE_DIM_0**2
+        num_params_single_unitary_line_1 = -1 + Optimizer.SINGLE_DIM_1**2
+
+        bounds_line_0 = Optimizer.bounds_assigner(
+            bound_1, bound_2, bound_3, num_params_single_unitary_line_0, Optimizer.SINGLE_DIM_0
+        )
+        bounds_line_1 = Optimizer.bounds_assigner(
+            bound_1, bound_2, bound_3, num_params_single_unitary_line_1, Optimizer.SINGLE_DIM_1
+        )
+
+        # Determine the length of the longest bounds list
+        # max_length = max(len(bounds_line_0), len(bounds_line_1))
+
+        # Create a new list by alternating elements from bounds_line_0 and bounds_line_1
+        bounds = []
+        num_layer = num_layer_search
+        for _i in range(num_layer + 1 + 1):
+            bounds += bounds_line_0
+            bounds += bounds_line_1
+
+        return bounds
+
+    @classmethod
+    def obj_fun_core(cls, ansatz: NDArray[np.complex128], lambdas: NDArray[np.float64]) -> float:
+        if (1 - fidelity_on_unitares(ansatz, cls.TARGET_GATE)) < cls.OBJ_FIDELITY:
+            cls.X_SOLUTION = lambdas
+            cls.FUN_SOLUTION = 1 - fidelity_on_unitares(ansatz, cls.TARGET_GATE)
+
+            raise FidelityReachError
+        if cls.timer_var:
+            raise TimeoutError
+
+        return 1 - fidelity_on_unitares(ansatz, cls.TARGET_GATE)
+
+    @classmethod
+    def objective_fnc_ms(cls, lambdas: NDArray[np.float64]) -> float:
+        ansatz = ms_ansatz(lambdas, [cls.SINGLE_DIM_0, cls.SINGLE_DIM_1])
+        return cls.obj_fun_core(ansatz, lambdas)
+
+    @classmethod
+    def objective_fnc_ls(cls, lambdas: NDArray[np.float64]) -> float:
+        ansatz = ls_ansatz(lambdas, [cls.SINGLE_DIM_0, cls.SINGLE_DIM_1])
+        return cls.obj_fun_core(ansatz, lambdas)
+
+    @classmethod
+    def objective_fnc_cu(cls, lambdas: NDArray[np.float64]) -> float:
+        ansatz = cu_ansatz(lambdas, [cls.SINGLE_DIM_0, cls.SINGLE_DIM_1])
+        return cls.obj_fun_core(ansatz, lambdas)
+
+    @classmethod
+    def solve_anneal(
+        cls,
+        bounds: list[tuple[float, float]],
+        ansatz_type: str,
+        result_queue: multiprocessing.Queue[tuple[float, NDArray[np.float64]]],
+    ) -> None:
+        try:
+            if ansatz_type == "MS":  # MS is 0
+                opt = dual_annealing(cls.objective_fnc_ms, bounds=bounds)
+            elif ansatz_type == "LS":  # LS is 1
+                opt = dual_annealing(cls.objective_fnc_ls, bounds=bounds)
+            elif ansatz_type == "CU":
+                opt = dual_annealing(cls.objective_fnc_cu, bounds=bounds)
+            else:
+                msg = f"Invalid ansatz type: {ansatz_type}"
+                raise ValueError(msg)
+
+            x = opt.x
+            fun = opt.fun
+
+            result_queue.put((fun, x))
+
+        except FidelityReachError:
+            result_queue.put((cls.FUN_SOLUTION, cls.X_SOLUTION))
+
+        except TimeoutError:
+            result_queue.put((cls.FUN_SOLUTION, cls.X_SOLUTION))
