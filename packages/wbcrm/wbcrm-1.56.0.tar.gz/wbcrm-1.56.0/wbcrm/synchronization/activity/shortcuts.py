c443b99a42d1eb5541b80e34b03c63bca427bd89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 14:05:34 +0000

    Add package wbcrm/1.56.0/wbcrm-1.56.0.tar.gz

---
+++ packages/wbcrm/wbcrm-1.56.0.tar.gz/wbcrm-1.56.0/wbcrm/synchronization/activity/shortcuts.py
@@ -0,0 +1,14 @@
+from contextlib import suppress
+
+from dynamic_preferences.exceptions import NotFoundInRegistry
+from dynamic_preferences.registries import global_preferences_registry
+from wbcore.utils.importlib import import_from_dotted_path
+
+
+def get_backend():
+    from wbcrm.synchronization.activity.controller import ActivityController
+
+    with suppress(NotFoundInRegistry):
+        if backend := global_preferences_registry.manager()["wbactivity_sync__sync_backend_calendar"]:
+            backend = import_from_dotted_path(backend)
+            return ActivityController(backend=backend)
