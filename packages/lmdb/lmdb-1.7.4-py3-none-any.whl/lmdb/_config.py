69b899700d23802a2f7fa778c437b06b472f7e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 19:05:19 +0000

    Add package lmdb/1.7.4/lmdb-1.7.4-py3-none-any.whl

---
+++ packages/lmdb/lmdb-1.7.4-py3-none-any.whl/lmdb/_config.py
@@ -0,0 +1,2 @@
+CONFIG = dict((('extra_compile_args', ['-DHAVE_PATCHED_LMDB=1', '-UNDEBUG', '-w']), ('extra_sources', ['/home/runner/work/py-lmdb/py-lmdb/build/lib/mdb.c', '/home/runner/work/py-lmdb/py-lmdb/build/lib/midl.c']), ('extra_library_dirs', []), ('extra_include_dirs', ['lib/py-lmdb', '/home/runner/work/py-lmdb/py-lmdb/build/lib', '/home/runner/work/py-lmdb/py-lmdb/lib/py-lmdb']), ('libraries', [])))
+
