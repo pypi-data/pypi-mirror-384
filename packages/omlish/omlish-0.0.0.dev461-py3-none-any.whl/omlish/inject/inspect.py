325dbe88b7d4a5dd38f207d14f0ec41a6e1e05ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 18:06:05 +0000

    Add package omlish/0.0.0.dev461/omlish-0.0.0.dev461-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev461-py3-none-any.whl/omlish/inject/inspect.py
@@ -0,0 +1,35 @@
+import typing as ta
+
+from .. import lang
+from .keys import Key
+
+
+if ta.TYPE_CHECKING:
+    from .impl import inspect as _inspect
+else:
+    _inspect = lang.proxy_import('.impl.inspect', __package__)
+
+
+T = ta.TypeVar('T')
+
+
+##
+
+
+class Kwarg(ta.NamedTuple):
+    name: str
+    key: Key
+    has_default: bool
+
+
+class KwargsTarget(ta.NamedTuple):
+    obj: ta.Any
+    kwargs: ta.Sequence[Kwarg]
+
+
+def tag(obj: T, **kwargs: ta.Any) -> T:
+    return _inspect.tag(obj, **kwargs)
+
+
+def build_kwargs_target(obj: ta.Any, **kwargs: ta.Any) -> KwargsTarget:
+    return _inspect.build_kwargs_target(obj, **kwargs)
