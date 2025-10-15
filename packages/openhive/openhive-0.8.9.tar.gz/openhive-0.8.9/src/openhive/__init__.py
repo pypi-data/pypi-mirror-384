834e9fa1fd6bcad7d4e54dedd9d6cd5f79bc02ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 21:04:52 +0000

    Add package openhive/0.8.9/openhive-0.8.9.tar.gz

---
+++ packages/openhive/openhive-0.8.9.tar.gz/openhive-0.8.9/src/openhive/__init__.py
@@ -0,0 +1,19 @@
+"""
+H.I.V.E. Protocol Core SDK for Python
+"""
+__version__ = "0.8.9"
+
+from .agent import Agent
+from .agent_config import AgentConfig
+from .types import AgentCapability, AgentMessage, AgentMessageType, AgentConfigStruct
+from .agent_signature import AgentSignature
+
+__all__ = [
+    "Agent",
+    "AgentConfig",
+    "AgentCapability",
+    "AgentMessage",
+    "AgentMessageType",
+    "AgentConfigStruct",
+    "AgentSignature",
+]
