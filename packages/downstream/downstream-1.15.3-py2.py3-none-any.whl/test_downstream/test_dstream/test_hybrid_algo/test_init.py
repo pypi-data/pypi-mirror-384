c04d1d1490927021c40895e2eed57c9af5eb9549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 21:04:54 +0000

    Add package downstream/1.15.3/downstream-1.15.3-py2.py3-none-any.whl

---
+++ packages/downstream/downstream-1.15.3-py2.py3-none-any.whl/test_downstream/test_dstream/test_hybrid_algo/test_init.py
@@ -0,0 +1,45 @@
+from downstream import dstream
+from downstream.dstream import hybrid_algo as algo_class
+
+
+def test_singleton():
+    algo = algo_class(
+        0,
+        dstream.steady_algo,
+        1,
+    )
+    assert algo._algos == [dstream.steady_algo]
+    assert algo._fenceposts == [0, 1]
+    assert algo._chunk_algo_indices == [0]
+
+
+def test_simple():
+    algo = algo_class(
+        0,
+        dstream.steady_algo,
+        1,
+        dstream.stretched_algo,
+        2,
+    )
+    assert algo._algos == [dstream.steady_algo, dstream.stretched_algo]
+    assert algo._fenceposts == [0, 1, 2]
+    assert algo._chunk_algo_indices == [0, 1]
+
+
+def test_complex():
+    algo = algo_class(
+        0,
+        dstream.steady_algo,
+        2,
+        dstream.stretched_algo,
+        3,
+        dstream.steady_algo,
+        4,
+    )
+    assert algo._algos == [
+        dstream.steady_algo,
+        dstream.stretched_algo,
+        dstream.steady_algo,
+    ]
+    assert algo._fenceposts == [0, 2, 3, 4]
+    assert algo._chunk_algo_indices == [0, 0, 1, 2]
