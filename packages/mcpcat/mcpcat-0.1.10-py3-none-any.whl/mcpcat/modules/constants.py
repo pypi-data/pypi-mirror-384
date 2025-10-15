5b6346d4b419f676ac7e3de77351368ca82327f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 19:05:19 +0000

    Add package mcpcat/0.1.10/mcpcat-0.1.10-py3-none-any.whl

---
+++ packages/mcpcat/mcpcat-0.1.10-py3-none-any.whl/mcpcat/modules/constants.py
@@ -0,0 +1,6 @@
+INACTIVITY_TIMEOUT_IN_MINUTES = 30
+LOG_PATH = "mcpcat.log"  # Default log file path
+SESSION_ID_PREFIX = "ses"
+EVENT_ID_PREFIX = "evt"
+MCPCAT_API_URL = "https://api.mcpcat.io"  # Default API URL for MCPCat events
+DEFAULT_CONTEXT_DESCRIPTION = "Describe why you are calling this tool and how it fits into your overall task"
