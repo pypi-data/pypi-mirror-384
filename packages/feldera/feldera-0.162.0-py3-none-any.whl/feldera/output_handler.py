9c163c0470193b6b704696ecaee2a9e8fccc2c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 21:05:02 +0000

    Add package feldera/0.162.0/feldera-0.162.0-py3-none-any.whl

---
+++ packages/feldera/feldera-0.162.0-py3-none-any.whl/feldera/output_handler.py
@@ -0,0 +1,63 @@
+import pandas as pd
+
+from feldera import FelderaClient
+from feldera._callback_runner import CallbackRunner
+
+
+class OutputHandler:
+    def __init__(
+        self,
+        client: FelderaClient,
+        pipeline_name: str,
+        view_name: str,
+    ):
+        """
+        Initializes the output handler, but doesn't start it.
+        To start the output handler, call the `.OutputHandler.start` method.
+        """
+
+        self.client: FelderaClient = client
+        self.pipeline_name: str = pipeline_name
+        self.view_name: str = view_name
+        self.buffer: list[pd.DataFrame] = []
+
+        # the callback that is passed to the `CallbackRunner`
+        def callback(df: pd.DataFrame, _: int):
+            if not df.empty:
+                self.buffer.append(df)
+
+        # sets up the callback runner
+        self.handler = CallbackRunner(
+            self.client, self.pipeline_name, self.view_name, callback
+        )
+
+    def start(self):
+        """
+        Starts the output handler in a separate thread
+        """
+
+        self.handler.start()
+
+    def to_pandas(self, clear_buffer: bool = True):
+        """
+        Returns the output of the pipeline as a pandas DataFrame
+
+        :param clear_buffer: Whether to clear the buffer after getting the output.
+        """
+
+        if len(self.buffer) == 0:
+            return pd.DataFrame()
+        res = pd.concat(self.buffer, ignore_index=True)
+        if clear_buffer:
+            self.buffer.clear()
+
+        return res
+
+    def to_dict(self, clear_buffer: bool = True):
+        """
+        Returns the output of the pipeline as a list of python dictionaries
+
+        :param clear_buffer: Whether to clear the buffer after getting the output.
+        """
+
+        return self.to_pandas(clear_buffer).to_dict(orient="records")
