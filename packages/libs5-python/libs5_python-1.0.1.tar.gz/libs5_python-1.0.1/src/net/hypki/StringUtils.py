0474b71b2f3c1864a1f30201f05ecbcb87d8fdff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 14:05:03 +0000

    Add package libs5-python/1.0.1/libs5_python-1.0.1.tar.gz

---
+++ packages/libs5-python/libs5_python-1.0.1.tar.gz/libs5_python-1.0.1/src/net/hypki/StringUtils.py
@@ -0,0 +1,20 @@
+'''
+Created on Oct 14, 2025
+
+@author: ahypki
+'''
+
+class StringUtils(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        pass
+    
+    def notEmpty(s):  # @NoSelf
+        return len(s.strip()) > 0 if s is not None else False 
