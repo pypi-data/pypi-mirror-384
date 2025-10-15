f1b6ba7cbfdb38e22825e0e5c6b83a010e669c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 19:04:32 +0000

    Add package mqt.qudits/0.4.0/mqt_qudits-0.4.0-cp313-cp313t-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/mqt.qudits/mqt_qudits-0.4.0-cp313-cp313t-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl/mqt/qudits/quantum_circuit/components/extensions/matrix_factory.py
@@ -0,0 +1,212 @@
+# Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+# Copyright (c) 2025 Munich Quantum Software Company GmbH
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+from __future__ import annotations
+
+import itertools
+import operator
+import typing
+from functools import reduce
+
+import numpy as np
+
+if typing.TYPE_CHECKING:
+    from numpy.typing import NDArray
+
+    from mqt.qudits.quantum_circuit.components.extensions.controls import ControlData
+    from mqt.qudits.quantum_circuit.gate import Gate
+
+
+class MatrixFactory:
+    def __init__(self, gate: Gate, identities_flag: int) -> None:
+        self.gate: Gate = gate
+        self.ids: int = identities_flag
+
+    def generate_matrix(self) -> NDArray[np.complex128]:
+        matrix = self.gate.__array__()
+        if self.gate.dagger:
+            matrix = matrix.conj().T
+
+        control_info = typing.cast("ControlData | None", self.gate.control_info["controls"])
+        lines = self.gate.reference_lines.copy()
+        circuit = self.gate.parent_circuit
+        ref_slice = list(range(min(lines), max(lines) + 1))
+        dimensions_slice = circuit.dimensions[min(lines) : max(lines) + 1]
+
+        if control_info is not None:
+            controls: list[int] = control_info.indices
+            ctrl_levs: list[int] = control_info.ctrl_states
+            # preferably only CONTROLLED-One qudit gates to be made as multi-controlled, it is still a low level
+            # control library
+
+            matrix = MatrixFactory.apply_identities_and_controls(
+                matrix, self.gate.target_qudits, dimensions_slice, ref_slice, controls, ctrl_levs
+            )
+        elif self.ids > 0:
+            matrix = MatrixFactory.apply_identities_and_controls(
+                matrix, self.gate.target_qudits, dimensions_slice, ref_slice
+            )
+
+        if self.ids >= 2:
+            matrix = MatrixFactory.wrap_in_identities(matrix, lines, circuit.dimensions)
+
+        return matrix
+
+    @classmethod
+    def apply_identities_and_controls(
+        cls,
+        matrix: NDArray[np.complex128],
+        qudits_applied: int | list[int],
+        dimensions: int | list[int],
+        ref_lines: list[int],
+        controls: list[int] | None = None,
+        controls_levels: list[int] | None = None,
+    ) -> NDArray[np.complex128]:
+        # dimensions = list(reversed(dimensions))
+        # Convert qudits_applied and dimensions to lists if they are not already
+        qudits_applied = [qudits_applied] if isinstance(qudits_applied, int) else qudits_applied
+        qudits_applied = qudits_applied.copy()
+        qudits_applied.sort()
+        slide_indices_qudits_a = [q - min(ref_lines) for q in qudits_applied]
+
+        dimensions = [dimensions] if isinstance(dimensions, int) else dimensions
+        if len(dimensions) == 0:
+            msg = "Dimensions cannot be an empty list"
+            raise ValueError(msg)
+        if len(qudits_applied) == len(ref_lines) and controls is None:
+            return matrix
+
+        if controls is not None:
+            slide_controls = [q - min(ref_lines) for q in controls]
+            rest_of_indices = set(ref_lines) - set(qudits_applied) - set(controls)
+        else:
+            rest_of_indices = set(ref_lines) - set(qudits_applied)
+        slide_indices_rest = [q - min(ref_lines) for q in rest_of_indices]
+
+        single_site_logics = (
+            [list(range(dimensions[qudits_applied[0]]))]
+            if len(qudits_applied) == 1
+            else [list(range(d)) for d in operator.itemgetter(*slide_indices_qudits_a)(dimensions)]
+        )
+
+        og_states_space = [list(element) for element in itertools.product(*single_site_logics)]
+
+        og_state_to_index = {tuple(state): i for i, state in enumerate(og_states_space)}
+
+        global_single_site_logics = [list(range(d)) for d in dimensions]
+        global_states_space = [list(element) for element in itertools.product(*global_single_site_logics)]
+        global_index_to_state = dict(enumerate(global_states_space))
+
+        result = np.identity(reduce(operator.mul, dimensions, 1), dtype=np.complex128)
+
+        for r in range(result.shape[0]):
+            for c in range(result.shape[1]):
+                if controls is not None:
+                    extract_r = operator.itemgetter(*slide_controls)(global_index_to_state[r])
+                    extract_c = operator.itemgetter(*slide_controls)(global_index_to_state[c])
+                    if isinstance(extract_r, int):
+                        extract_r = [extract_r]
+                        extract_c = [extract_c]
+                    if (
+                        list(extract_r) == controls_levels
+                        and extract_r == extract_c
+                        and (
+                            not rest_of_indices
+                            or operator.itemgetter(*slide_indices_rest)(global_index_to_state[r])
+                            == operator.itemgetter(*slide_indices_rest)(global_index_to_state[c])
+                        )
+                    ):
+                        og_row_key = operator.itemgetter(*slide_indices_qudits_a)(global_index_to_state[r])
+                        og_col_key = operator.itemgetter(*slide_indices_qudits_a)(global_index_to_state[c])
+                        if isinstance(og_row_key, int):
+                            og_row_key = (og_row_key,)
+                        if isinstance(og_col_key, int):
+                            og_col_key = (og_col_key,)
+                        matrix_row = og_state_to_index[tuple(og_row_key)]
+                        matrix_col = og_state_to_index[tuple(og_col_key)]
+                        value = matrix[matrix_row, matrix_col]
+                        result[r, c] = value
+
+                elif not rest_of_indices or operator.itemgetter(*slide_indices_rest)(
+                    global_index_to_state[r]
+                ) == operator.itemgetter(*slide_indices_rest)(global_index_to_state[c]):
+                    og_row_key = operator.itemgetter(*slide_indices_qudits_a)(global_index_to_state[r])
+                    og_col_key = operator.itemgetter(*slide_indices_qudits_a)(global_index_to_state[c])
+                    if isinstance(og_row_key, int):
+                        og_row_key = (og_row_key,)
+                    if isinstance(og_col_key, int):
+                        og_col_key = (og_col_key,)
+                    matrix_row = og_state_to_index[tuple(og_row_key)]
+                    matrix_col = og_state_to_index[tuple(og_col_key)]
+                    value = matrix[matrix_row, matrix_col]
+                    result[r, c] = value
+
+        return result
+
+    @classmethod
+    def wrap_in_identities(
+        cls, matrix: NDArray[np.complex128], indices: list[int], sizes: list[int]
+    ) -> NDArray[np.complex128]:
+        indices.sort()
+        if any(index >= len(sizes) for index in indices):
+            msg = "Index out of range"
+            raise ValueError(msg)
+
+        i = 0
+        result = np.identity(sizes[i], dtype=np.complex128)
+        while i < len(sizes):
+            if i == indices[0]:
+                result = matrix if i == 0 else np.kron(result, matrix).astype(np.complex128)
+            elif (i < indices[0] and i != 0) or i > indices[-1]:
+                result = np.kron(result, np.identity(sizes[i], dtype=np.complex128)).astype(np.complex128)
+
+            i += 1
+
+        return result
+
+
+def from_dirac_to_basis(vec: list[int], d: list[int] | int) -> NDArray[np.complex128]:
+    # |00> -> [1,0,...,0] -> len() == other_size**2
+    if isinstance(d, int):
+        d = [d] * len(vec)
+
+    basis_vecs = []
+    for i, basis in enumerate(vec):
+        temp = [0] * d[i]
+        temp[basis] = 1
+        basis_vecs.append(temp)
+
+    ret = np.array(basis_vecs[0], dtype=np.complex128)
+    for e_i in range(1, len(basis_vecs)):
+        ret = np.kron(ret, np.array(basis_vecs[e_i])).astype(np.complex128)
+
+    return ret
+
+
+def calculate_q0_q1(lev: int, dim: int) -> tuple[int, int]:
+    q1 = lev % dim
+    q0 = (lev - q1) // dim
+
+    return q0, q1
+
+
+def insert_at(
+    big_arr: NDArray[np.complex128], pos: tuple[int, int], to_insert_arr: NDArray[np.complex128]
+) -> NDArray[np.complex128]:
+    """Quite a forceful way of embedding a parameters into big_arr."""
+    x1 = pos[0]
+    y1 = pos[1]
+    x2 = x1 + to_insert_arr.shape[0]
+    y2 = y1 + to_insert_arr.shape[1]
+
+    assert x2 <= big_arr.shape[0], "the position will make the small parameters exceed the boundaries at x"
+    assert y2 <= big_arr.shape[1], "the position will make the small parameters exceed the boundaries at y"
+
+    big_arr[x1:x2, y1:y2] = to_insert_arr
+
+    return big_arr
---
+++ packages/mqt.qudits/mqt_qudits-0.4.0-cp313-cp313t-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl/mqt/qudits/simulation/backends/tnsim.py
@@ -0,0 +1,128 @@
+# Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+# Copyright (c) 2025 Munich Quantum Software Company GmbH
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+from __future__ import annotations
+
+import operator
+from functools import reduce
+from typing import TYPE_CHECKING
+
+import numpy as np
+import tensornetwork as tn  # type: ignore[import-not-found]
+from typing_extensions import Unpack
+
+from ...quantum_circuit.components.extensions.gate_types import GateTypes
+from ..jobs import Job, JobResult
+from .backendv2 import Backend
+from .stochastic_sim import stochastic_simulation
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+    from numpy.typing import NDArray
+
+    from ...quantum_circuit import QuantumCircuit
+    from ...quantum_circuit.gate import Gate
+    from .. import MQTQuditProvider
+    from ..noise_tools import NoiseModel
+
+
+class TNSim(Backend):
+    def __init__(
+        self,
+        provider: MQTQuditProvider,
+        name: str | None = None,
+        description: str | None = None,
+        **fields: Unpack[Backend.DefaultOptions],
+    ) -> None:
+        super().__init__(provider, name=name, description=description, **fields)
+
+    def __noise_model(self) -> NoiseModel | None:
+        return self.noise_model
+
+    def run(self, circuit: QuantumCircuit, **options: Unpack[Backend.DefaultOptions]) -> Job:
+        job = Job(self)
+        self._options.update(options)
+        self.noise_model: NoiseModel | None = self._options.get("noise_model", None)
+        self.shots = self._options.get("shots", 50)
+        self.memory = self._options.get("memory", False)
+        self.full_state_memory = self._options.get("full_state_memory", False)
+        self.file_path = self._options.get("file_path", None)
+        self.file_name = self._options.get("file_name", None)
+
+        if self.noise_model is not None:
+            assert self.shots >= 50, "Number of shots should be above 50"
+            job.set_result(JobResult(state_vector=self.execute(circuit), counts=stochastic_simulation(self, circuit)))  # type: ignore [arg-type]
+        else:
+            job.set_result(JobResult(state_vector=self.execute(circuit), counts=[]))
+
+        return job
+
+    def execute(self, circuit: QuantumCircuit, noise_model: NoiseModel | None = None) -> NDArray[np.complex128]:  # noqa: ARG002
+        self.system_sizes = circuit.dimensions
+        self.circ_operations = circuit.instructions
+
+        result = self.__contract_circuit(self.system_sizes, self.circ_operations)
+
+        result = np.transpose(result.tensor, list(range(len(self.system_sizes))))
+
+        state_size = reduce(operator.mul, self.system_sizes, 1)
+        return np.asarray(result.reshape(1, state_size), dtype=np.complex128)
+
+    @staticmethod
+    def __apply_gate(qudit_edges: tn.Edge, gate: NDArray[np.complex128], operating_qudits: list[int]) -> None:
+        op = tn.Node(gate)
+        for i, bit in enumerate(operating_qudits):
+            tn.connect(qudit_edges[bit], op[i])
+            qudit_edges[bit] = op[i + len(operating_qudits)]
+
+    def __contract_circuit(
+        self, system_sizes: list[int], operations: Sequence[Gate]
+    ) -> tn.network_components.AbstractNode:
+        all_nodes: Sequence[tn.network_components.AbstractNode] = []
+
+        with tn.NodeCollection(all_nodes):
+            state_nodes = []
+            for s in system_sizes:
+                z = [0] * s
+                z[0] = 1
+                state_nodes.append(tn.Node(np.array(z, dtype=np.complex128)))
+
+            qudits_legs = [node[0] for node in state_nodes]
+
+            for op in operations:
+                op_matrix = op.to_matrix(identities=1)
+                lines = op.reference_lines
+
+                if op.gate_type == GateTypes.SINGLE:
+                    op_matrix = op_matrix.T
+                    # op_matrix = op_matrix.reshape((system_sizes[lines[0]], system_sizes[lines[0]]))
+
+                elif op.gate_type == GateTypes.TWO and not op.is_long_range:
+                    op_matrix = op_matrix.T
+                    lines = lines.copy()
+                    lines.sort()
+
+                    op_matrix = op_matrix.reshape((
+                        system_sizes[lines[0]],
+                        system_sizes[lines[1]],
+                        system_sizes[lines[0]],
+                        system_sizes[lines[1]],
+                    ))
+
+                elif op.is_long_range or op.gate_type == GateTypes.MULTI:
+                    op_matrix = op_matrix.T
+                    minimum_line, maximum_line = min(lines), max(lines)
+                    interested_lines = list(range(minimum_line, maximum_line + 1))
+                    inputs_outputs_legs = [system_sizes[i] for i in interested_lines] * 2
+                    op_matrix = op_matrix.reshape(tuple(inputs_outputs_legs))
+                    lines = interested_lines
+
+                self.__apply_gate(qudits_legs, op_matrix, lines)
+
+        return tn.contractors.auto(all_nodes, output_edge_order=qudits_legs)
