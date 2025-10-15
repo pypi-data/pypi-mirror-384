c782ff66be23f9a6a2ea107ee2cb4a3bf105f6a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 17:04:33 +0000

    Add package faststream/0.6.1/faststream-0.6.1-py3-none-any.whl

---
+++ packages/faststream/faststream-0.6.1-py3-none-any.whl/faststream/confluent/prometheus/middleware.py
@@ -0,0 +1,35 @@
+from collections.abc import Sequence
+from typing import TYPE_CHECKING
+
+from confluent_kafka import Message
+
+from faststream._internal.constants import EMPTY
+from faststream.confluent.prometheus.provider import settings_provider_factory
+from faststream.confluent.response import KafkaPublishCommand
+from faststream.prometheus.middleware import PrometheusMiddleware
+
+if TYPE_CHECKING:
+    from prometheus_client import CollectorRegistry
+
+
+class KafkaPrometheusMiddleware(
+    PrometheusMiddleware[
+        KafkaPublishCommand,
+        Message | Sequence[Message],
+    ],
+):
+    def __init__(
+        self,
+        *,
+        registry: "CollectorRegistry",
+        app_name: str = EMPTY,
+        metrics_prefix: str = "faststream",
+        received_messages_size_buckets: Sequence[float] | None = None,
+    ) -> None:
+        super().__init__(
+            settings_provider_factory=settings_provider_factory,  # type: ignore[arg-type]
+            registry=registry,
+            app_name=app_name,
+            metrics_prefix=metrics_prefix,
+            received_messages_size_buckets=received_messages_size_buckets,
+        )
