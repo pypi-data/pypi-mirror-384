4b6394eb57cfa6dc8ab20e72789a007eaff5dc32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 17:04:36 +0000

    Add package computemate/0.1.76/computemate-0.1.76.tar.gz

---
+++ packages/computemate/computemate-0.1.76.tar.gz/computemate-0.1.76/computemate/mcp_configurations.py
@@ -0,0 +1,8 @@
+{
+    # Check configuration format at https://gofastmcp.com/clients/client#configuration-format
+    # For example:
+    #"github": {
+    #    "url": "https://api.githubcopilot.com/mcp",
+    #    "headers": {"Authorization": os.getenv("GITHUB_TOKEN")},
+    #},
+}
