08bf85e637742e89521fb05aca6b06be725e8c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 17:04:36 +0000

    Add package lenskit/2025.3.2/lenskit-2025.3.2-cp311-abi3-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/lenskit/lenskit-2025.3.2-cp311-abi3-manylinux_2_17_i686.manylinux2014_i686.whl/lenskit/_accel/__init__.pyi
@@ -0,0 +1,31 @@
+"""
+Rust acceleration code.
+"""
+
+from __future__ import annotations
+
+from lenskit.data.types import NPMatrix, NPVector
+from lenskit.funksvd import FunkSVDTrainingData, FunkSVDTrainingParams
+
+from . import als, data
+
+__all__ = [
+    "als",
+    "data",
+    "init_accel_pool",
+    "thread_count",
+    "FunkSVDTrainer",
+]
+
+def init_accel_pool(n_threads: int): ...
+def thread_count() -> int: ...
+
+class FunkSVDTrainer:
+    def __init__(
+        self,
+        config: FunkSVDTrainingParams,
+        data: FunkSVDTrainingData,
+        user_features: NPMatrix,
+        item_features: NPMatrix,
+    ): ...
+    def feature_epoch(self, feature: int, estimates: NPVector, trail: float) -> float: ...
