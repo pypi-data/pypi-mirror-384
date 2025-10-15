101e8ef4fef564cf6d22b1e84bae0d6e4f762f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 19:05:14 +0000

    Add package inkhinge/1.0.5/inkhinge-1.0.5.tar.gz

---
+++ packages/inkhinge/inkhinge-1.0.5.tar.gz/inkhinge-1.0.5/inkhinge/__init__.py
@@ -0,0 +1,7 @@
+"""inkhinge - 一个专为光谱分析设计的下一代深度学习工具集"""
+
+__version__ = '1.0.5'
+__author__ = 'Fenglong Li,Siyang Li,Kehan Li,Zhiqing Guo'
+__email__ = '15340666394@163.com'
+__license__ = 'MIT'
+__url__ = 'https://github.com/1412universe/inkhinge'
