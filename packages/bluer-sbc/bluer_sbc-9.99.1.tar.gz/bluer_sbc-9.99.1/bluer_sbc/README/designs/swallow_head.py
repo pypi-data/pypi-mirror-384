64ba0e11ab40d8184e76f69d48b3063176d5771b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 17:05:00 +0000

    Add package bluer-sbc/9.99.1/bluer_sbc-9.99.1.tar.gz

---
+++ packages/bluer-sbc/bluer_sbc-9.99.1.tar.gz/bluer_sbc-9.99.1/bluer_sbc/README/designs/swallow_head.py
@@ -0,0 +1,21 @@
+from bluer_objects import README
+from bluer_objects.README.items import ImageItems
+
+from bluer_sbc.README.designs.consts import assets2
+
+
+image_template = assets2 + "swallow/design/head-v1/{}?raw=true"
+
+marquee = README.Items(
+    [
+        {
+            "name": "swallow head",
+            "marquee": image_template.format("01.jpg"),
+            "url": "./bluer_sbc/docs/swallow-head.md",
+        }
+    ]
+)
+
+items = ImageItems(
+    {image_template.format(f"{index+1:02}.jpg"): "" for index in range(6)}
+)
