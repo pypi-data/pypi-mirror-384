76649a045789f4cb95b2ac85c14c2364348f9ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 17:04:40 +0000

    Add package pyshareit/1.8.0/pyshareit-1.8.0.tar.gz

---
+++ packages/pyshareit/pyshareit-1.8.0.tar.gz/pyshareit-1.8.0/setup.py
@@ -0,0 +1,6 @@
+# This is a legacy file for compatibility with older tools.
+# The main configuration is now in pyproject.toml.
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
