a409642655411f5fd6bbf5dfdce128b427825e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 14:04:51 +0000

    Add package llm-repeator-redis/0.1.10/llm_repeator_redis-0.1.10-py3-none-any.whl

---
+++ packages/llm-repeator-redis/llm_repeator_redis-0.1.10-py3-none-any.whl/llm_repeator_redis/demo_push_stream_huaweicloud.py
@@ -0,0 +1,17 @@
+from langchain_core.messages import BaseMessage, SystemMessage, HumanMessage
+
+from llm_repeator_redis import LLMRepeaterRedis
+
+if __name__ == '__main__':
+    llm_repeator_redis = LLMRepeaterRedis(llm_json_path="config/llm_resources.json",
+                                          config_path="config/config.ini")
+
+    model: str = "huawei_deepseek_r1_32k_demo"
+    # model: str = "huawei_deepseek_v3_32k_demo"
+
+    messages: [BaseMessage] = [SystemMessage("你是一个好助手"), HumanMessage("你好")]
+
+    for chunk in llm_repeator_redis.request_stream(messages=messages, model=model):
+        print(chunk, end='')
+
+    print('done')
