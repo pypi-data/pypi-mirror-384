bb4597661c8117f262d48b6bf1c4653d15e52246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 18:05:55 +0000

    Add package rocketlogger/2.1.3/rocketlogger-2.1.3.tar.gz

---
+++ packages/rocketlogger/rocketlogger-2.1.3.tar.gz/rocketlogger-2.1.3/setup.py
@@ -0,0 +1,85 @@
+"""
+RocketLogger Python Library.
+
+Copyright (c) 2016-2020, ETH Zurich, Computer Engineering Group
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="rocketlogger",
+    version="2.1.3",
+    author="ETH Zurich, Computer Engineering Group",
+    description="RocketLogger Python Support",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="BSD 3-Clause",
+    url="https://github.com/ETHZ-TEC/RocketLogger#readme",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="rocketlogger, data analysis, calibration",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    python_requires=">=3.6, <4",
+    install_requires=[
+        "numpy>=1.13,<2.4",
+    ],
+    extras_require={
+        "dataframe": ["pandas"],
+        "plot": ["matplotlib"],
+        "dev": ["black", "sphinx"],
+        "test": ["pytest", "pytest-cov", "tox"],
+    },
+    project_urls={
+        "Documentation": "https://github.com/ETHZ-TEC/RocketLogger/wiki/python",
+        "Source": "https://github.com/ETHZ-TEC/RocketLogger",
+        "Tracker": "https://github.com/ETHZ-TEC/RocketLogger/issues",
+    },
+)
