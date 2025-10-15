4a76b8e53d193f7a2dfd6b7a85efeda8315994dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 16:04:54 +0000

    Add package hiredis/3.3.0/hiredis-3.3.0.tar.gz

---
+++ packages/hiredis/hiredis-3.3.0.tar.gz/hiredis-3.3.0/src/reader.h
@@ -0,0 +1,34 @@
+#ifndef __READER_H
+#define __READER_H
+
+#include "hiredis.h"
+
+typedef struct {
+    PyObject_HEAD
+    redisReader *reader;
+    char *encoding;
+    char *errors;
+    int shouldDecode;
+    PyObject *protocolErrorClass;
+    PyObject *replyErrorClass;
+    PyObject *notEnoughDataObject;
+
+    /* Stores error object in between incomplete calls to #gets, in order to
+     * only set the error once a full reply has been read. Otherwise, the
+     * reader could get in an inconsistent state. */
+    struct {
+        PyObject *ptype;
+        PyObject *pvalue;
+        PyObject *ptraceback;
+    } error;
+} hiredis_ReaderObject;
+
+typedef struct {
+    PyListObject list;
+} PushNotificationObject;
+
+extern PyTypeObject hiredis_ReaderType;
+extern PyTypeObject PushNotificationType;
+extern redisReplyObjectFunctions hiredis_ObjectFunctions;
+
+#endif
