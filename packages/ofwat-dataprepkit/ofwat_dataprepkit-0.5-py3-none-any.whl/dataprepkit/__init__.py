dd306db66ced73e056b96106ebbb75d3c021524b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 17:05:12 +0000

    Add package ofwat-dataprepkit/0.5/ofwat_dataprepkit-0.5-py3-none-any.whl

---
+++ packages/ofwat-dataprepkit/ofwat_dataprepkit-0.5-py3-none-any.whl/dataprepkit/__init__.py
@@ -0,0 +1,6 @@
+"""Main entry point into the dataprepkit package
+"""
+from dataprepkit.helpers.connectors.warehouse import (
+    get_fabric_warehouse_engine,
+    validate_fabric_warehouse_engine
+)
