2668240b8b551ca9421ec1e9c7039808b7ca4958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 19:04:34 +0000

    Add package red-tidegear/1.20.0/red_tidegear-1.20.0-py3-none-any.whl

---
+++ packages/red-tidegear/red_tidegear-1.20.0-py3-none-any.whl/tidegear/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2025 cswimr <copyright@csw.im>
+# SPDX-License-Identifier: MPL-2.0
+
+from .cog import Cog
+from .version import __version__, meta, version
+
+__all__ = [
+    "__version__",
+    "Cog",
+    "meta",
+    "version",
+]
