aa9926087077c7a7fc996b4a2dfa2f16eba370df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 16:04:58 +0000

    Add package alita-sdk/0.3.370/alita_sdk-0.3.370-py3-none-any.whl

---
+++ packages/alita-sdk/alita_sdk-0.3.370-py3-none-any.whl/alita_sdk/runtime/tools/function.py
@@ -0,0 +1,67 @@
+import logging
+from json import dumps
+
+from langchain_core.callbacks import dispatch_custom_event
+from langchain_core.messages import ToolCall
+from langchain_core.runnables import RunnableConfig
+from langchain_core.tools import BaseTool, ToolException
+from typing import Any, Optional, Union, Annotated
+from langchain_core.utils.function_calling import convert_to_openai_tool
+from pydantic import ValidationError
+from ..langchain.utils import propagate_the_input_mapping
+
+logger = logging.getLogger(__name__)
+
+
+class FunctionTool(BaseTool):
+    name: str = 'FunctionalTool'
+    description: str = 'This is direct call node for tools'
+    tool: BaseTool = None
+    return_type: str = "str"
+    input_variables: Optional[list[str]] = None
+    input_mapping: Optional[dict[str, dict]] = None
+    output_variables: Optional[list[str]] = None
+
+    def invoke(
+            self,
+            state: Union[str, dict, ToolCall],
+            config: Optional[RunnableConfig] = None,
+            **kwargs: Any,
+    ) -> Any:
+        params = convert_to_openai_tool(self.tool).get(
+            'function', {'parameters': {}}).get(
+            'parameters', {'properties': {}}).get('properties', {})
+        func_args = propagate_the_input_mapping(input_mapping=self.input_mapping, input_variables=self.input_variables,
+                                                state=state)
+        try:
+            tool_result = self.tool.invoke(func_args, config, **kwargs)
+            dispatch_custom_event(
+                "on_function_tool_node", {
+                    "input_mapping": self.input_mapping,
+                    "input_variables": self.input_variables,
+                    "state": state,
+                    "tool_result": tool_result,
+                }, config=config
+            )
+            logger.info(f"ToolNode response: {tool_result}")
+            if not self.output_variables:
+                return {"messages": [{"role": "assistant", "content": dumps(tool_result)}]}
+            else:
+                if self.output_variables[0] == "messages":
+                    return {
+                        "messages": [{
+                            "role": "assistant",
+                            "content": dumps(tool_result) if not isinstance(tool_result, ToolException) else str(
+                                tool_result)
+                        }]
+                    }
+                else:
+                    return { self.output_variables[0]: tool_result }
+        except ValidationError:
+            return {"messages": [
+                {"role": "assistant", "content": f"""Tool input to the {self.tool.name} with value {func_args} raised ValidationError. 
+        \n\nTool schema is {dumps(params)} \n\nand the input to LLM was 
+        {func_args}"""}]}
+
+    def _run(self, *args, **kwargs):
+        return self.invoke(**kwargs)
