0631e765e2845de75c4ecfd104187999086256be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 15:04:09 +0000

    Add package discrete-distribution-network/0.0.9/discrete_distribution_network-0.0.9.tar.gz

---
+++ packages/discrete-distribution-network/discrete_distribution_network-0.0.9.tar.gz/discrete_distribution_network-0.0.9/tests/test_ddn.py
@@ -0,0 +1,100 @@
+import pytest
+param = pytest.mark.parametrize
+
+import torch
+from torch import tensor, nn
+
+@param('use_mlp', (False, True))
+@param('straight_through', (False, True))
+def test_ddn(
+    use_mlp,
+    straight_through
+):
+    from discrete_distribution_network.ddn import GuidedSampler
+
+    network = None
+    if use_mlp:
+        network = nn.Sequential(
+            nn.Conv2d(16, 8, 1),
+            nn.ReLU(),
+            nn.Conv2d(8, 3, 1)
+        )
+
+    sampler = GuidedSampler(
+        dim = 16,
+        dim_query = 3,
+        codebook_size = 10,
+        network = network,
+        min_total_count_before_split_prune = 1,
+        crossover_top2_prob = 1.,
+        straight_through_distance_logits = straight_through
+    )
+
+    features = torch.randn(10, 16, 32, 32)
+    query_image = torch.randn(10, 3, 32, 32)
+
+    out, codes, commit_loss = sampler(features, query_image)
+
+    assert out.shape == query_image.shape
+    assert codes.shape == (10,)
+    assert commit_loss.numel() == 1
+
+    sampler.split_and_prune_()
+
+    # after much training
+
+    assert sampler.forward_for_codes(features[:3], tensor([3, 5, 2])).shape == (3, 3, 32, 32)
+    assert sampler.forward_for_codes(features[:3], tensor(7)).shape == (3, 3, 32, 32)
+
+def test_patches():
+    from discrete_distribution_network.ddn import GuidedSampler
+
+    sampler = GuidedSampler(
+        dim = 16,
+        dim_query = 3,
+        codebook_size = 10,
+        min_total_count_before_split_prune = 1,
+        crossover_top2_prob = 1.,
+        patch_size = 16
+    )
+
+    features = torch.randn(10, 16, 32, 32)
+    query_image = torch.randn(10, 3, 32, 32)
+
+    out, codes, commit_loss = sampler(features, query_image)
+
+    assert out.shape == query_image.shape
+    assert codes.shape == (10, 2, 2) # (2, 2) since each batch has 4 patches
+    assert commit_loss.numel() == 1
+
+    sampler.split_and_prune_()
+
+    # after much training
+
+    assert sampler.forward_for_codes(features[:3], codes[:3]).shape == (3, 3, 32, 32)
+
+def test_non_image():
+    from discrete_distribution_network.ddn import GuidedSampler
+
+    sampler = GuidedSampler(
+        dim = 32,
+        codebook_size = 10,
+        network = nn.Linear(32, 16),
+        min_total_count_before_split_prune = 1,
+        crossover_top2_prob = 1.,
+    )
+
+    features = torch.randn(10, 32)
+    query = torch.randn(10, 16)
+
+    out, codes, commit_loss = sampler(features, query)
+
+    assert out.shape == query.shape
+    assert codes.shape == (10,)
+    assert commit_loss.numel() == 1
+
+    sampler.split_and_prune_()
+
+    # after much training
+
+    assert sampler.forward_for_codes(features[:3], codes[:3]).shape == (3, 16)
