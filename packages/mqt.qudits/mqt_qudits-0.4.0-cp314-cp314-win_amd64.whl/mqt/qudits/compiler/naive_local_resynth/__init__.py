59b1a22372cd10d40d29ca489af9fc264dd78672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 19:04:33 +0000

    Add package mqt.qudits/0.4.0/mqt_qudits-0.4.0-cp314-cp314-win_amd64.whl

---
+++ packages/mqt.qudits/mqt_qudits-0.4.0-cp314-cp314-win_amd64.whl/mqt/qudits/compiler/naive_local_resynth/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+# Copyright (c) 2025 Munich Quantum Software Company GmbH
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+from __future__ import annotations
+
+from mqt.qudits.compiler.naive_local_resynth.local_resynth import NaiveLocResynthOptPass
+
+__all__ = [
+    "NaiveLocResynthOptPass",
+]
