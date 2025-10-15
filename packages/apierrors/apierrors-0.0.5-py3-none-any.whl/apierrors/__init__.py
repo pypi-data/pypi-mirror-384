77d328801456cba2bd6fd3fc367d9ecafcca7f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 17:04:50 +0000

    Add package apierrors/0.0.5/apierrors-0.0.5-py3-none-any.whl

---
+++ packages/apierrors/apierrors-0.0.5-py3-none-any.whl/apierrors/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.0.5"
+
+from .adapters import *
+
+from .status_codes import *
---
+++ packages/apierrors/apierrors-0.0.5-py3-none-any.whl/apierrors/status_codes/err4xx/__init__.py
@@ -0,0 +1,7 @@
+from .err400 import Err400, HttpErr400BadRequest
+from .err401 import Err401, HttpErr401Unauthorized
+from .err403 import Err403, HttpErr403Forbidden
+from .err404 import Err404, HttpErr404NotFound
+from .err405 import Err405, HttpErr405MethodNotAllowed
+from .err409 import Err409, HttpErr409Conflict
+from .err422 import Err422, HttpErr422UnprocessableEntity
