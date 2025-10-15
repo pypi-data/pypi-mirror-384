335fae1c5832dcc88acf8e16f32801cc02825cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 17:04:06 +0000

    Add package liftover/1.3.3/liftover-1.3.3-cp314-cp314-musllinux_1_2_x86_64.whl

---
+++ packages/liftover/liftover-1.3.3-cp314-cp314-musllinux_1_2_x86_64.whl/liftover/download_file.py
@@ -0,0 +1,13 @@
+
+import urllib3
+
+def download_file(url, path):
+    http = urllib3.PoolManager()
+    r = http.request('GET', url, preload_content=False)
+    if r.status != 200:
+        raise ValueError('problem accessing ' + url)
+    with open(path, 'wb') as f:
+        for chunk in r.stream(1600):
+            f.write(chunk)
+    r.release_conn()
+    http.clear()
