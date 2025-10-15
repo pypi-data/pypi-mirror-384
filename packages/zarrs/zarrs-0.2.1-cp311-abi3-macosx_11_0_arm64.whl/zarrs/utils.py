745ddd1f262770f3ea08ba4d167fe83ad10d8538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 17:04:27 +0000

    Add package zarrs/0.2.1/zarrs-0.2.1-cp311-abi3-macosx_11_0_arm64.whl

---
+++ packages/zarrs/zarrs-0.2.1-cp311-abi3-macosx_11_0_arm64.whl/zarrs/utils.py
@@ -0,0 +1,205 @@
+from __future__ import annotations
+
+import operator
+import os
+from dataclasses import dataclass
+from functools import reduce
+from typing import TYPE_CHECKING, Any
+
+import numpy as np
+from zarr.core.array_spec import ArraySpec
+from zarr.core.indexing import SelectorTuple, is_integer
+
+from zarrs._internal import Basic, WithSubset
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
+    from types import EllipsisType
+
+    from zarr.abc.store import ByteGetter, ByteSetter
+    from zarr.dtype import ZDType
+
+
+# adapted from https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.ThreadPoolExecutor
+def get_max_threads() -> int:
+    return (os.cpu_count() or 1) + 4
+
+
+class DiscontiguousArrayError(Exception):
+    pass
+
+
+class CollapsedDimensionError(Exception):
+    pass
+
+
+class FillValueNoneError(Exception):
+    pass
+
+
+# This is a (mostly) copy of the function from zarr.core.indexing that fixes:
+#   DeprecationWarning: Conversion of an array with ndim > 0 to a scalar is deprecated
+# TODO: Upstream this fix
+def make_slice_selection(selection: tuple[np.ndarray | float]) -> list[slice]:
+    ls: list[slice] = []
+    for dim_selection in selection:
+        if is_integer(dim_selection):
+            ls.append(slice(int(dim_selection), int(dim_selection) + 1, 1))
+        elif isinstance(dim_selection, np.ndarray):
+            dim_selection = dim_selection.ravel()
+            if len(dim_selection) == 1:
+                ls.append(
+                    slice(int(dim_selection.item()), int(dim_selection.item()) + 1, 1)
+                )
+            else:
+                diff = np.diff(dim_selection)
+                if (diff != 1).any() and (diff != 0).any():
+                    raise DiscontiguousArrayError(diff)
+                ls.append(slice(dim_selection[0], dim_selection[-1] + 1, 1))
+        else:
+            ls.append(dim_selection)
+    return ls
+
+
+def selector_tuple_to_slice_selection(selector_tuple: SelectorTuple) -> list[slice]:
+    if isinstance(selector_tuple, slice):
+        return [selector_tuple]
+    if all(isinstance(s, slice) for s in selector_tuple):
+        return list(selector_tuple)
+    return make_slice_selection(selector_tuple)
+
+
+def resulting_shape_from_index(
+    array_shape: tuple[int, ...],
+    index_tuple: tuple[int | slice | EllipsisType | np.ndarray],
+    drop_axes: tuple[int, ...],
+    *,
+    pad: bool,
+) -> tuple[int, ...]:
+    result_shape = []
+    advanced_index_shapes = [
+        idx.shape for idx in index_tuple if isinstance(idx, np.ndarray)
+    ]
+    basic_shape_index = 0
+
+    # Broadcast all advanced indices, if any
+    if advanced_index_shapes:
+        result_shape += np.broadcast_shapes(*advanced_index_shapes)
+        # Consume dimensions from array_shape
+        basic_shape_index += len(advanced_index_shapes)
+
+    # Process each remaining index in index_tuple
+    for idx in index_tuple:
+        if isinstance(idx, int):
+            # Integer index reduces dimension, so skip this dimension in array_shape
+            basic_shape_index += 1
+        elif isinstance(idx, slice):
+            if idx.step is not None and idx.step > 1:
+                raise DiscontiguousArrayError(
+                    "Step size greater than 1 is not supported"
+                )
+            # Slice keeps dimension, adjust size accordingly
+            start, stop, _ = idx.indices(array_shape[basic_shape_index])
+            result_shape.append(stop - start)
+            basic_shape_index += 1
+        elif idx is Ellipsis:
+            # Calculate number of dimensions that Ellipsis should fill
+            num_to_fill = len(array_shape) - len(index_tuple) + 1
+            result_shape += array_shape[
+                basic_shape_index : basic_shape_index + num_to_fill
+            ]
+            basic_shape_index += num_to_fill
+        elif not isinstance(idx, np.ndarray):
+            raise ValueError(f"Invalid index type: {type(idx)}")
+
+    # Step 4: Append remaining dimensions from array_shape if fewer indices were used
+    if basic_shape_index < len(array_shape) and pad:
+        result_shape += array_shape[basic_shape_index:]
+
+    return tuple(size for idx, size in enumerate(result_shape) if idx not in drop_axes)
+
+
+def prod_op(x: Iterable[int]) -> int:
+    return reduce(operator.mul, x, 1)
+
+
+def get_shape_for_selector(
+    selector_tuple: SelectorTuple,
+    shape: tuple[int, ...],
+    *,
+    pad: bool,
+    drop_axes: tuple[int, ...] = (),
+) -> tuple[int, ...]:
+    if isinstance(selector_tuple, slice | np.ndarray):
+        return resulting_shape_from_index(
+            shape,
+            (selector_tuple,),
+            drop_axes,
+            pad=pad,
+        )
+    return resulting_shape_from_index(shape, selector_tuple, drop_axes, pad=pad)
+
+
+def get_implicit_fill_value(dtype: ZDType, fill_value: Any) -> Any:
+    if fill_value is None:
+        fill_value = dtype.default_scalar()
+    return fill_value
+
+
+@dataclass(frozen=True)
+class RustChunkInfo:
+    chunk_info_with_indices: list[WithSubset]
+    write_empty_chunks: bool
+
+
+def make_chunk_info_for_rust_with_indices(
+    batch_info: Iterable[
+        tuple[ByteGetter | ByteSetter, ArraySpec, SelectorTuple, SelectorTuple, bool]
+    ],
+    drop_axes: tuple[int, ...],
+    shape: tuple[int, ...],
+) -> RustChunkInfo:
+    shape = shape if shape else (1,)  # constant array
+    chunk_info_with_indices: list[WithSubset] = []
+    write_empty_chunks: bool = True
+    for (
+        byte_getter,
+        chunk_spec,
+        chunk_selection,
+        out_selection,
+        _,
+    ) in batch_info:
+        write_empty_chunks = chunk_spec.config.write_empty_chunks
+        if chunk_spec.fill_value is None:
+            chunk_spec = ArraySpec(
+                chunk_spec.shape,
+                chunk_spec.dtype,
+                get_implicit_fill_value(chunk_spec.dtype, chunk_spec.fill_value),
+                chunk_spec.config,
+                chunk_spec.prototype,
+            )
+        chunk_info = Basic(byte_getter, chunk_spec)
+        out_selection_as_slices = selector_tuple_to_slice_selection(out_selection)
+        chunk_selection_as_slices = selector_tuple_to_slice_selection(chunk_selection)
+        shape_chunk_selection_slices = get_shape_for_selector(
+            tuple(chunk_selection_as_slices),
+            chunk_spec.shape,
+            pad=True,
+            drop_axes=drop_axes,
+        )
+        shape_chunk_selection = get_shape_for_selector(
+            chunk_selection, chunk_spec.shape, pad=True, drop_axes=drop_axes
+        )
+        if prod_op(shape_chunk_selection) != prod_op(shape_chunk_selection_slices):
+            raise CollapsedDimensionError(
+                f"{shape_chunk_selection} != {shape_chunk_selection_slices}"
+            )
+        chunk_info_with_indices.append(
+            WithSubset(
+                chunk_info,
+                chunk_subset=chunk_selection_as_slices,
+                subset=out_selection_as_slices,
+                shape=shape,
+            )
+        )
+    return RustChunkInfo(chunk_info_with_indices, write_empty_chunks)
