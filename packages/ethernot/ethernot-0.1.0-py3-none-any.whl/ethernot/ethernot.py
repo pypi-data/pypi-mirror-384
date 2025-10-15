25e22bee18503abf3943f6b03a801e61b42c25c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 19:05:08 +0000

    Add package ethernot/0.1.0/ethernot-0.1.0-py3-none-any.whl

---
+++ packages/ethernot/ethernot-0.1.0-py3-none-any.whl/ethernot/ethernot.py
@@ -0,0 +1,41 @@
+import argparse
+import threading
+import sys
+import time
+import random
+
+def loading_anim(): # do not call this from main thread
+    symbols = [
+        ["◴", "◷", "◶", "◵"],
+        ["◰", "◳", "◲", "◱"],
+        ["←", "↖", "↑", "↗", "→", "↘", "↓", "↙"],
+        ["▁", "▂", "▃", "▄", "▅", "▆", "▇", "█", "▇", "▆", "▅", "▄", "▃"],
+        ["⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷"],
+        ["⢎⡰", "⢎⡡", "⢎⡑", "⢎⠱", "⠎⡱", "⢊⡱", "⢌⡱", "⢆⡱"],
+        ["-", "/", "-", "\\"],
+        ["[    ]", "[=   ]", "[==  ]", "[=== ]", "[====]", "[ ===]", "[  ==]", "[   =]", "[    ]", "[   =]", "[  ==]", "[ ===]", "[====]", "[=== ]", "[==  ]", "[=   ]"],
+        ["▹▹▹▹▹", "▸▹▹▹▹", "▹▸▹▹▹", "▹▹▸▹▹", "▹▹▹▸▹", "▹▹▹▹▸"]
+    ]
+    random.shuffle(symbols)
+    while(1):
+        for i in symbols[1]:
+            print(i)
+
+def set_loading_anim(enable):
+    pass
+
+def connect(server, user):
+    print(f"Connecting to {server} as {user}...")
+    loading_anim()
+
+
+def main():
+    parser = argparse.ArgumentParser(prog="ethernot", description="EtherNOT CLI Client")
+    parser.add_argument("--server", help="Server IP to connect to", required = True)
+    parser.add_argument("--user", help="Username to connect under (anonymous)", required = True)
+    args = parser.parse_args()
+    
+    connect(args.server, args.user)
+    
+if __name__ == "__main":
+    main()
