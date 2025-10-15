c8e032811bd081549ad4e292bd1d5df079278371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 15:04:42 +0000

    Add package KratosOptimizationApplication/10.3.1/kratosoptimizationapplication-10.3.1-cp38-cp38-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/KratosOptimizationApplication/kratosoptimizationapplication-10.3.1-cp38-cp38-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/KratosMultiphysics/OptimizationApplication/ImplicitFilterUtils.pyi
@@ -0,0 +1,4 @@
+import Kratos
+
+def SetBulkRadiusForShapeFiltering(input_model_part: Kratos.ModelPart) -> None:
+    """SetBulkRadiusForShapeFiltering(input_model_part: Kratos.ModelPart) -> None"""
