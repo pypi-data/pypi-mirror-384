92f893cf58e1894f591a508709afef149a4b9e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 16:05:30 +0000

    Add package ataraxis-time/5.0.0/ataraxis_time-5.0.0-cp312-cp312-macosx_15_0_arm64.whl

---
+++ packages/ataraxis-time/ataraxis_time-5.0.0-cp312-cp312-macosx_15_0_arm64.whl/ataraxis_time/precision_timer/__init__.py
@@ -0,0 +1,7 @@
+"""This package provides the PrecisionTimer class and support functions used to work with the class and benchmark its
+performance.
+"""
+
+from .timer_class import PrecisionTimer, TimerPrecisions
+
+__all__ = ["PrecisionTimer", "TimerPrecisions"]
