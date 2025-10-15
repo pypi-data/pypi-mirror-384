35e30a11340674e9312a9e9afbed9bcfc39f3ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 19:05:00 +0000

    Add package nnsight/0.5.9/nnsight-0.5.9-cp39-cp39-win32.whl

---
+++ packages/nnsight/nnsight-0.5.9-cp39-cp39-win32.whl/nnsight/modeling/base.py
@@ -0,0 +1,14 @@
+
+from ..intervention.envoy import Envoy
+
+class NNsight(Envoy):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        # TODO: legacy
+        self.__dict__['_model'] = self._module
+        
+        self._session = None
+
+        
+        
