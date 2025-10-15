d47507d6b0479392b2fdc1da7c7623e49e24abc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 16:04:36 +0000

    Add package load-distribution/0.1.7/load_distribution-0.1.7-py3-none-any.whl

---
+++ packages/load-distribution/load_distribution-0.1.7-py3-none-any.whl/load_distribution/__init__.py
@@ -0,0 +1,8 @@
+"""
+A package for converting arbitrary polygons into linearly distributed
+loads on beams or other linear elements.
+"""
+
+__version__ = "0.1.7"
+
+from load_distribution.load_distribution import *
