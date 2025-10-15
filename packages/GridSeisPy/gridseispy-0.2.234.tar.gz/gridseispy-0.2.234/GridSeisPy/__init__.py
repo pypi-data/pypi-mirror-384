ab637d1d82ecf16c069f3abe332acf319a12015b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 15:04:17 +0000

    Add package GridSeisPy/0.2.234/gridseispy-0.2.234.tar.gz

---
+++ packages/GridSeisPy/gridseispy-0.2.234.tar.gz/gridseispy-0.2.234/GridSeisPy/__init__.py
@@ -0,0 +1,16 @@
+"""
+SeisPy is a Python library for seismic data processing.
+"""
+
+__version__ = "0.2.234"
+
+from .seis_data import SeisData
+from .horiz import Horiz
+from .mapping import TraceField, BinField, CVDFile, Trace
+
+
+"""
+       py -m build
+       py -m twine check dist/*
+       py -m twine upload --non-interactive -r pypi dist/*
+"""
