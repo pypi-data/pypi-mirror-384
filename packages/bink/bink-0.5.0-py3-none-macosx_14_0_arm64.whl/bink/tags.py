1509ab99d85719562db2fdca3b5a2c70fc45e9b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 18:06:20 +0000

    Add package bink/0.5.0/bink-0.5.0-py3-none-macosx_14_0_arm64.whl

---
+++ packages/bink/bink-0.5.0-py3-none-macosx_14_0_arm64.whl/bink/tags.py
@@ -0,0 +1,63 @@
+# pylint: disable=E1101, R0903
+
+"""Handle Ink tags."""
+import ctypes
+from bink import LIB, BINK_OK
+
+
+class TagsIterator:
+    """Iterator for tags."""
+    def __init__(self, tags):
+        self._tags = tags
+        self._index = 0
+
+    def __next__(self):
+        if self._index >= len(self._tags):
+            raise StopIteration
+
+        self._index += 1
+        return self._tags[self._index - 1]
+
+
+class Tags:
+    """Contains a list of tags."""
+    def __init__(self, tags, c_len: int):
+        self._tags = tags
+        self._len = c_len
+
+    def __len__(self) -> int:
+        """Returns the number of tags."""
+        return self._len
+
+    def __bool__(self) -> bool:
+        return self._len != 0
+
+    def __iter__(self):
+        return TagsIterator(self)
+
+    def __getitem__(self, idx: int) -> str:
+        """Returns the tag text"""
+
+        if not isinstance(idx, int):
+            raise TypeError
+
+        if idx < 0 or idx >= self._len:
+            raise IndexError
+
+        return self.get(idx)
+
+    def get(self, idx) -> str:
+        """Returns the tag text."""
+        tag = ctypes.c_char_p()
+        ret = LIB.bink_tags_get(self._tags, idx, ctypes.byref(tag))
+
+        if ret != BINK_OK:
+            raise RuntimeError("Error getting tag, index out of bounds?")
+
+        result = tag.value.decode('utf-8')
+        LIB.bink_cstring_free(tag)
+
+        return result
+
+    def __del__(self):
+        LIB.bink_tags_free(self._tags)
