db322bb273f7b1fbf02eb886b2c6d317c8eedb09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 17:05:09 +0000

    Add package hyperdiv/0.1.9/hyperdiv-0.1.9.tar.gz

---
+++ packages/hyperdiv/hyperdiv-0.1.9.tar.gz/hyperdiv-0.1.9/hyperdiv/hyperdiv-docs/hyperdiv_docs/pages/introduction/docs_overview.py
@@ -0,0 +1,55 @@
+import hyperdiv as hd
+from ...router import router
+from ...page import page
+from ...code_examples import docs_markdown
+
+
+@router.route("/introduction/docs-overview")
+def docs_overview():
+    with page() as p:
+        p.title("# This Documentation App")
+
+        hd.markdown(
+            """
+            The rest of this app is organized in the following sections, which
+            can be accessed via the sidebar:
+            """
+        )
+
+        hd.markdown("### Guide", font_color="blue")
+
+        hd.markdown(
+            """
+
+            The [Guide](/guide/getting-started) section is a guided
+            walk through some of Hyperdiv's core features. It is not
+            an exhaustive explanation of Hyperdiv. Instead, it tries
+            to impart a high level taste of what's currently possible
+            with Hyperdiv.
+
+            """
+        )
+
+        hd.markdown("### Extending Hyperdiv", font_color="blue")
+
+        hd.markdown(
+            """
+
+            The [Extending Hyperdiv](/extending-hyperdiv) section
+            dives into various ways to customize and extend Hyperdiv
+            components, and build new components with custom
+            Javascript, HTML, and CSS.
+
+            """
+        )
+
+        hd.markdown("### Reference", font_color="blue")
+
+        hd.markdown(
+            """
+
+            The [Reference](/reference/components) section documents
+            each Hyperdiv component and function in detail.
+
+            """
+        )
