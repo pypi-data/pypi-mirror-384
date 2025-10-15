d384f4f250b7f03919da0708eb17170287b0df97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 20:06:27 +0000

    Add package commandchat/0.0.6/commandchat-0.0.6-py3-none-any.whl

---
+++ packages/commandchat/commandchat-0.0.6-py3-none-any.whl/occ/configuration/profile_config.py
@@ -0,0 +1,36 @@
+import click
+
+import occ.commons.config as config
+import occ.utils.logger as logger
+
+
+def add_default_profile():
+    api_key = click.prompt(logger.style('\n[default] Your default api_key '),
+                           default=config.get_default_env("api_key"), type=str)
+    config.set_env('default', 'api_key', api_key)
+
+    api_key = click.prompt(logger.style('\n[default] Your api_base_url(Optional) '),
+                           default=config.get_default_env("api_base_url"), type=str)
+    config.set_env('default', 'api_base_url', api_key)
+
+    limit_history = click.prompt(logger.style('\n[default] Your default limit_history '),
+                                 default=config.get_default_env("limit_history"), type=str)
+    config.set_env('default', 'limit_history', limit_history)
+
+
+def input_config_vars(profile_name, key_name, is_default_exist):
+    if is_default_exist:
+        default_value = config.get_env('default', key_name)
+        user_input = click.prompt(logger.style('\ndefault value  ' + key_name), default=default_value, type=str)
+    else:
+        user_input = click.prompt(logger.style('\nYour ' + key_name), type=str)
+
+    config.set_env(profile_name, key_name, user_input)
+
+
+def add_profile(profile_name):
+    config.add_profile(profile_name)
+    config.set_env(profile_name, 'profile_name', profile_name)
+    input_config_vars(profile_name, 'api_key', False)
+    input_config_vars(profile_name, 'api_base_url', False)
+    input_config_vars(profile_name, 'limit_history', False)
