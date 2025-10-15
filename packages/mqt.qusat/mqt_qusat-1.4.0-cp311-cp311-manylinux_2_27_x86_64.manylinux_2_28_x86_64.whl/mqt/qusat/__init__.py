9bc3f18ba775fbc489ec333bf0d1e9d0cf0cac1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 17:04:30 +0000

    Add package mqt.qusat/1.4.0/mqt_qusat-1.4.0-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/mqt.qusat/mqt_qusat-1.4.0-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/mqt/qusat/__init__.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+# Copyright (c) 2025 Munich Quantum Software Company GmbH
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+"""MQT QuSAT library."""
+
+from __future__ import annotations
+
+import os
+import sys
+from pathlib import Path
+
+if sys.platform == "win32" and "Z3_ROOT" in os.environ:
+    lib_path = Path(os.environ["Z3_ROOT"]) / "lib"
+    if lib_path.exists():
+        os.add_dll_directory(str(lib_path))
+    bin_path = Path(os.environ["Z3_ROOT"]) / "bin"
+    if bin_path.exists():
+        os.add_dll_directory(str(bin_path))
+
+from ._version import version as __version__
+from .pyqusat import check_equivalence, generate_dimacs
+
+__all__ = ["__version__", "check_equivalence", "generate_dimacs"]
