537115736a2c60d882fb04c741d99634b80559e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 20:06:51 +0000

    Add package korpy/1.1.5/korpy-1.1.5.tar.gz

---
+++ packages/korpy/korpy-1.1.5.tar.gz/korpy-1.1.5/src/korpy/korpyinfo.py
@@ -0,0 +1,13 @@
+"""
+a module to give information about korpy
+"""
+import os
+import urllib.request
+
+def get_release_note() -> str:
+    if not os.path.exists('release_note.rst'):
+        urllib.request.urlretrieve('https://raw.githubusercontent.com/MatthewKim12/korpy/main/HISTORY.rst', 'release_note.rst')
+    with open('release_note.rst', 'rb') as f:
+        release_note = f.read().decode('utf-8')
+        filtered = release_note.replace('\n======\n', '\n').replace('- ', '')
+    return filtered
