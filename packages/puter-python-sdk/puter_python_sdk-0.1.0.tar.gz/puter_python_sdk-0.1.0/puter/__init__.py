8cfecee66e1be8e0169eecafbd36dcb55335bfb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 17:05:12 +0000

    Add package puter-python-sdk/0.1.0/puter_python_sdk-0.1.0.tar.gz

---
+++ packages/puter-python-sdk/puter_python_sdk-0.1.0.tar.gz/puter_python_sdk-0.1.0/puter/__init__.py
@@ -0,0 +1,6 @@
+from .ai import PuterAI
+from .exceptions import PuterError, PuterAuthError, PuterAPIError
+
+__version__ = "0.1.1"
+__all__ = ["PuterAI", "PuterError", "PuterAuthError", "PuterAPIError"]
+
