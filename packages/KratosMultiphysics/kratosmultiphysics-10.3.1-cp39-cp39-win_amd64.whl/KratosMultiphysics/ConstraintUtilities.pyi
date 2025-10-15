fd081fc3b53aa24485e027782493c6ea380668e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 15:05:07 +0000

    Add package KratosMultiphysics/10.3.1/kratosmultiphysics-10.3.1-cp39-cp39-win_amd64.whl

---
+++ packages/KratosMultiphysics/kratosmultiphysics-10.3.1-cp39-cp39-win_amd64.whl/KratosMultiphysics/ConstraintUtilities.pyi
@@ -0,0 +1,6 @@
+import Kratos
+
+def ApplyConstraints(arg0: Kratos.ModelPart) -> None:
+    """ApplyConstraints(arg0: Kratos.ModelPart) -> None"""
+def ResetSlaveDofs(arg0: Kratos.ModelPart) -> None:
+    """ResetSlaveDofs(arg0: Kratos.ModelPart) -> None"""
