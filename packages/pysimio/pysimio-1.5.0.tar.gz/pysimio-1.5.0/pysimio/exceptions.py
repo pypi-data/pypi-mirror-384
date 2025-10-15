f1be748a59861c64b11993d44ac8812cf39866d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 15:05:25 +0000

    Add package pysimio/1.5.0/pysimio-1.5.0.tar.gz

---
+++ packages/pysimio/pysimio-1.5.0.tar.gz/pysimio-1.5.0/pysimio/exceptions.py
@@ -0,0 +1,13 @@
+from pysimio.logger import logger
+from http_exceptions import UnauthorizedException
+
+class AuthenticationError(Exception):
+    pass
+
+def exception_handler(func):
+    def wrapper(self, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        except AuthenticationError as e:
+            logger.fatal(f"An error occurred, please try again later. {e}")
+        return wrapper
