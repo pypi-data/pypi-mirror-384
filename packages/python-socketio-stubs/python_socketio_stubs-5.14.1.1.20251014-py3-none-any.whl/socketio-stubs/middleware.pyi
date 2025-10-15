792f77419753c34c536ea793db484699cf261069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 15:04:44 +0000

    Add package python-socketio-stubs/5.14.1.1.20251014/python_socketio_stubs-5.14.1.1.20251014-py3-none-any.whl

---
+++ packages/python-socketio-stubs/python_socketio_stubs-5.14.1.1.20251014-py3-none-any.whl/socketio-stubs/middleware.pyi
@@ -0,0 +1,21 @@
+from typing import Any
+
+import engineio
+from socketio.base_server import BaseServer
+
+class WSGIApp(engineio.WSGIApp):
+    def __init__(
+        self,
+        socketio_app: BaseServer[Any, Any],
+        wsgi_app: Any = ...,
+        static_files: dict[str, dict[str, str]] | None = ...,
+        socketio_path: str = ...,
+    ) -> None: ...
+
+class Middleware(WSGIApp):
+    def __init__(
+        self,
+        socketio_app: BaseServer[Any, Any],
+        wsgi_app: Any = ...,
+        socketio_path: str = ...,
+    ) -> None: ...
