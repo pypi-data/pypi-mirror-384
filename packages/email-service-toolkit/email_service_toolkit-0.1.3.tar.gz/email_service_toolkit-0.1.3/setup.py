81a5177386af097c1d65a309f299c9911b4e5273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 22:04:52 +0000

    Add package email-service-toolkit/0.1.3/email_service_toolkit-0.1.3.tar.gz

---
+++ packages/email-service-toolkit/email_service_toolkit-0.1.3.tar.gz/email_service_toolkit-0.1.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+# Read the long description from README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="email_service_toolkit",  # Package name
+    version="0.1.3",               # Incremented version
+    author="Vamsi Gudapati",
+    author_email="vamsi7673916775@gmail.com",
+    description="Reusable Python toolkit for sending emails and managing email templates",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vamsichowdaryg/email_service_toolkit",
+    packages=find_packages(),
+    install_requires=[
+        "aiohttp>=3.8.0",
+        "fastapi>=0.102.0",
+        "python-dotenv>=1.0.0"
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ],
+    license="MIT",
+    include_package_data=True,
+)
