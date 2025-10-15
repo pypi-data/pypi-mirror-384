43433fbe6ec19ded2d2c2f360a05b6f673b38948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 17:04:40 +0000

    Add package tiferet/1.1.5/tiferet-1.1.5-py3-none-any.whl

---
+++ packages/tiferet/tiferet-1.1.5-py3-none-any.whl/tiferet/__init__.py
@@ -0,0 +1,32 @@
+"""Tiferet Version and Global Exports"""
+
+# *** exports
+
+# ** app
+# Export the main application context and related modules.
+# Use a try-except block to avoid import errors on build systems.
+try:
+    from .contexts import AppManagerContext as App
+    from .models import (
+        ModelObject,
+        StringType,
+        IntegerType,
+        BooleanType,
+        FloatType,
+        ListType,
+        DictType,
+        ModelType,
+    )
+    from .commands import *
+    from .contracts import (
+        ModelContract,
+        Repository
+    )
+    from .data import *
+    from .proxies import *
+except:
+    pass
+
+# *** version
+__version__ = '1.1.5'
+
---
+++ packages/tiferet/tiferet-1.1.5-py3-none-any.whl/tiferet/data/__init__.py
@@ -0,0 +1,4 @@
+# *** imports
+
+# ** app
+from .settings import *
