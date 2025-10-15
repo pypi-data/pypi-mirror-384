dca125b074747a362861d9283b722c8214d755c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 19:04:34 +0000

    Add package red-tidegear/1.20.0/red_tidegear-1.20.0.tar.gz

---
+++ packages/red-tidegear/red_tidegear-1.20.0.tar.gz/red_tidegear-1.20.0/tidegear/pydantic/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2025 cswimr <copyright@csw.im>
+# SPDX-License-Identifier: MPL-2.0
+
+
+from .basemodel import BaseModel, CogModel
+from .httpurl import HttpUrl
+
+__all__ = ["BaseModel", "CogModel", "HttpUrl"]
