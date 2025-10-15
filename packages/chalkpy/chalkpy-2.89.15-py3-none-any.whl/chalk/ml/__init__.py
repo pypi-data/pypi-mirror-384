68b330ffb17f02dbd197733fd0b0d48447983e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 17:05:08 +0000

    Add package chalkpy/2.89.15/chalkpy-2.89.15-py3-none-any.whl

---
+++ packages/chalkpy/chalkpy-2.89.15-py3-none-any.whl/chalk/ml/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from chalk.ml.model_file_transfer import HFSourceConfig, LocalSourceConfig, S3SourceConfig, SourceConfig
+from chalk.ml.model_reference import ModelReference
+from chalk.ml.utils import ModelEncoding, ModelType
+
+__all__ = (
+    "ModelType",
+    "ModelEncoding",
+    "ModelReference",
+    "SourceConfig",
+    "LocalSourceConfig",
+    "S3SourceConfig",
+    "HFSourceConfig",
+)
