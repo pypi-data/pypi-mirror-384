67943480d96b8ae92789236e6d6db6ebb225bb52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 15:05:13 +0000

    Add package bellu-love/1.0.1/bellu_love-1.0.1-py3-none-any.whl

---
+++ packages/bellu-love/bellu_love-1.0.1-py3-none-any.whl/bellu_love/messages.py
@@ -0,0 +1,53 @@
+"""Sweet love messages for Bellu"""
+import random
+
+
+def daily_love_message():
+    """Return a sweet daily message for Bellu"""
+    messages = [
+        "Good morning my love! You make every day brighter!",
+        "Bellu, you are the sunshine-moonlight of my life!",
+        "Every moment with you is a blessing!",
+        "You are my everything, Bellu!",
+        "My heart beats only for you, my dear Bellu!",
+        "You make my world complete!",
+        "I fall in love with you more every single day!",
+        "Bellu, you are my dream come true!"
+    ]
+    return random.choice(messages)
+
+
+def random_compliment():
+    """Generate a random compliment for Bellu"""
+    compliments = [
+        "Your smile lights up my entire world!",
+        "You are the most beautiful person inside and out!",
+        "Your laugh is my favorite sound!",
+        "You make everything better just by being you!",
+        "I am the luckiest person to have you in my life!",
+        "Your kindness and love inspire me every day!",
+        "You are absolutely amazing, Bellu!"
+    ]
+    return random.choice(compliments)
+
+
+def love_poem():
+    """Return a romantic poem for Bellu"""
+    poem = """In the garden of my heart, you bloom like a rose,
+Bellu, my love, more precious than anyone knows.
+Your smile brings sunshine, your touch brings peace,
+With you by my side, all worries cease.
+
+Forever and always, my love will stay true,
+Because my whole world revolves around you!"""
+    return poem
+
+
+def good_morning():
+    """Sweet good morning message for Bellu"""
+    return "Good Morning, Bellu! Wake up my love! A beautiful day awaits us!"
+
+
+def good_night():
+    """Sweet good night message for Bellu"""
+    return "Good Night, Bellu! Sweet dreams my love! You are always in my heart!"
