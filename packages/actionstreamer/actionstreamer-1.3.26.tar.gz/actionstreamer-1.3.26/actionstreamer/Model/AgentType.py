d788959f77f3e33d6093eff2353665fbd1f381de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 15:04:28 +0000

    Add package actionstreamer/1.3.26/actionstreamer-1.3.26.tar.gz

---
+++ packages/actionstreamer/actionstreamer-1.3.26.tar.gz/actionstreamer-1.3.26/actionstreamer/Model/AgentType.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class AgentType(Enum):
+
+    Video = 1
+    View = 2
+    Player = 3
+    Transcode = 4
+    Transfer = 5
+    Follower = 6
+    Announcer = 7
+    AnnounceClip = 8
+    UploadFileList = 9
+    TestAgent = 7
+    Device = 8
+    Health = 9
