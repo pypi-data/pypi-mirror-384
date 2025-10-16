e3d506d631a02447031fcf8d7c6544f75324ac43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 05:06:57 +0000

    Add package matp/0.1.0/matp-0.1.0-py3-none-any.whl

---
+++ packages/matp/matp-0.1.0-py3-none-any.whl/python/__init__.py
@@ -0,0 +1,6 @@
+"""Matryoshka Protocol - Invisible secure messaging."""
+
+from .matp import MatryoshkaProtocol, GhostMessage, FutureBundle, InnocenceProof
+
+__version__ = '0.1.0'
+__all__ = ['MatryoshkaProtocol', 'GhostMessage', 'FutureBundle', 'InnocenceProof']
