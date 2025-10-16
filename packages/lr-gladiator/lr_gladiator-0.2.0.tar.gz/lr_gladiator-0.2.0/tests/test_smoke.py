7e5a253cec41a094f8ed99b7f6675d451597b07d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 03:20:25 +0000

    Add package lr-gladiator/0.2.0/lr_gladiator-0.2.0.tar.gz

---
+++ packages/lr-gladiator/lr_gladiator-0.2.0.tar.gz/lr_gladiator-0.2.0/tests/test_smoke.py
@@ -0,0 +1,6 @@
+from gladiator.config import LoginConfig
+from gladiator.arena import ArenaClient
+
+def test_client_instantiates():
+    cfg = LoginConfig(arena_subdomain="dummy", api_key="x")
+    ArenaClient(cfg)
