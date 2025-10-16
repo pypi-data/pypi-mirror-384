0cedf4ae7cf202a7ac411e2fb19e2b35ac1c001d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 17:12:16 +0000

    Add package tsfx/0.2.0/tsfx-0.2.0-cp312-cp312-musllinux_1_2_armv7l.whl

---
+++ packages/tsfx/tsfx-0.2.0-cp312-cp312-musllinux_1_2_armv7l.whl/tsfx/tsfx.pyi
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from collections.abc import Sequence
+from enum import Enum, auto
+
+class FeatureSetting(Enum):
+    Minimal = auto()
+    Efficient = auto()
+    Comprehensive = auto()
+
+class ExtractionSettings:
+    def __init__(
+        self,
+        grouping_cols: Sequence[str],
+        value_cols: Sequence[str],
+        feature_setting: FeatureSetting,
+        config_path: str | None = None,
+        dynamic_settings: DynamicGroupBySettings | None = None,
+    ) -> None:
+        self.grouping_cols = grouping_cols
+        self.value_cols = value_cols
+        self.feature_setting = feature_setting
+        self.dynamic_settings = dynamic_settings
+
+class DynamicGroupBySettings:
+    def __init__(
+        self,
+        time_col: str,
+        every: str,
+        period: str,
+        offset: str,
+        datetime_format: str,
+    ) -> None:
+        self.time_col = time_col
+        self.every = every
+        self.period = period
+        self.offset = offset
+        self.datetime_format = datetime_format
