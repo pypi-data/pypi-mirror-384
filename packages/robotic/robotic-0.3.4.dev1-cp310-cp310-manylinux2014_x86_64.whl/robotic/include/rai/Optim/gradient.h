3cc3eaefc8298305b1afd45b24bdff122dc9e3ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 07:05:44 +0000

    Add package robotic/0.3.4.dev1/robotic-0.3.4.dev1-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/robotic/robotic-0.3.4.dev1-cp310-cp310-manylinux2014_x86_64.whl/robotic/include/rai/Optim/gradient.h
@@ -0,0 +1,63 @@
+/*  ------------------------------------------------------------------
+    Copyright (c) 2011-2024 Marc Toussaint
+    email: toussaint@tu-berlin.de
+
+    This code is distributed under the MIT License.
+    Please see <root-path>/LICENSE for details.
+    --------------------------------------------------------------  */
+
+#pragma once
+
+#include "options.h"
+#include "../Core/array.h"
+
+//===========================================================================
+//
+// proper (monotone) plain gradient descent with line search
+//
+
+struct OptGrad {
+  arr& x;
+  ScalarFunction& f;
+  rai::OptOptions o;
+
+  enum StopCriterion { stopNone=0, stopCrit1, stopCrit2, stopCritLineSteps, stopCritEvals, stopStepFailed };
+  double fx;
+  arr gx;
+  double alpha;
+  uint it, evals, numTinySteps;
+  StopCriterion stopCriterion;
+  ofstream fil;
+
+  OptGrad(arr& x, ScalarFunction& f, rai::OptOptions o=DEFAULT_OPTIONS);
+  ~OptGrad();
+  StopCriterion step();
+  StopCriterion run(uint maxIt = 1000);
+  void reinit(const arr& _x=NoArr);
+};
+
+inline int optGrad(arr& x, ScalarFunction& f, rai::OptOptions opt=DEFAULT_OPTIONS) {
+  return OptGrad(x, f, opt).run();
+}
+
+//===========================================================================
+//
+// Rprop
+//
+
+/** Rprop, a fast gradient-based minimization */
+struct Rprop {
+  unique_ptr<struct sRprop> self;
+  double fx;
+  uint evals;
+  Rprop();
+  ~Rprop();
+  void init(double initialStepSize=1., double minStepSize=1e-6, double stepMaxSize=50.);
+  bool step(arr& x, ScalarFunction& f);
+  uint loop(arr& x, ScalarFunction& f, double stoppingTolerance=1e-2, double initialStepSize=1., uint maxIterations=1000, int verbose=0);
+};
+
+inline uint optRprop(arr& x, ScalarFunction& f, rai::OptOptions opt=DEFAULT_OPTIONS) {
+  return Rprop().loop(x, f, opt.stopTolerance, opt.stepInit, opt.stopEvals, opt.verbose);
+}
+
