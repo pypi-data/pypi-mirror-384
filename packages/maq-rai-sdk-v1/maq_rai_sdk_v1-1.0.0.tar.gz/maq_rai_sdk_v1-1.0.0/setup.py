7f712f4ff5dda9e67290c996ccdf4dee8cb86d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 16:13:36 +0000

    Add package maq-rai-sdk-v1/1.0.0/maq_rai_sdk_v1-1.0.0.tar.gz

---
+++ packages/maq-rai-sdk-v1/maq_rai_sdk_v1-1.0.0.tar.gz/maq_rai_sdk_v1-1.0.0/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Setup script for maq-rai-sdk-v1 package."""
+
+from setuptools import setup
+
+# This setup.py is for backward compatibility with tools that don't support pyproject.toml
+# The main configuration is in pyproject.toml
+if __name__ == "__main__":
+    setup()
