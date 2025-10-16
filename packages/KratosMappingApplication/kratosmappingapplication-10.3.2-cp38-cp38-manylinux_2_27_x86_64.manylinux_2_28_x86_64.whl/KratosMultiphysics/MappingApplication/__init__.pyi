b3bf0261a3583fe2bd8b94673214720dd1306979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 18:06:00 +0000

    Add package KratosMappingApplication/10.3.2/kratosmappingapplication-10.3.2-cp38-cp38-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/KratosMappingApplication/kratosmappingapplication-10.3.2-cp38-cp38-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/KratosMultiphysics/MappingApplication/__init__.pyi
@@ -0,0 +1,6 @@
+import Kratos
+from . import IgaMappingIntersectionUtilities as IgaMappingIntersectionUtilities, MapperUtilities as MapperUtilities, MappingIntersectionUtilities as MappingIntersectionUtilities
+
+class KratosMappingApplication(Kratos.KratosApplication):
+    def __init__(self) -> None:
+        """__init__(self: KratosMappingApplication.KratosMappingApplication) -> None"""
