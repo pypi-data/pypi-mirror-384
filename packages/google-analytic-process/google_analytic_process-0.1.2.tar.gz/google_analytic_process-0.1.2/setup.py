ecc67969f828f2a3f2365c855c8e88f0208abef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 17:12:37 +0000

    Add package google-analytic-process/0.1.2/google_analytic_process-0.1.2.tar.gz

---
+++ packages/google-analytic-process/google_analytic_process-0.1.2.tar.gz/google_analytic_process-0.1.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="google-analytic-process",
+    version="0.1.2",
+    author="Sang Pham",
+    author_email="sang@polaroid.com",
+    description="A simple API for running Google Analytics 4 reports",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/ga4-reporter",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "google-analytics-data>=0.16.0",
+        "pandas>=1.3.0",
+        "numpy>=1.21.0",
+        "protobuf>=4.21.0",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=7.0",
+            "pytest-cov>=3.0",
+            "black>=22.0",
+            "flake8>=4.0",
+        ],
+    },
+)
