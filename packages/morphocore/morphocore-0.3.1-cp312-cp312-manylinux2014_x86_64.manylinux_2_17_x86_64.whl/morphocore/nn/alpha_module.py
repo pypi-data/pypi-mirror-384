483a83ce3ad036aa5065a6727dbc9c3d988a7cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 03:20:06 +0000

    Add package morphocore/0.3.1/morphocore-0.3.1-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/morphocore/morphocore-0.3.1-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/morphocore/nn/alpha_module.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from .morpho_module import MorphoModule
+
+
+class AlphaModule(MorphoModule):
+    """
+    Common module for Mathematical Morpholocial operations which approximate both erosion and dilation depending on alpha !
+    """
+    def __init__(self, in_channel : int, out_channel : int, kernel_shape : tuple, channel_merge_mode: str = "sum", init_alpha : float = 0.0):
+        super().__init__(in_channel, out_channel, kernel_shape, channel_merge_mode)
+        self.alpha = nn.Parameter(torch.tensor(init_alpha))
+
+
+        
---
+++ packages/morphocore/morphocore-0.3.1-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/morphocore/nn/dilation.py
@@ -0,0 +1,21 @@
+from .morpho_module import MorphoModule
+from morphocore.functional import dilation
+
+
+class Dilation(MorphoModule):
+    """
+    Dilation Module
+    Perform a dilation on the input with the given weight
+    Args:
+        in_channel (int): Number of input channels
+        out_channel (int): Number of output channels
+        kernel_shape (tuple): Shape of the morphological kernel
+        channel_merge_mode (str): Channel merge mode
+    Returns:
+        Output tensor, shape : (batch, out_channels, height, width)
+    """
+    def __init__(self, in_channel : int, out_channel : int, kernel_shape : tuple, channel_merge_mode: str = "sum"):
+        super().__init__(in_channel, out_channel, kernel_shape, channel_merge_mode)
+
+    def forward(self, x):
+        return dilation(x, self.weight, self.channel_merge_mode)
