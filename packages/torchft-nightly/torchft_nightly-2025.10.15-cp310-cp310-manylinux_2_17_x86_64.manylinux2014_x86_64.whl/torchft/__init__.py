0a0c5d24c8a0e7b02c31657a6dbd0b4ec88700e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 03:20:01 +0000

    Add package torchft-nightly/2025.10.15/torchft_nightly-2025.10.15-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/torchft-nightly/torchft_nightly-2025.10.15-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/torchft/__init__.py
@@ -0,0 +1,29 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from torchft.data import DistributedSampler
+from torchft.ddp import DistributedDataParallel
+from torchft.manager import Manager
+from torchft.optim import OptimizerWrapper as Optimizer
+from torchft.process_group import (
+    ProcessGroupBabyNCCL,
+    ProcessGroupBabyXCCL,
+    ProcessGroupGloo,
+    ProcessGroupNCCL,
+    ProcessGroupXCCL,
+)
+
+__all__ = (
+    "DistributedDataParallel",
+    "DistributedSampler",
+    "Manager",
+    "Optimizer",
+    "ProcessGroupNCCL",
+    "ProcessGroupXCCL",
+    "ProcessGroupBabyNCCL",
+    "ProcessGroupBabyXCCL",
+    "ProcessGroupGloo",
+)
