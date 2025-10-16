e354de3c3eff7eae2569f5cbc8cd60daf24afa19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 15:06:29 +0000

    Add package grpc-observability/1.0.0/grpc_observability-1.0.0.tar.gz

---
+++ packages/grpc-observability/grpc_observability-1.0.0.tar.gz/grpc_observability-1.0.0/README.rst
@@ -0,0 +1,2 @@
+The official package of gRPC Python observability package is `grpcio-observability <https://pypi.org/project/grpcio-observability/>`_.
+Please download that package instead.
---
+++ packages/grpc-observability/grpc_observability-1.0.0.tar.gz/grpc_observability-1.0.0/setup.py
@@ -0,0 +1,53 @@
+# Copyright 2020 The gRPC Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Setup module for the gRPC Python observability package package."""
+
+import sys
+import os
+
+import setuptools
+
+_PACKAGE_PATH = os.path.realpath(os.path.dirname(__file__))
+_README_PATH = os.path.join(_PACKAGE_PATH, "README.rst")
+
+# Ensure we're in the proper directory whether or not we're being used by pip.
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+CLASSIFIERS = [
+    "Development Status :: 7 - Inactive",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: Apache Software License",
+]
+
+
+HINT = "Please install the official package with: pip install grpcio-observability"
+
+
+if "sdist" not in sys.argv:
+    raise RuntimeError(HINT)
+
+
+setuptools.setup(
+    name="grpc-observability",
+    version="1.0.0",
+    description=HINT,
+    author="The gRPC Authors",
+    author_email="grpc-io@googlegroups.com",
+    long_description=open(_README_PATH, "r").read(),
+    long_description_content_type="text/x-rst",
+    url="https://grpc.io",
+    license="Apache License 2.0",
+    classifiers=CLASSIFIERS,
+)
