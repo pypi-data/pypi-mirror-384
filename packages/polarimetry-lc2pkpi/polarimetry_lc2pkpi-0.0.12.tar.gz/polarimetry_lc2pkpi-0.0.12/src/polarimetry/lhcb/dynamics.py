6604b5fbee74542c91f44cc048634009286bcab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 18:05:48 +0000

    Add package polarimetry-lc2pkpi/0.0.12/polarimetry_lc2pkpi-0.0.12.tar.gz

---
+++ packages/polarimetry-lc2pkpi/polarimetry_lc2pkpi-0.0.12.tar.gz/polarimetry_lc2pkpi-0.0.12/src/polarimetry/lhcb/dynamics.py
@@ -0,0 +1,146 @@
+"""Dynamics lineshape definitions for the LHCb amplitude model."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import sympy as sp
+from ampform_dpd.dynamics import (
+    BlattWeisskopf,
+    BreitWignerMinL,
+    BuggBreitWigner,
+    FlattéSWave,
+    Q,
+)
+from ampform_dpd.dynamics.builder import (
+    _get_angular_momentum,  # noqa: PLC2701  # pyright:ignore[reportPrivateUsage]s
+    create_mass_symbol,
+    get_mandelstam_s,
+)
+
+from polarimetry.lhcb.particle import Σ, π
+
+if TYPE_CHECKING:
+    from ampform_dpd.decay import ThreeBodyDecayChain
+
+
+def formulate_bugg_breit_wigner(
+    decay_chain: ThreeBodyDecayChain,
+) -> tuple[BuggBreitWigner, dict[sp.Symbol, complex | float]]:
+    if {s.index for s in decay_chain.decay_products} != {2, 3}:
+        msg = f"Bugg Breit-Wigner only defined for K* → Kπ (subsystem 1, not {decay_chain.spectator.index})"
+        raise ValueError(msg)
+    s = get_mandelstam_s(decay_chain.decay_node)
+    m2, m3 = _create_decay_product_masses(decay_chain)
+    gamma = sp.Symbol(Rf"\gamma_{{{decay_chain.resonance.name}}}")
+    mass = sp.Symbol(f"m_{{{decay_chain.resonance.name}}}")
+    width = sp.Symbol(Rf"\Gamma_{{{decay_chain.resonance.name}}}")
+    parameter_defaults: dict[sp.Symbol, complex | float] = {
+        mass: decay_chain.resonance.mass,
+        width: decay_chain.resonance.width,
+        m2: decay_chain.decay_products[0].mass,
+        m3: decay_chain.decay_products[1].mass,
+        gamma: 1,
+    }
+    expr = BuggBreitWigner(s, mass, width, m3, m2, gamma)  # Adler zero for K minus π
+    return expr, parameter_defaults
+
+
+def formulate_exponential_bugg_breit_wigner(
+    decay_chain: ThreeBodyDecayChain,
+) -> tuple[sp.Expr, dict[sp.Symbol, complex | float]]:
+    """See `this paper, Eq. (4) <https://arxiv.org/pdf/hep-ex/0510019.pdf#page=3>`_."""
+    expr, parameter_defaults = formulate_bugg_breit_wigner(decay_chain)
+    alpha = sp.Symbol(Rf"\alpha_{{{decay_chain.resonance.name}}}")
+    parameter_defaults[alpha] = sp.Rational(0)
+    s = get_mandelstam_s(decay_chain.decay_node)
+    m0, m1 = sp.symbols("m0 m1", nonnegative=True)
+    q = Q(s, m0, m1)
+    expr *= sp.exp(-alpha * q**2)
+    return expr, parameter_defaults
+
+
+def formulate_flatte_1405(  # noqa: PLR0914
+    decay_chain: ThreeBodyDecayChain,
+) -> tuple[FlattéSWave, dict[sp.Symbol, complex | float]]:
+    s = get_mandelstam_s(decay_chain.decay_node)
+    resonance = decay_chain.resonance
+    p1, p2 = decay_chain.decay_products
+    m1, m2 = _create_decay_product_masses(decay_chain)
+    m_res = sp.Symbol(f"m_{{{resonance.name}}}")
+    Γ1 = sp.Symbol(Rf"\Gamma_{{{resonance.name} \to {p1.latex} {p2.latex}}}")
+    Γ2 = sp.Symbol(Rf"\Gamma_{{{resonance.name} \to {π.latex} {Σ.latex}}}")
+    m_top = sp.Symbol(f"m{decay_chain.parent.index}", nonnegative=True)
+    m_spec = create_mass_symbol(decay_chain.spectator)
+    mπ = create_mass_symbol(π)
+    mΣ = create_mass_symbol(Σ)
+    parameter_defaults: dict[sp.Symbol, complex | float] = {
+        m_res: resonance.mass,
+        Γ1: resonance.width,
+        Γ2: resonance.width,
+        m1: decay_chain.decay_products[0].mass,
+        m2: decay_chain.decay_products[1].mass,
+        m_top: decay_chain.parent.mass,
+        m_spec: decay_chain.spectator.mass,
+        mπ: π.mass,
+        mΣ: Σ.mass,
+    }
+    l_prod = _get_angular_momentum(decay_chain.production_node)
+    R_prod = sp.Symbol(R"R_{\Lambda_c}")
+    q = Q(s, m_top, m_spec)
+    q0 = Q(m_res**2, m_top, m_spec)
+    dynamics = sp.Mul(
+        (q / q0) ** l_prod,
+        BlattWeisskopf(q * R_prod, l_prod) / BlattWeisskopf(q0 * R_prod, l_prod),
+        FlattéSWave(s, m_res, (Γ1, Γ2), (m1, m2), (mπ, mΣ)),
+    )
+    return dynamics, parameter_defaults
+
+
+def formulate_breit_wigner(
+    decay_chain: ThreeBodyDecayChain,
+) -> tuple[BreitWignerMinL, dict[sp.Symbol, complex | float]]:
+    s = get_mandelstam_s(decay_chain.decay_node)
+    m1, m2 = _create_decay_product_masses(decay_chain)
+    l_dec = _get_angular_momentum(decay_chain.decay_node)
+    l_prod = _get_angular_momentum(decay_chain.production_node)
+    parent_mass = sp.Symbol(f"m_{{{decay_chain.parent.name}}}")
+    spectator_mass = sp.Symbol(f"m_{{{decay_chain.spectator.name}}}")
+    resonance_mass = sp.Symbol(f"m_{{{decay_chain.resonance.name}}}")
+    resonance_width = sp.Symbol(Rf"\Gamma_{{{decay_chain.resonance.name}}}")
+    R_dec = sp.Symbol(R"R_\mathrm{res}")
+    R_prod = sp.Symbol(R"R_{\Lambda_c}")
+    parameter_defaults: dict[sp.Symbol, complex | float] = {
+        parent_mass: decay_chain.parent.mass,
+        spectator_mass: decay_chain.spectator.mass,
+        resonance_mass: decay_chain.resonance.mass,
+        resonance_width: decay_chain.resonance.width,
+        m1: decay_chain.decay_products[0].mass,
+        m2: decay_chain.decay_products[1].mass,
+        # https://github.com/ComPWA/polarimetry/pull/11#issuecomment-1128784376
+        R_dec: 1.5,
+        R_prod: 5,
+    }
+    dynamics = BreitWignerMinL(
+        s,
+        parent_mass,
+        spectator_mass,
+        resonance_mass,
+        resonance_width,
+        m1,
+        m2,
+        l_dec,
+        l_prod,
+        R_dec,
+        R_prod,
+    )
+    return dynamics, parameter_defaults
+
+
+def _create_decay_product_masses(
+    decay_chain: ThreeBodyDecayChain,
+) -> tuple[sp.Symbol, sp.Symbol]:
+    mi, mj = (
+        sp.Symbol(f"m{s.index}", nonnegative=True) for s in decay_chain.decay_products
+    )
+    return mi, mj
---
+++ packages/polarimetry-lc2pkpi/polarimetry_lc2pkpi-0.0.12.tar.gz/polarimetry_lc2pkpi-0.0.12/tests/test_function.py
@@ -0,0 +1,105 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import numpy as np
+import pytest
+import sympy as sp
+from ampform_dpd.io import cached
+
+from polarimetry import lhcb
+from polarimetry.data import create_data_transformer, generate_phasespace_sample
+from polarimetry.function import (
+    integrate_intensity,
+    interference_intensity,
+    sub_intensity,
+)
+from polarimetry.lhcb import load_model
+from polarimetry.lhcb.particle import load_particles
+
+if TYPE_CHECKING:
+    from ampform_dpd import AmplitudeModel
+    from tensorwaves.interface import DataSample, ParametrizedFunction
+
+
+@pytest.fixture(scope="session")
+def model() -> AmplitudeModel:
+    data_dir = Path(lhcb.__file__).parent
+    particles = load_particles(data_dir / "particle-definitions.yaml")
+    return load_model(data_dir / "model-definitions.yaml", particles, model_id=0)
+
+
+@pytest.fixture(scope="session")
+def intensity_func(model: AmplitudeModel) -> ParametrizedFunction:
+    unfolded_intensity_expr = cached.unfold(model)
+    free_parameters = {
+        symbol: value
+        for symbol, value in model.parameter_defaults.items()
+        if isinstance(symbol, sp.Indexed)
+        if "production" in str(symbol)
+    }
+    fixed_parameters = {
+        symbol: value
+        for symbol, value in model.parameter_defaults.items()
+        if symbol not in free_parameters
+    }
+    subs_intensity_expr = cached.xreplace(unfolded_intensity_expr, fixed_parameters)
+    return cached.lambdify(
+        subs_intensity_expr,
+        parameters=free_parameters,  # type:ignore[arg-type]
+        backend="jax",
+    )
+
+
+@pytest.fixture(scope="session")
+def phsp(model: AmplitudeModel) -> DataSample:
+    transformer = create_data_transformer(model)
+    phsp = generate_phasespace_sample(model.decay, n_events=100_000, seed=0)
+    return transformer(phsp)
+
+
+@pytest.mark.slow
+def test_interference_intensity(intensity_func: ParametrizedFunction, phsp: DataSample):
+    K = "K(1430)"
+    L = R"\Lambda(1405)"
+
+    I_KL = interference_intensity(intensity_func, phsp, [K], [L])
+    I_LK = interference_intensity(intensity_func, phsp, [L], [K])
+    I_KK = interference_intensity(intensity_func, phsp, [K], [K])
+    I_LL = interference_intensity(intensity_func, phsp, [L], [L])
+    I_tot = integrate_intensity(intensity_func(phsp))
+
+    assert pytest.approx(I_LK) == I_KL
+    decay_rates_array = [
+        I_KK / I_tot,
+        I_KL / I_tot,
+        I_LL / I_tot,
+    ]
+    decay_rates = [float(v) for v in decay_rates_array]
+    assert pytest.approx(decay_rates) == [
+        -0.147005113,
+        +0.047790508,
+        -0.077789273,
+    ]
+
+
+@pytest.mark.slow
+def test_sub_intensity_all(intensity_func: ParametrizedFunction, phsp: DataSample):
+    I_tot = integrate_intensity(intensity_func(phsp))
+    np.testing.assert_allclose(
+        I_tot,
+        sub_intensity(intensity_func, phsp, ["K", "L", "D"]),
+    )
+
+
+@pytest.mark.slow
+def test_total_intensity(intensity_func: ParametrizedFunction, phsp: DataSample):
+    I_K = sub_intensity(intensity_func, phsp, non_zero_couplings=["K"])
+    I_Λ = sub_intensity(intensity_func, phsp, non_zero_couplings=["L"])
+    I_Δ = sub_intensity(intensity_func, phsp, non_zero_couplings=["D"])
+    I_ΛΔ = interference_intensity(intensity_func, phsp, ["L"], ["D"])
+    I_KΔ = interference_intensity(intensity_func, phsp, ["K"], ["D"])
+    I_KΛ = interference_intensity(intensity_func, phsp, ["K"], ["L"])
+    I_tot = integrate_intensity(intensity_func(phsp))
+    np.testing.assert_allclose(I_tot, I_K + I_Λ + I_Δ + I_ΛΔ + I_KΔ + I_KΛ)
