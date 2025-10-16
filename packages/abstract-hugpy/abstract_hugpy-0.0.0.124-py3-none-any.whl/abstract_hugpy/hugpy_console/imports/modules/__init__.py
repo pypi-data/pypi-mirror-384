cb39c872ba21e34455c1782af7c2ad1aa0486729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 16:13:47 +0000

    Add package abstract-hugpy/0.0.0.124/abstract_hugpy-0.0.0.124-py3-none-any.whl

---
+++ packages/abstract-hugpy/abstract_hugpy-0.0.0.124-py3-none-any.whl/abstract_hugpy/hugpy_console/imports/modules/__init__.py
@@ -0,0 +1,15 @@
+from .bigbirdManager import *
+from .flanManager import *
+from .keybertManager import *
+from .summarizerManager import *
+from .whisperManager import *
+from .zerosearchManager import *
+bp_list = [
+    hugpy_deepcoder_bp,
+    whisper_bp,
+    summarizer_bp,
+    keybert_bp,
+    flan_summarizer_bp,
+    summarizer_bp,
+           ]
+
