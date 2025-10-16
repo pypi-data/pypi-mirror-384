cce1554d43ea253d39e9d15ed5904a3c92e970d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 18:05:54 +0000

    Add package KratosMappingApplication/10.3.2/kratosmappingapplication-10.3.2-cp312-cp312-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/KratosMappingApplication/kratosmappingapplication-10.3.2-cp312-cp312-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/KratosMultiphysics/MappingApplication/MappingIntersectionUtilities.pyi
@@ -0,0 +1,6 @@
+import Kratos
+
+def CreateQuadraturePointsCoupling1DGeometries2D(arg0: Kratos.ModelPart, arg1: float) -> None:
+    """CreateQuadraturePointsCoupling1DGeometries2D(arg0: Kratos.ModelPart, arg1: float) -> None"""
+def FindIntersection1DGeometries2D(arg0: Kratos.ModelPart, arg1: Kratos.ModelPart, arg2: Kratos.ModelPart, arg3: float) -> None:
+    """FindIntersection1DGeometries2D(arg0: Kratos.ModelPart, arg1: Kratos.ModelPart, arg2: Kratos.ModelPart, arg3: float) -> None"""
