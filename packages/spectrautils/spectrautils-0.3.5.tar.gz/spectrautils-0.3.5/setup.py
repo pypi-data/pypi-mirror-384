793041707895f42c7eba771241cb388222422a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 18:05:47 +0000

    Add package spectrautils/0.3.5/spectrautils-0.3.5.tar.gz

---
+++ packages/spectrautils/spectrautils-0.3.5.tar.gz/spectrautils-0.3.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+    
+setup(
+    # readme file
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    # package information
+    
+    name="spectrautils",
+    version="0.3.5",
+    packages=find_packages(),
+    description="A powerful tools for python",
+    author="bruce_cui",
+    author_email="summer56567@163.com",
+    install_requires=[
+        # 依赖列表
+        "termcolor >= 2.3.0",
+        "colorama >= 0.4.4",
+        "onnxruntime-gpu >= 1.18.0",
+        "onnx >= 1.16.1",
+        "tqdm >= 4.67.1",
+        "pandas >= 1.5.3",
+        "holoviews >= 1.17.1",
+        "hvplot >= 0.10.0"
+    ],
+)
---
+++ packages/spectrautils/spectrautils-0.3.5.tar.gz/spectrautils-0.3.5/test/test_onnx.py
@@ -0,0 +1,44 @@
+import os, sys
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_root)
+
+import torch
+from torchvision import models
+from  torchvision.models import ResNet18_Weights
+from spectrautils.onnx_utils import visualize_onnx_model_weights,visualize_torch_model_weights
+from spectrautils.onnx_utils import export_model_onnx, visualize_onnx_model_weights
+
+if __name__ == '__main__':
+    
+    # 加载onnx模型
+    # onnx_path = "/home/bruce_ultra/workspace/perception_quanti/avp_parkspace/20240603/psd2d_v1_1_0_8650_eca_simplifier.onnx"
+    # model_name = "psd2d"
+    # visualize_onnx_model_weights(onnx_path, model_name)
+    
+    # exit(0)
+    
+    # 加载torch模型
+    # model_new = torch.load('/home/bruce_ultra/workspace/quant_workspace/Quantizer-Tools/_outputs/models/resnet18-f37072fd.pth')
+    model_new = models.resnet18(pretrained=True)
+    visualize_torch_model_weights(model_new, "resnet18_new")
+    
+    # exit(0)
+    # 加载预训练的ResNet18模型
+    model = models.resnet18(pretrained=True)
+    
+    # 定义输入信息
+    input_info = {'input': (1, 3, 224, 224)}  # ResNet18 期望的输入尺寸
+    
+    # 定义输出名称
+    output_names = ['output']
+    export_model_onnx(model, 
+                      input_info,
+                      "./resnet18.onnx",
+                      output_names
+                    )
+    
+    
+    
+
+    
+    
