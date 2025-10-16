87a7766f0fa8284e6e0950613bfbe6b0ae70a2f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 04:05:06 +0000

    Add package web-scraper-vyas-yogesh-v01/0.1.2/web_scraper_vyas_yogesh_v01-0.1.2-py3-none-any.whl

---
+++ packages/web-scraper-vyas-yogesh-v01/web_scraper_vyas_yogesh_v01-0.1.2-py3-none-any.whl/web_scraper/__init__.py
@@ -0,0 +1,7 @@
+# src/web_auto_pkg/__init__.py
+
+__version__ = "0.1.0"
+
+# आप यहां से मुख्य फ़ंक्शन (main function) भी इम्पोर्ट कर सकते हैं 
+# ताकि यूज़र सीधे 'from web_auto_pkg import fetch_top_website_data' लिख सके।
+from .scraper import fetch_top_website_data
