21cf82b7542ea9d66915a88a6177956ad67e620f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 05:06:45 +0000

    Add package magnum-cluster-api/0.33.0/magnum_cluster_api-0.33.0.tar.gz

---
+++ packages/magnum-cluster-api/magnum_cluster_api-0.33.0.tar.gz/magnum_cluster_api-0.33.0/src/cluster_api/mod.rs
@@ -0,0 +1,10 @@
+#![allow(clippy::all)]
+pub mod clusterclasses;
+pub mod clusterresourcesets;
+pub mod clusters;
+pub mod kubeadmconfigtemplates;
+pub mod kubeadmcontrolplane;
+pub mod kubeadmcontrolplanetemplates;
+pub mod machines;
+pub mod openstackclustertemplates;
+pub mod openstackmachinetemplates;
