a0f04d12e8168461d0fa94bf468fe060c3e72a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 04:05:54 +0000

    Add package wnetalign/0.9.3/wnetalign-0.9.3.tar.gz

---
+++ packages/wnetalign/wnetalign-0.9.3.tar.gz/wnetalign-0.9.3/src/wnetalign/__init__.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .aligner import WNetAligner
+from .spectrum import Spectrum, Spectrum_1D
+
+def py_hello():
+    print("Hello, World from WNetAlign (Python)!")
