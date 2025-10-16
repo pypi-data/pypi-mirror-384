34b6238df77a697c712e0cc5cb6a40c4d926761b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 18:06:39 +0000

    Add package asynctor/0.10.0/asynctor-0.10.0-py3-none-any.whl

---
+++ packages/asynctor/asynctor-0.10.0-py3-none-any.whl/asynctor/__init__.py
@@ -0,0 +1,23 @@
+from .aio import bulk_gather, gather, map_group, run, run_async, start_tasks, wait_for
+from .client import AsyncRedis
+from .timing import timeit
+from .utils import AsyncClientGenerator, AsyncTestClient, AttrDict, Shell, cache_attr
+
+__version__ = "0.10.0"
+__all__ = (
+    "__version__",
+    "AsyncClientGenerator",
+    "AsyncRedis",
+    "AsyncTestClient",
+    "AttrDict",
+    "Shell",
+    "bulk_gather",
+    "cache_attr",
+    "gather",
+    "map_group",
+    "run",
+    "run_async",
+    "start_tasks",
+    "timeit",
+    "wait_for",
+)
