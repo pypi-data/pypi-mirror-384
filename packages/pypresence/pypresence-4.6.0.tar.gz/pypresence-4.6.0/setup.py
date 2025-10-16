f9fdf3b202765ac8391f4da593a335e3b53491a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 06:04:57 +0000

    Add package pypresence/4.6.0/pypresence-4.6.0.tar.gz

---
+++ packages/pypresence/pypresence-4.6.0.tar.gz/pypresence-4.6.0/setup.py
@@ -0,0 +1,18 @@
+"""
+Legacy setup.py for backward compatibility.
+All configuration has been migrated to pyproject.toml.
+This file is kept for compatibility with older tools.
+"""
+
+from setuptools import setup
+
+# All configuration is now in pyproject.toml
+setup()
+
+print(
+    r"""
+___  _   _ ___  ____ ____ ____ ____ _  _ ____ ____
+|__]  \_/  |__] |__/ |___ [__  |___ |\ | |    |___
+|      |   |    |  \ |___ ___] |___ | \| |___ |___
+"""
+)
