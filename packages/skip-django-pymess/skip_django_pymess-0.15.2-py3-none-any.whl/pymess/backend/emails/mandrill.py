3ed42a1281394084fbbcd3736cb79f89c4c6c909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 03:19:36 +0000

    Add package skip-django-pymess/0.15.2/skip_django_pymess-0.15.2-py3-none-any.whl

---
+++ packages/skip-django-pymess/skip_django_pymess-0.15.2-py3-none-any.whl/pymess/backend/emails/mandrill.py
@@ -0,0 +1,136 @@
+import os
+import base64
+
+import requests
+
+from json.decoder import JSONDecodeError
+
+from enum import Enum
+
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext
+
+import mandrill
+
+from pymess.backend.emails import EmailBackend
+from pymess.enums import EmailMessageState
+from pymess.config import settings
+from pymess.utils.logged_requests import generate_session
+
+
+class MandrillState(str, Enum):
+
+    SENT = 'SENT'
+    QUEUED = 'QUEUED'
+    SCHEDULED = 'SCHEDULED'
+    REJECTED = 'REJECTED'
+    INVALID = 'INVALID'
+
+
+class MandrillEmailBackend(EmailBackend):
+    """
+    E-mail backend implementing Mandrill service (https://mandrillapp.com/api/docs/index.python.html).
+    """
+
+    MANDRILL_STATES_MAPPING = {
+        MandrillState.SENT: EmailMessageState.SENT,
+        MandrillState.QUEUED: EmailMessageState.SENT,
+        MandrillState.SCHEDULED: EmailMessageState.SENT,
+        MandrillState.REJECTED: EmailMessageState.ERROR,
+        MandrillState.INVALID: EmailMessageState.ERROR,
+    }
+
+    config = {
+        'HEADERS': None,
+        'TRACK_OPENS': False,
+        'TRACK_CLICKS': False,
+        'AUTO_TEXT': False,
+        'INLINE_CSS': False,
+        'URL_STRIP_QS': False,
+        'PRESERVE_RECIPIENTS': False,
+        'VIEW_CONTENT_LINK': True,
+        'ASYNC': False,
+        'TIMEOUT': 5,  # 5s
+    }
+
+    def _serialize_attachments(self, message):
+        return [
+            {
+                'type': attachment.content_type,
+                'name': attachment.filename or os.path.basename(attachment.file.name),
+                'content': base64.b64encode(attachment.file.read()).decode('utf-8')
+            } for attachment in message.attachments.all()
+        ]
+
+    def _create_client(self, message):
+        mandrill_client = mandrill.Mandrill(self.config['KEY'])
+        mandrill_client.session = generate_session(
+            slug='pymess - Mandrill',
+            related_objects=(message,),
+            timeout=self.config['TIMEOUT']
+        )
+        return mandrill_client
+
+    def publish_message(self, message):
+        mandrill_client = self._create_client(message)
+        try:
+            result = mandrill_client.messages.send(
+                message={
+                    'to': [{'email': message.recipient}],
+                    'from_email': message.sender,
+                    'from_name': message.sender_name,
+                    'html': message.content,
+                    'subject': message.subject,
+                    'headers': self.config['HEADERS'],
+                    'track_opens': self.config['TRACK_OPENS'],
+                    'auto_text': self.config['AUTO_TEXT'],
+                    'inline_css': self.config['INLINE_CSS'],
+                    'url_strip_qs': self.config['URL_STRIP_QS'],
+                    'preserve_recipients': self.config['PRESERVE_RECIPIENTS'],
+                    'view_content_link': self.config['VIEW_CONTENT_LINK'],
+                    'async': self.config['ASYNC'],
+                    'attachments': self._serialize_attachments(message)
+                },
+            )[0]
+            mandrill_state = MandrillState(result['status'].upper())
+            state = self.MANDRILL_STATES_MAPPING.get(mandrill_state)
+            error = None
+            if mandrill_state == MandrillState.INVALID:
+                error = gettext('invalid')
+            elif mandrill_state == MandrillState.REJECTED:
+                error = gettext('rejected, mandrill message: "{}"').format(result['reject_reason'])
+
+            extra_sender_data = message.extra_sender_data or {}
+            extra_sender_data['result'] = result
+            self._update_message_after_sending(
+                message,
+                state=state,
+                sent_at=timezone.now(),
+                extra_sender_data=extra_sender_data,
+                error=error,
+                external_id=result.get('_id')
+            )
+        except (mandrill.Error, JSONDecodeError, requests.exceptions.RequestException) as ex:
+            self._update_message_after_sending_error(
+                message,
+                error=str(ex)
+            )
+            # Do not re-raise caught exception. Re-raise exception causes transaction rollback (lost of information
+            # about exception).
+
+    def pull_message_info(self, message):
+        if message.external_id:
+            mandrill_client = self._create_client(message)
+            try:
+                info = mandrill_client.messages.info(message.external_id)
+                self._update_message(
+                    message,
+                    extra_sender_data={**message.extra_sender_data, 'info': info},
+                    info_changed_at=timezone.now(),
+                    update_only_changed_fields=True,
+                )
+            except mandrill.UnknownMessageError:
+                self._update_message(message, info_changed_at=timezone.now(), update_only_changed_fields=True)
+        else:
+            self._update_message(message, info_changed_at=timezone.now(), update_only_changed_fields=True)
---
+++ packages/skip-django-pymess/skip_django_pymess-0.15.2-py3-none-any.whl/pymess/models/dialer.py
@@ -0,0 +1,106 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils.encoding import force_str
+from django.utils.translation import gettext_lazy as _
+
+from pymess.config import settings
+from pymess.enums import DialerMessageState
+from pymess.utils import normalize_phone_number
+
+from .common import BaseAbstractTemplate, BaseMessage, BaseRelatedObject
+
+
+__all__ = (
+    'AbstractDialerMessage',
+    'AbstractDialerTemplate',
+    'DialerMessage',
+    'DialerMessageRelatedObject',
+    'DialerTemplate',
+    'DialerTemplateDisallowedObject',
+)
+
+
+class AbstractDialerMessage(BaseMessage):
+
+    State = DialerMessageState
+
+    template = models.ForeignKey(settings.DIALER_TEMPLATE_MODEL, verbose_name=_('template'), blank=True, null=True,
+                                 on_delete=models.SET_NULL, related_name='dialer_messages')
+    state = models.IntegerField(verbose_name=_('state'), null=False, blank=False,
+                                choices=DialerMessageState.choices, editable=False,
+                                db_index=True)
+    is_autodialer = models.BooleanField(verbose_name=_('is autodialer'), null=False, default=True)
+    number_of_status_check_attempts = models.PositiveIntegerField(verbose_name=_('number of status check attempts'),
+                                                                  null=False, blank=False, default=0)
+    content = models.TextField(verbose_name=_('content'), null=True, blank=True)
+
+    class Meta(BaseMessage.Meta):
+        abstract = True
+        verbose_name = _('dialer message')
+        verbose_name_plural = _('dialer messages')
+
+    def clean(self):
+        if self.is_autodialer and not self.content:
+            raise ValidationError(_('Autodialer message must contain content.'))
+        super().clean()
+
+    def clean_recipient(self):
+        self.recipient = normalize_phone_number(force_str(self.recipient))
+
+    @property
+    def failed(self):
+        return self.state in {DialerMessageState.ERROR, DialerMessageState.ERROR_RETRY}
+
+
+class DialerMessage(AbstractDialerMessage):
+
+    is_final_state = models.BooleanField(verbose_name=_('is final state'), null=False, default=False)
+
+    def __str__(self):
+        return '{recipient}, {template_slug}, {state}'.format(
+            recipient=self.recipient, template_slug=self.template_slug, state=self.get_state_display(),
+        )
+
+
+class DialerMessageRelatedObject(BaseRelatedObject):
+    dialer_message = models.ForeignKey(DialerMessage, verbose_name=_('dialer message'), null=False,
+                                       blank=False, on_delete=models.CASCADE, related_name='related_objects')
+
+    class Meta(BaseRelatedObject.Meta):
+        verbose_name = _('related object of a dialer message')
+        verbose_name_plural = _('related objects of dialer messages')
+
+
+class AbstractDialerTemplate(BaseAbstractTemplate):
+
+    def get_controller(self):
+        from pymess.backend.dialer import DialerController
+        return DialerController()
+
+    def send(self, recipient, context_data, related_objects=None, tag=None, **kwargs):
+        return super().send(recipient, context_data, related_objects, tag, **kwargs)
+
+    class Meta(BaseAbstractTemplate.Meta):
+        abstract = True
+        verbose_name = _('dialer template')
+        verbose_name_plural = _('dialer templates')
+
+
+class DialerTemplate(AbstractDialerTemplate):
+    pass
+
+
+class DialerTemplateDisallowedObject(BaseRelatedObject):
+
+    template = models.ForeignKey(
+        verbose_name=_('template'),
+        to=DialerTemplate,
+        null=True, blank=True,
+        on_delete=models.CASCADE,
+        related_name='disallowed_objects',
+        db_index=True
+    )
+
+    class Meta(BaseRelatedObject.Meta):
+        verbose_name = _('disallowed object of a dialer template')
+        verbose_name_plural = _('disallowed objects of dialer templates')
