64d105a5b1bd9c823525dc78c7c7e6547831d8de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 08:06:15 +0000

    Add package computemate/0.1.90/computemate-0.1.90-py3-none-any.whl

---
+++ packages/computemate/computemate-0.1.90-py3-none-any.whl/computemate/mcp_configurations.py
@@ -0,0 +1,10 @@
+{
+    # Check configuration format at https://gofastmcp.com/clients/client#configuration-format
+    # Example - Youtube Utilities; uncomment the following line to use it
+    #"youtube": {"command": "python", "args": [os.path.join(COMPUTEMATE_PACKAGE_PATH, "mcp", "youtube.py")]},
+    # Example - GitHub MCP; uncomment the following 4 lines to use it
+    #"github": {
+    #    "url": "https://api.githubcopilot.com/mcp",
+    #    "headers": {"Authorization": os.getenv("GITHUB_TOKEN")},
+    #},
+}
