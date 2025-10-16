0f95009c7a7c1b40aa65dff7d33c86b011c5e515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 05:06:40 +0000

    Add package otree/6.0.0b16/otree-6.0.0b16.tar.gz

---
+++ packages/otree/otree-6.0.0b16.tar.gz/otree-6.0.0b16/otree/static/otree/js/common_admin.js
@@ -0,0 +1,19 @@
+function makeReconnectingWebSocket(path) {
+    // https://github.com/pladaria/reconnecting-websocket/issues/91#issuecomment-431244323
+    var ws_scheme = window.location.protocol === "https:" ? "wss" : "ws";
+    var ws_path = ws_scheme + '://' + window.location.host + path;
+    var socket = new ReconnectingWebSocket(ws_path);
+    socket.onclose = function (e) {
+        if (e.code === 1011) {
+            // this may or may not exist in child pages.
+            var serverErrorDiv = document.getElementById("websocket-server-error");
+            if (serverErrorDiv) {
+                // better to put the message here rather than the div, otherwise it's confusing when
+                // you do "view source" and there's an error message.
+                serverErrorDiv.innerText = "Server error. Check the server logs or Sentry.";
+                serverErrorDiv.style.visibility = "visible";
+            }
+        }
+    };
+    return socket;
+}
