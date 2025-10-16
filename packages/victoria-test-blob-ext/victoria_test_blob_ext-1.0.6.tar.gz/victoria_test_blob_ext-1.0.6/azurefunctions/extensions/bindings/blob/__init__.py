fe7295ee50b54cdd89b8c5820c2037f62f8242bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 07:05:18 +0000

    Add package victoria-test-blob-ext/1.0.6/victoria_test_blob_ext-1.0.6.tar.gz

---
+++ packages/victoria-test-blob-ext/victoria_test_blob_ext-1.0.6.tar.gz/victoria_test_blob_ext-1.0.6/azurefunctions/extensions/bindings/blob/__init__.py
@@ -0,0 +1,16 @@
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  Licensed under the MIT License.
+
+from .blobClient import BlobClient
+from .blobClientConverter import BlobClientConverter
+from .containerClient import ContainerClient
+from .storageStreamDownloader import StorageStreamDownloader
+
+__all__ = [
+    "BlobClient",
+    "ContainerClient",
+    "StorageStreamDownloader",
+    "BlobClientConverter",
+]
+
+__version__ = '1.0.6'
