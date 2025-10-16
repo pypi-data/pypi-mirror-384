0f07e05f1d99584b8f227bfd88361d8be2fd510c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 06:04:58 +0000

    Add package mqt-debugger/1.2.0/mqt_debugger-1.2.0-cp314-cp314t-win_arm64.whl

---
+++ packages/mqt-debugger/mqt_debugger-1.2.0-cp314-cp314t-win_arm64.whl/mqt/debugger/__init__.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2024 - 2025 Chair for Design Automation, TUM
+# Copyright (c) 2025 Munich Quantum Software Company GmbH
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+"""A module for interfacing with the mqt-debugger library."""
+
+from __future__ import annotations
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_11_2():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'mqt_debugger.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_11_2()
+del _delvewheel_patch_1_11_2
+# end delvewheel patch
+
+from . import check, dap
+from ._version import version as __version__
+from .pydebugger import (
+    CompilationSettings,
+    Complex,
+    Diagnostics,
+    ErrorCause,
+    ErrorCauseType,
+    SimulationState,
+    Statevector,
+    Variable,
+    VariableType,
+    VariableValue,
+    create_ddsim_simulation_state,
+    destroy_ddsim_simulation_state,
+)
+
+__all__ = [
+    "CompilationSettings",
+    "Complex",
+    "Diagnostics",
+    "ErrorCause",
+    "ErrorCauseType",
+    "SimulationState",
+    "Statevector",
+    "Variable",
+    "VariableType",
+    "VariableValue",
+    "__version__",
+    "check",
+    "create_ddsim_simulation_state",
+    "create_ddsim_simulation_state",
+    "dap",
+    "destroy_ddsim_simulation_state",
+]
