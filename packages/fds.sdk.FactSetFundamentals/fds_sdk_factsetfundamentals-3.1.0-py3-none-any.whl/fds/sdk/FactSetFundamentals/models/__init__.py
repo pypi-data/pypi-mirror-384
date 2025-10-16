5cb33bd3ddd4e4b1e11067cfc8511cc0d3482bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 14:07:05 +0000

    Add package fds.sdk.FactSetFundamentals/3.1.0/fds_sdk_factsetfundamentals-3.1.0-py3-none-any.whl

---
+++ packages/fds.sdk.FactSetFundamentals/fds_sdk_factsetfundamentals-3.1.0-py3-none-any.whl/fds/sdk/FactSetFundamentals/models/__init__.py
@@ -0,0 +1,65 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from fds.sdk.FactSetFundamentals.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from fds.sdk.FactSetFundamentals.model.address import Address
+from fds.sdk.FactSetFundamentals.model.batch import Batch
+from fds.sdk.FactSetFundamentals.model.batch_error_object import BatchErrorObject
+from fds.sdk.FactSetFundamentals.model.batch_result import BatchResult
+from fds.sdk.FactSetFundamentals.model.batch_result_response import BatchResultResponse
+from fds.sdk.FactSetFundamentals.model.batch_status import BatchStatus
+from fds.sdk.FactSetFundamentals.model.batch_status_response import BatchStatusResponse
+from fds.sdk.FactSetFundamentals.model.company_fundamentals_response import CompanyFundamentalsResponse
+from fds.sdk.FactSetFundamentals.model.company_report_error_object import CompanyReportErrorObject
+from fds.sdk.FactSetFundamentals.model.dividend import Dividend
+from fds.sdk.FactSetFundamentals.model.error_object import ErrorObject
+from fds.sdk.FactSetFundamentals.model.error_object_links import ErrorObjectLinks
+from fds.sdk.FactSetFundamentals.model.error_response import ErrorResponse
+from fds.sdk.FactSetFundamentals.model.exchange import Exchange
+from fds.sdk.FactSetFundamentals.model.financial_response import FinancialResponse
+from fds.sdk.FactSetFundamentals.model.financials import Financials
+from fds.sdk.FactSetFundamentals.model.fiscal_period import FiscalPeriod
+from fds.sdk.FactSetFundamentals.model.frequency_enum import FrequencyEnum
+from fds.sdk.FactSetFundamentals.model.fundamental import Fundamental
+from fds.sdk.FactSetFundamentals.model.fundamental_fiscal_period import FundamentalFiscalPeriod
+from fds.sdk.FactSetFundamentals.model.fundamental_request_body import FundamentalRequestBody
+from fds.sdk.FactSetFundamentals.model.fundamental_value import FundamentalValue
+from fds.sdk.FactSetFundamentals.model.fundamentals import Fundamentals
+from fds.sdk.FactSetFundamentals.model.fundamentals_pit_data import FundamentalsPITData
+from fds.sdk.FactSetFundamentals.model.fundamentals_pit_request import FundamentalsPITRequest
+from fds.sdk.FactSetFundamentals.model.fundamentals_pit_request_body import FundamentalsPITRequestBody
+from fds.sdk.FactSetFundamentals.model.fundamentals_pit_response import FundamentalsPITResponse
+from fds.sdk.FactSetFundamentals.model.fundamentals_request import FundamentalsRequest
+from fds.sdk.FactSetFundamentals.model.fundamentals_response import FundamentalsResponse
+from fds.sdk.FactSetFundamentals.model.identifier_list import IdentifierList
+from fds.sdk.FactSetFundamentals.model.ids_batch_max30000 import IdsBatchMax30000
+from fds.sdk.FactSetFundamentals.model.item import Item
+from fds.sdk.FactSetFundamentals.model.metric import Metric
+from fds.sdk.FactSetFundamentals.model.metric_list import MetricList
+from fds.sdk.FactSetFundamentals.model.metrics import Metrics
+from fds.sdk.FactSetFundamentals.model.metrics_response import MetricsResponse
+from fds.sdk.FactSetFundamentals.model.per_share import PerShare
+from fds.sdk.FactSetFundamentals.model.period_info import PeriodInfo
+from fds.sdk.FactSetFundamentals.model.periodicity import Periodicity
+from fds.sdk.FactSetFundamentals.model.periodicity_enum import PeriodicityEnum
+from fds.sdk.FactSetFundamentals.model.periods_request import PeriodsRequest
+from fds.sdk.FactSetFundamentals.model.periods_request_body import PeriodsRequestBody
+from fds.sdk.FactSetFundamentals.model.periods_response import PeriodsResponse
+from fds.sdk.FactSetFundamentals.model.profile import Profile
+from fds.sdk.FactSetFundamentals.model.profile_response import ProfileResponse
+from fds.sdk.FactSetFundamentals.model.ratios import Ratios
+from fds.sdk.FactSetFundamentals.model.segment import Segment
+from fds.sdk.FactSetFundamentals.model.segment_request_body import SegmentRequestBody
+from fds.sdk.FactSetFundamentals.model.segment_type import SegmentType
+from fds.sdk.FactSetFundamentals.model.segment_value import SegmentValue
+from fds.sdk.FactSetFundamentals.model.segments_periodicity import SegmentsPeriodicity
+from fds.sdk.FactSetFundamentals.model.segments_request import SegmentsRequest
+from fds.sdk.FactSetFundamentals.model.segments_response import SegmentsResponse
+from fds.sdk.FactSetFundamentals.model.update_type import UpdateType
