a524c52b75c852d4db3936f2f3ca8ec0969c739d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 14:06:21 +0000

    Add package Maxmodule/0.0.1/maxmodule-0.0.1.tar.gz

---
+++ packages/Maxmodule/maxmodule-0.0.1.tar.gz/maxmodule-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Maxmodule",  # must be unique on PyPI
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple example Python module",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Max",
+    author_email="your-email@example.com",
+    url="https://github.com/yourusername/mymodule",  # optional
+    license="MIT",
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
