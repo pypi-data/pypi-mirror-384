92dd90e3f4987c2b7d6cf4f9ab14ec4ed17e51e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 07:05:25 +0000

    Add package singer-encodings/0.2.0/singer_encodings-0.2.0.tar.gz

---
+++ packages/singer-encodings/singer_encodings-0.2.0.tar.gz/singer_encodings-0.2.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import subprocess
+
+setup(name="singer-encodings",
+      version='0.2.0',
+      description="Singer.io encodings library",
+      author="Stitch",
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      url="http://singer.io",
+      install_requires=[
+          "singer-python",
+          "pyarrow==21.0.0",
+          "fastavro==1.12.0"
+      ],
+      extras_require={
+          "dev": ["pytest"]
+      },
+      packages=find_packages(),
+)
