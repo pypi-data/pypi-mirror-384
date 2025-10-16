33d36048d14413f2c8694441ee696ef069b019c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 07:05:45 +0000

    Add package robotic/0.3.4.dev1/robotic-0.3.4.dev1-cp311-cp311-manylinux2014_x86_64.whl

---
+++ packages/robotic/robotic-0.3.4.dev1-cp311-cp311-manylinux2014_x86_64.whl/robotic/include/rai/Optim/GlobalIterativeNewton.h
@@ -0,0 +1,32 @@
+/*  ------------------------------------------------------------------
+    Copyright (c) 2011-2024 Marc Toussaint
+    email: toussaint@tu-berlin.de
+
+    This code is distributed under the MIT License.
+    Please see <root-path>/LICENSE for details.
+    --------------------------------------------------------------  */
+
+#pragma once
+
+#include "newton.h"
+#include "gradient.h"
+
+struct GlobalIterativeNewton {
+  arr x;
+  OptNewton newton;
+  OptGrad grad;
+  arr bounds;
+
+  struct LocalMinimum { arr x; double fx; uint hits; };
+  rai::Array<LocalMinimum> localMinima;
+  LocalMinimum* best;
+
+  GlobalIterativeNewton(ScalarFunction& f, const arr& bounds, rai::OptOptions& opt);
+  ~GlobalIterativeNewton();
+
+  void step();
+  void run(uint maxIt=10);
+  void report();
+
+  void reOptimizeAllPoints();
+};
