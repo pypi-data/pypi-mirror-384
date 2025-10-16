99fbad71ca0eae8f7e82cb1f723883c842cdc418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 08:05:45 +0000

    Add package hackmudChatAPI/0.0.6/hackmudchatapi-0.0.6.tar.gz

---
+++ packages/hackmudChatAPI/hackmudchatapi-0.0.6.tar.gz/hackmudchatapi-0.0.6/hackmudChatAPI/__init__.py
@@ -0,0 +1,7 @@
+"""A python implementation of the hackmud chat API, using the requests module."""
+
+__version__ = "0.0.6"
+
+from hackmudChatAPI.hackmudChatAPI import ChatAPI
+
+__all__ = ["ChatAPI"]
