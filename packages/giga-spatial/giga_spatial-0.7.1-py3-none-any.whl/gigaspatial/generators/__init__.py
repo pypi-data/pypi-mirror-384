e555b80f5fa5042eb3f716d2214b66d121dee09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 03:20:38 +0000

    Add package giga-spatial/0.7.1/giga_spatial-0.7.1-py3-none-any.whl

---
+++ packages/giga-spatial/giga_spatial-0.7.1-py3-none-any.whl/gigaspatial/generators/__init__.py
@@ -0,0 +1,6 @@
+from gigaspatial.generators.poi import PoiViewGenerator, PoiViewGeneratorConfig
+from gigaspatial.generators.zonal import (
+    GeometryBasedZonalViewGenerator,
+    MercatorViewGenerator,
+    AdminBoundariesViewGenerator,
+)
