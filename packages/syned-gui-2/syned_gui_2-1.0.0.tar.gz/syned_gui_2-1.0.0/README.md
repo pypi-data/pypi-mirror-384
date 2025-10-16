b554f487c4620241400a7080f6b803d58afa294e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 05:05:50 +0000

    Add package syned-gui-2/1.0.0/syned_gui_2-1.0.0.tar.gz

---
+++ packages/syned-gui-2/syned_gui_2-1.0.0.tar.gz/syned_gui_2-1.0.0/README.md
@@ -0,0 +1,2 @@
+# syned-gui-2
+Generic graphic tools for OASYS2
---
+++ packages/syned-gui-2/syned_gui_2-1.0.0.tar.gz/syned_gui_2-1.0.0/setup.py
@@ -0,0 +1,86 @@
+#! /usr/bin/env python3
+
+import os
+
+try:
+    from setuptools import find_packages, setup
+except AttributeError:
+    from setuptools import find_packages, setup
+
+NAME = 'syned-gui-2'
+
+VERSION = '1.0.0'
+ISRELEASED = True
+
+DESCRIPTION = 'SYNED (SYNchrotron Elements Dictionary) gui library'
+README_FILE = os.path.join(os.path.dirname(__file__), 'README.md')
+LONG_DESCRIPTION = open(README_FILE).read()
+AUTHOR = 'Manuel Sanchez del Rio, Luca Rebuffi'
+AUTHOR_EMAIL = 'lrebuffi@anl.gov'
+URL = 'https://github.com/oasys-kit/syned-gui-2'
+DOWNLOAD_URL = 'https://github.com/oasys-kit/syned-gui-2'
+MAINTAINER = 'L Rebuffi and M Sanchez del Rio'
+MAINTAINER_EMAIL = 'lrebuffi@anl.gov'
+LICENSE = 'BSD'
+
+KEYWORDS = [
+    'dictionary',
+    'glossary',
+    'synchrotron'
+    'simulation',
+]
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'Environment :: Plugins',
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: '
+    'GNU General Public License v3 or later (GPLv3+)',
+    'Operating System :: POSIX',
+    'Operating System :: Microsoft :: Windows',
+    'Topic :: Scientific/Engineering :: Visualization',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Intended Audience :: Education',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Developers',
+]
+
+INSTALL_REQUIRES = (
+    'setuptools',
+    'oasys2>=0.0.1',
+)
+
+SETUP_REQUIRES = (
+    'setuptools',
+)
+
+PACKAGES = find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests'))
+PACKAGE_DATA = {}
+
+def setup_package():
+    setup(
+        name=NAME,
+        version=VERSION,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        url=URL,
+        download_url=DOWNLOAD_URL,
+        license=LICENSE,
+        keywords=KEYWORDS,
+        classifiers=CLASSIFIERS,
+        packages=PACKAGES,
+        package_data=PACKAGE_DATA,
+        # extra setuptools args
+        zip_safe=False,  # the package can run out of an .egg file
+        include_package_data=True,
+        install_requires=INSTALL_REQUIRES,
+        setup_requires=SETUP_REQUIRES,
+    )
+
+if __name__ == '__main__':
+    setup_package()
