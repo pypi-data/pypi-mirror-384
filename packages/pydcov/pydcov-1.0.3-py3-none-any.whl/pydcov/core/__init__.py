582cc854592f4e60394901d890a607c1a604c3ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 08:05:30 +0000

    Add package pydcov/1.0.3/pydcov-1.0.3-py3-none-any.whl

---
+++ packages/pydcov/pydcov-1.0.3-py3-none-any.whl/pydcov/core/__init__.py
@@ -0,0 +1,12 @@
+"""
+Core coverage management modules.
+
+This package contains the incremental coverage management class:
+- IncrementalCoverageManager: Incremental coverage tracking
+"""
+
+from .incremental_coverage import IncrementalCoverageManager
+
+__all__ = [
+    "IncrementalCoverageManager"
+]
