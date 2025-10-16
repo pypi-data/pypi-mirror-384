51ee996c0d9a152ee56e399ec5d17eae35bb37ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 16:14:13 +0000

    Add package openenergyid/0.1.30/openenergyid-0.1.30.tar.gz

---
+++ packages/openenergyid/openenergyid-0.1.30.tar.gz/openenergyid-0.1.30/openenergyid/__init__.py
@@ -0,0 +1,8 @@
+"""Open Energy ID Python SDK."""
+
+__version__ = "0.1.30"
+
+from .enums import Granularity
+from .models import TimeDataFrame, TimeSeries
+
+__all__ = ["Granularity", "TimeDataFrame", "TimeSeries"]
