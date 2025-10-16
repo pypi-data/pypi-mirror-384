8c3d004f59bf174d8cd0b619574a90c7c5ce57f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 18:06:27 +0000

    Add package KratosMultiphysics/10.3.2/kratosmultiphysics-10.3.2-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/KratosMultiphysics/kratosmultiphysics-10.3.2-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/KratosMultiphysics/CompareElementsAndConditionsUtility.pyi
@@ -0,0 +1,21 @@
+import Kratos
+from typing import overload
+
+@overload
+def GetRegisteredName(arg0: Kratos.Element) -> str:
+    """GetRegisteredName(*args, **kwargs)
+    Overloaded function.
+
+    1. GetRegisteredName(arg0: Kratos.Element) -> str
+
+    2. GetRegisteredName(arg0: Kratos.Condition) -> str
+    """
+@overload
+def GetRegisteredName(arg0: Kratos.Condition) -> str:
+    """GetRegisteredName(*args, **kwargs)
+    Overloaded function.
+
+    1. GetRegisteredName(arg0: Kratos.Element) -> str
+
+    2. GetRegisteredName(arg0: Kratos.Condition) -> str
+    """
