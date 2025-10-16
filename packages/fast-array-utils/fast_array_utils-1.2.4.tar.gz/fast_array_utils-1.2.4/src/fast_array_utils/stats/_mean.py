503d5c887f41273f07fcb6a3af00a50d6d97e7ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 05:06:24 +0000

    Add package fast-array-utils/1.2.4/fast_array_utils-1.2.4.tar.gz

---
+++ packages/fast-array-utils/fast_array_utils-1.2.4.tar.gz/fast_array_utils-1.2.4/src/fast_array_utils/stats/_mean.py
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: MPL-2.0
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import numpy as np
+
+from ._sum import sum_
+
+
+if TYPE_CHECKING:
+    from typing import Any, Literal
+
+    from numpy.typing import DTypeLike, NDArray
+
+    from .. import types
+    from ..typing import CpuArray, DiskArray, GpuArray
+
+
+def mean_(
+    x: CpuArray | GpuArray | DiskArray | types.DaskArray,
+    /,
+    *,
+    axis: Literal[0, 1] | None = None,
+    dtype: DTypeLike | None = None,
+) -> NDArray[np.number[Any]] | np.number[Any] | types.DaskArray:
+    total = sum_(x, axis=axis, dtype=dtype)
+    n = np.prod(x.shape) if axis is None else x.shape[axis]
+    return total / n  # type: ignore[call-overload,operator,return-value]
---
+++ packages/fast-array-utils/fast_array_utils-1.2.4.tar.gz/fast_array_utils-1.2.4/src/fast_array_utils/stats/_power.py
@@ -0,0 +1,41 @@
+# SPDX-License-Identifier: MPL-2.0
+from __future__ import annotations
+
+from functools import singledispatch
+from typing import TYPE_CHECKING
+
+from .. import types
+
+
+if TYPE_CHECKING:
+    from typing import TypeAlias, TypeVar
+
+    from fast_array_utils.typing import CpuArray, GpuArray
+
+    # All supported array types except for disk ones and CSDataset
+    Array: TypeAlias = CpuArray | GpuArray | types.DaskArray
+
+    _Arr = TypeVar("_Arr", bound=Array)
+
+
+def power(x: _Arr, n: int, /) -> _Arr:
+    """Take array or matrix to a power."""
+    # This wrapper is necessary because TypeVars can’t be used in `singledispatch` functions
+    return _power(x, n)  # type: ignore[return-value]
+
+
+@singledispatch
+def _power(x: Array, n: int, /) -> Array:
+    if TYPE_CHECKING:
+        assert not isinstance(x, types.DaskArray | types.CSMatrix)
+    return x**n  # type: ignore[operator]
+
+
+@_power.register(types.CSMatrix | types.CupyCSMatrix)
+def _power_cs(x: types.CSMatrix | types.CupyCSMatrix, n: int, /) -> types.CSMatrix | types.CupyCSMatrix:
+    return x.power(n)
+
+
+@_power.register(types.DaskArray)
+def _power_dask(x: types.DaskArray, n: int, /) -> types.DaskArray:
+    return x.map_blocks(lambda c: power(c, n))  # type: ignore[type-var]
