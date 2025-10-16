36684d19d093ef9574e7010df14c1554cd094069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 15:07:48 +0000

    Add package phg-vis/1.1.7/phg_vis-1.1.7.tar.gz

---
+++ packages/phg-vis/phg_vis-1.1.7.tar.gz/phg_vis-1.1.7/phg/visphg.py
@@ -0,0 +1,52 @@
+#################################
+# Launch vis.exe
+#################################
+import http.client
+import subprocess
+import os
+import psutil  # Make sure to import psutil
+
+# Set the path to vis.exe in the current directory of the current script
+current_directory = os.path.dirname(os.path.abspath(__file__))
+vis_exe_path = os.path.join(current_directory, "vis", "vis.exe")
+print(vis_exe_path)
+# Check if vis.exe is running
+def is_vis_running():
+    for proc in psutil.process_iter(['name']):
+        if proc.info['name'] == 'vis.exe':
+            return True
+    return False
+    
+# Send network message
+def HTTPPOST(ip, phg):
+    headers = {
+        "Connection": "keep-alive",
+        "Content-Type": "text/plain"
+    }
+    conn = http.client.HTTPConnection(f"{ip}:5088")
+    conn.request('POST', '/phg', phg, headers)
+    res = conn.getresponse()
+    conn.close()
+    
+# Call HTTPPOST function to send network message
+def vis(phg):
+    if not is_vis_running():  # Check if the process is running
+        subprocess.Popen(vis_exe_path)  # Start the process
+    return HTTPPOST('127.0.0.1', phg.encode())    
+    
+# Send network message
+def HTTPPOST_IMG(ip, phg):
+    headers = {
+        "Connection": "keep-alive",
+        "Content-Type": "text/plain"
+    }
+    conn = http.client.HTTPConnection(f"{ip}:5088")
+    conn.request('POST', '/phg_img', phg, headers)
+    res = conn.getresponse()
+    conn.close()
+    
+# Call HTTPPOST function to send network message
+def image(phg):
+    if not is_vis_running():  # Check if the process is running
+        subprocess.Popen(vis_exe_path)  # Start the process
+    return HTTPPOST_IMG('127.0.0.1', phg.encode())        
