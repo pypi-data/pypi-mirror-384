9a1b55678fab119a1d550505c7ae3445b0bfb9bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 07:05:59 +0000

    Add package xai-sdk/1.3.0/xai_sdk-1.3.0.tar.gz

---
+++ packages/xai-sdk/xai_sdk-1.3.0.tar.gz/xai_sdk-1.3.0/examples/sync/server_side_tools.py
@@ -0,0 +1,67 @@
+from xai_sdk import Client
+from xai_sdk.chat import user
+from xai_sdk.tools import code_execution, web_search, x_search
+
+
+def agentic_search(client: Client, model: str, query: str) -> None:
+    chat = client.chat.create(
+        model=model,
+        # All three tools are active, you can add/remove server-side tools as needed
+        tools=[
+            web_search(),
+            x_search(),
+            code_execution(),
+        ],
+    )
+    chat.append(user(query))
+
+    is_thinking = True
+    for response, chunk in chat.stream():
+        for tool_call in chunk.tool_calls:
+            print(f"\nCalling tool: {tool_call.function.name} with arguments: {tool_call.function.arguments}")
+        if response.usage.reasoning_tokens and is_thinking:
+            print(f"\rThinking... ({response.usage.reasoning_tokens} tokens)", end="", flush=True)
+        if chunk.content and is_thinking:
+            print("\n\nFinal Response:")
+            is_thinking = False
+        if chunk.content and not is_thinking:
+            print(chunk.content, end="", flush=True)
+
+    print("\n\nCitations:")
+    print(response.citations)
+    print("\n\nUsage:")
+    print(response.usage)
+    print(response.server_side_tool_usage)
+    print("\n\nTool Calls:")
+    print(response.tool_calls)
+
+
+def main() -> None:
+    client = Client()
+
+    # Trigger web/x search
+    agentic_search(
+        client,
+        model="grok-4-fast",
+        query=(
+            "What was the result of Arsenal's most recent game? Where did they play, who scored and in which minutes?"
+        ),
+    )
+
+    # Trigger code execution
+    # agentic_search(
+    #     client,
+    #     model="grok-4-fast",
+    #     query=("What is the 102nd number in the Fibonacci sequence?. show me the code"),
+    # )
+
+    # Trigger x search/web search
+    # agentic_search(
+    #     client,
+    #     model="grok-4-fast",
+    #     query="What can you tell me about the X user 0xPromar and his recent activity?",
+    # )
+
+
+if __name__ == "__main__":
+    main()
