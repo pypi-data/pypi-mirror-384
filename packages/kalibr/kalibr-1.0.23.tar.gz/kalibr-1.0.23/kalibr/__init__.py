b9da1a71a9ea36786a2fa094566116b841a7e90f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 06:05:32 +0000

    Add package kalibr/1.0.23/kalibr-1.0.23.tar.gz

---
+++ packages/kalibr/kalibr-1.0.23.tar.gz/kalibr-1.0.23/kalibr/__init__.py
@@ -0,0 +1,7 @@
+"""Kalibr SDK - Multi-Model AI Integration Framework"""
+
+from kalibr.kalibr import Kalibr
+from kalibr.kalibr_app import KalibrApp
+
+__version__ = "1.0.20"
+__all__ = ["Kalibr", "KalibrApp"]
