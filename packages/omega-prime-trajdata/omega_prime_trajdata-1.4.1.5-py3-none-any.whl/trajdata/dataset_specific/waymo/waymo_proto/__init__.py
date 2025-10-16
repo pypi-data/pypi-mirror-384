34f926d82200d08f6ca4879f6fbf16faf3631425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 16:13:39 +0000

    Add package omega-prime-trajdata/1.4.1.5/omega_prime_trajdata-1.4.1.5-py3-none-any.whl

---
+++ packages/omega-prime-trajdata/omega_prime_trajdata-1.4.1.5-py3-none-any.whl/trajdata/dataset_specific/waymo/waymo_proto/__init__.py
@@ -0,0 +1,4 @@
+from . import generated_stubs # noqa: F403
+from .generated_stubs.waymo import open_dataset as waymo
+
+__all__ = ['waymo']
---
+++ packages/omega-prime-trajdata/omega_prime_trajdata-1.4.1.5-py3-none-any.whl/trajdata/proto/__init__.py
@@ -0,0 +1,12 @@
+from .generated_stubs.trajdata import MapElement, PedCrosswalk, PedWalkway, Point, Polyline, RoadArea, RoadLane, VectorizedMap
+
+__all__ = (
+    "MapElement",
+    "PedCrosswalk",
+    "PedWalkway",
+    "Point",
+    "Polyline",
+    "RoadArea",
+    "RoadLane",
+    "VectorizedMap",
+)
