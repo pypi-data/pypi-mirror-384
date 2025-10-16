f0aabb3fbc098bb65f42e198b4cd593fee3a9142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 14:07:00 +0000

    Add package judgeval/0.16.9/judgeval-0.16.9.tar.gz

---
+++ packages/judgeval/judgeval-0.16.9.tar.gz/judgeval-0.16.9/src/judgeval/trainer/__init__.py
@@ -0,0 +1,5 @@
+from judgeval.trainer.trainer import JudgmentTrainer
+from judgeval.trainer.config import TrainerConfig, ModelConfig
+from judgeval.trainer.trainable_model import TrainableModel
+
+__all__ = ["JudgmentTrainer", "TrainerConfig", "ModelConfig", "TrainableModel"]
---
+++ packages/judgeval/judgeval-0.16.9.tar.gz/judgeval-0.16.9/src/judgeval/version.py
@@ -0,0 +1,5 @@
+__version__ = "0.16.9"
+
+
+def get_version() -> str:
+    return __version__
