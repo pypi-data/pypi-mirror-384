a99c77d7be936348c2db3461ea89c449fa276a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 15:08:02 +0000

    Add package fastcodedog/3.0.32/fastcodedog-3.0.32.tar.gz

---
+++ packages/fastcodedog/fastcodedog-3.0.32.tar.gz/fastcodedog-3.0.32/fastcodedog/generation/commonfiles/generation.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from fastcodedog.generation.commonfiles.utilfiles.case_converter import CaseConverter
+from fastcodedog.generation.commonfiles.utilfiles.log import Log
+
+
+def generate_common_files():
+    CaseConverter().save()
+    Log().save()
