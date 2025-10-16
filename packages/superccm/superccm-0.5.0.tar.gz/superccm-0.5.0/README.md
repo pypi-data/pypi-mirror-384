3f932eae2f45d9a379b40e590cfc2e5bcbaf3de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 15:07:08 +0000

    Add package superccm/0.5.0/superccm-0.5.0.tar.gz

---
+++ packages/superccm/superccm-0.5.0.tar.gz/superccm-0.5.0/README.md
@@ -0,0 +1,76 @@
+<div align="center">
+  <img src="docs/assets/superccm.png" alt="description" />
+
+<hr>
+
+English | [简体中文](./README_cn.md)
+
+</div>
+
+### *SuperCCM Version 0.5.0*
+
+## 🚀 Introduction
+
+✨️SuperCCM is an open-source Python framework for processing and analyzing corneal nerve images from corneal confocal microscopy (CCM).
+By providing a CCM corneal nerve image as input, SuperCCM can automatically process the image and output various clinically relevant morphological parameters.
+SuperCCM also allows fast and easy integration of independent algorithms (e.g., segmentation) into the framework.
+
+### 🎆 Github: [https://github.com/qlnfm/SuperCCM](https://github.com/qlnfm/SuperCCM)
+
+## 🔮 Use Online
+
+### 🤗 Hugging Face: [https://huggingface.co/spaces/jugking6688/SuperCCM-Web](https://huggingface.co/spaces/jugking6688/SuperCCM-Web)
+
+### 🏠 Our Website: [http://aiccm.fun/](http://aiccm.fun/)
+
+## ❇️ Environment Setup
+
+```shell
+conda create -n superccm python=3.10 -y
+conda activate superccm
+pip install -r requirements.txt
+```
+
+* Install from PyPI:
+
+```shell
+pip install superccm
+```
+
+## ⚡ Quick Start
+
+```python
+from superccm import DefaultWorkFlow
+
+wf = DefaultWorkFlow()
+metrics = wf.run('your/img/path')
+print(metrics)
+```
+
+Or a simpler, less formal version:
+
+```python
+from superccm.api import analysis
+
+metrics = analysis('your/img/path')
+print(metrics)
+```
+Or, enable Web service locally:
+```shell
+python app.py
+```
+
+## 📖 Documentation & Tutorials
+
+SuperCCM follows a principle of simplicity, allowing users and developers to get started and master it with minimal cost and time.
+
+* ✨️ [Quick Tutorial](docs/doc1.md): Learn how to use SuperCCM in detail
+* ✨️ [Module Development](docs/doc2.md): Learn how to customize workflows and integrate your own algorithms into SuperCCM
+
+## 📄 License
+
+This project is licensed under the [GPL v3](LICENSE) open-source license.
+
+## 🎓 Academic Citation
+
+> coming soon ...
---
+++ packages/superccm/superccm-0.5.0.tar.gz/superccm-0.5.0/superccm/impl/trunk/extract_trunk.py
@@ -0,0 +1,70 @@
+from superccm.impl.trunk.eval_path import analyze_curve_sharpness_windowed
+from superccm.impl.trunk.ep_path import get_paths
+import numpy as np
+from scipy.spatial.distance import euclidean
+from scipy.signal import medfilt
+
+from superccm.impl.utils.ccm_vignetting import vignetting_correction
+from superccm.impl.utils.histogram_matching import histogram_standardization
+from superccm.impl.utils.estimate_width import estimate_width
+from superccm.impl.utils.tools import get_canvas, cal_length, show_image
+
+
+def extract_trunk(
+        image: np.ndarray,
+        skeleton: np.ndarray,
+        *,
+        side_x=30,
+        min_distance=200,
+        min_intensity=90,
+) -> np.ndarray:
+    image_ = vignetting_correction(histogram_standardization(image))
+    skeleton_i = estimate_width(image_, skeleton)
+
+    all_shortest_paths = get_paths(skeleton, side_x)
+    canvas_list = []
+    for pair, path in all_shortest_paths.items():
+        distance = euclidean(*pair)
+        if distance < min_distance:
+            continue
+        canvas = get_canvas(1)
+        for y, x in path:
+            intensity: int = skeleton_i[y, x]
+            canvas[y, x] = intensity
+
+        length = cal_length(canvas)
+        intensities = [skeleton_i[y, x] for y, x in path]
+        mean_intensity = sum(intensities) / length
+        if mean_intensity < min_intensity:
+            continue
+
+        smooth = medfilt(np.array(intensities), kernel_size=3)
+        rsd = np.std(smooth) / np.mean(smooth)
+        if rsd > 0.5:
+            continue
+
+        # results = detect_sustained_intensity_change(path, intensities, window_size=15)
+        # intensity_diff = [x['change_magnitude'] for x in results]
+        # if not intensity_diff:
+        #     continue
+        # mean_intensity_diff = sum(intensity_diff) / len(intensity_diff)
+
+        results = analyze_curve_sharpness_windowed(path, half_window_size=15)
+        angles = [x['angle'] for x in results]
+
+        max_angle = max(angles)
+        if max_angle >= 90:
+            continue
+
+        # print(rsd)
+        # show_image(canvas)
+        canvas_list.append((canvas, max_angle + rsd * 0.5))
+
+    # paths = sorted(paths, key=lambda x: x['max_angle'])
+    canvas_list = sorted(canvas_list, key=lambda x: x[1])
+    canvas_all = get_canvas(1)
+    for canvas in canvas_list:
+        if not np.any(np.logical_and(canvas_all > 0, canvas[0] > 0)):
+            canvas_all += canvas[0]
+
+    return canvas_all
