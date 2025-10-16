6f2a67eb5be9dda1f955b19d933f45865cf03129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 14:07:05 +0000

    Add package minikai/0.1.2/minikai-0.1.2-py3-none-any.whl

---
+++ packages/minikai/minikai-0.1.2-py3-none-any.whl/minikai/models/patch_mini_command.py
@@ -0,0 +1,80 @@
+from collections.abc import Mapping
+from typing import Any, TypeVar, Union, cast
+
+from attrs import define as _attrs_define
+
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="PatchMiniCommand")
+
+
+@_attrs_define
+class PatchMiniCommand:
+    """
+    Attributes:
+        id (Union[Unset, int]):
+        name (Union[None, Unset, str]):
+        description (Union[None, Unset, str]):
+    """
+
+    id: Union[Unset, int] = UNSET
+    name: Union[None, Unset, str] = UNSET
+    description: Union[None, Unset, str] = UNSET
+
+    def to_dict(self) -> dict[str, Any]:
+        id = self.id
+
+        name: Union[None, Unset, str]
+        if isinstance(self.name, Unset):
+            name = UNSET
+        else:
+            name = self.name
+
+        description: Union[None, Unset, str]
+        if isinstance(self.description, Unset):
+            description = UNSET
+        else:
+            description = self.description
+
+        field_dict: dict[str, Any] = {}
+
+        field_dict.update({})
+        if id is not UNSET:
+            field_dict["id"] = id
+        if name is not UNSET:
+            field_dict["name"] = name
+        if description is not UNSET:
+            field_dict["description"] = description
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: type[T], src_dict: Mapping[str, Any]) -> T:
+        d = dict(src_dict)
+        id = d.pop("id", UNSET)
+
+        def _parse_name(data: object) -> Union[None, Unset, str]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, str], data)
+
+        name = _parse_name(d.pop("name", UNSET))
+
+        def _parse_description(data: object) -> Union[None, Unset, str]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, str], data)
+
+        description = _parse_description(d.pop("description", UNSET))
+
+        patch_mini_command = cls(
+            id=id,
+            name=name,
+            description=description,
+        )
+
+        return patch_mini_command
