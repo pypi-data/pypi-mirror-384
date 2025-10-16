78f22b2f79cdc47e14e3bc6028d40d4bb15d09b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 17:11:50 +0000

    Add package abstract-hugpy/0.0.0.157/abstract_hugpy-0.0.0.157-py3-none-any.whl

---
+++ packages/abstract-hugpy/abstract_hugpy-0.0.0.157-py3-none-any.whl/abstract_hugpy/hugpy_console/hugpy_flasks/__init__.py
@@ -0,0 +1,5 @@
+from .hugpy_deepcoder_flask_app import *
+from .hugpy_zerosearch_flask_app import *
+from .hugpy_proxyvideo_flask_app import *
+from .hugpy_video_flask_app import *
+from .hugpy_all_flask_app import hugpy_all_app
