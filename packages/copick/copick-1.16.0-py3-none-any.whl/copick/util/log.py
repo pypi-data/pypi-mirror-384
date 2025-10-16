af24f3be8acece6d362e7f0fc299d5a45de56e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 15:06:36 +0000

    Add package copick/1.16.0/copick-1.16.0-py3-none-any.whl

---
+++ packages/copick/copick-1.16.0-py3-none-any.whl/copick/util/log.py
@@ -0,0 +1,45 @@
+import logging
+import sys
+
+
+def get_logger(
+    name: str,
+    debug: bool = False,
+):
+    """
+    Configure basic console logging using basicConfig.
+
+    Args:
+        name: Name of the logger
+        debug: If True, set the logging level to DEBUG; otherwise, set it to INFO.
+
+    Returns:
+        The root logger
+    """
+
+    date_format = "%Y-%m-%d %H:%M:%S"
+
+    if debug:
+        level = logging.DEBUG
+        log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    else:
+        level = logging.INFO
+        log_format = "%(message)s"
+
+    # Configure the root logger with basicConfig
+    logging.basicConfig(
+        level=level,
+        format=log_format,
+        datefmt=date_format,
+        stream=sys.stderr,
+        force=True,
+    )
+    gql_logger = logging.getLogger("gql")
+    gql_logger.setLevel(logging.WARN if not debug else logging.DEBUG)
+
+    sshfs_logger = logging.getLogger("sshfs")
+    sshfs_logger.setLevel(logging.WARN if not debug else logging.DEBUG)
+
+    asyncssh_logger = logging.getLogger("asyncssh")
+    asyncssh_logger.setLevel(logging.WARN if not debug else logging.DEBUG)
+    return logging.getLogger(name)
