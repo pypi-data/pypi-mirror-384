a3a6b26556456c3d0f4e93a8ac1492903d3f161d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 15:07:18 +0000

    Add package mtcli/3.1.0/mtcli-3.1.0.tar.gz

---
+++ packages/mtcli/mtcli-3.1.0.tar.gz/mtcli-3.1.0/mtcli/logger.py
@@ -0,0 +1,29 @@
+"""Módulo de logging."""
+
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+def setup_logger(name="mtcli"):
+    if os.name == "nt":
+        base_dir = os.getenv("APPDATA", os.path.expanduser("~"))
+        log_dir = os.path.join(base_dir, name, "logs")
+    else:
+        base_dir = os.path.expanduser("~/.local/share")
+        log_dir = os.path.join(base_dir, name, "logs")
+
+    os.makedirs(log_dir, exist_ok=True)
+    log_path = os.path.join(log_dir, f"{name}.log")
+
+    handler = RotatingFileHandler(log_path, maxBytes=1_000_000, backupCount=3)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    if not logger.handlers:
+        logger.addHandler(handler)
+    logger.propagate = True
+
+    return logger
