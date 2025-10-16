7261d2e19cf646293421e585f7da24f74e900793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 08:06:02 +0000

    Add package chttpx/6.0.0rc14/chttpx-6.0.0rc14.tar.gz

---
+++ packages/chttpx/chttpx-6.0.0rc14.tar.gz/chttpx-6.0.0rc14/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+setup(
+    name='chttpx',
+    version='6.0.0rc14',
+    setup_requires='setupmeta',
+    packages=['chttpx'],
+    install_requires=[
+        'cli2',
+        'httpx<1',
+        'truststore',
+    ],
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    url='https://yourlabs.io/oss/cli2',
+    include_package_data=True,
+    license='MIT',
+    keywords='cli',
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'chttpx-example = chttpx.example:cli.entry_point',
+        ],
+        'pytest11': [
+            'chttpx = chttpx.pytest',
+        ],
+    },
+)
