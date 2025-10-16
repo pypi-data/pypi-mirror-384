8a651cdc99e3f1c8d22a28afc5cb41c733877b0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 06:04:59 +0000

    Add package cometx/3.0.1/cometx-3.0.1.tar.gz

---
+++ packages/cometx/cometx-3.0.1.tar.gz/cometx-3.0.1/cometx/cli/delete_assets.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ****************************************
+#                              __
+#   _________  ____ ___  ___  / /__  __
+#  / ___/ __ \/ __ `__ \/ _ \/ __/ |/_/
+# / /__/ /_/ / / / / / /  __/ /__>  <
+# \___/\____/_/ /_/ /_/\___/\__/_/|_|
+#
+#
+#  Copyright (c) 2022 Cometx Development
+#      Team. All rights reserved.
+# ****************************************
+"""
+Examples:
+
+To delete experiments assets with optional query:
+
+$ cometx delete-assets WORKSPACE/PROJECT --type=image --query='Other("Name") == "absolute_eagle_6153"'
+$ cometx delete-assets WORKSPACE/PROJECT/EXPERIMENT --type=all
+
+If `--query` is given as a Comet query string, only download those
+experiments that match
+
+Where TYPE is one of the following names:
+
+* all
+* asset
+* audio
+* code
+* image
+* notebook
+* text-sample
+* video
+"""
+import argparse
+import sys
+
+from comet_ml import API
+
+from ..utils import get_query_experiments
+
+ADDITIONAL_ARGS = False
+
+
+def get_parser_arguments(parser):
+    parser.add_argument(
+        "COMET_PATH",
+        help=(
+            "The Comet identifier, such as 'WORKSPACE', 'WORKSPACE/PROJECT', or "
+            + "'WORKSPACE/PROJECT/EXPERIMENT'. Leave empty to use defaults."
+        ),
+        nargs="?",
+        default=None,
+        type=str,
+    )
+    parser.add_argument(
+        "--type",
+        help="The type of item to log",
+        type=str,
+        default=None,
+    )
+    parser.add_argument(
+        "--debug", help="If given, allow debugging", default=False, action="store_true"
+    )
+    parser.add_argument(
+        "--query",
+        help="Only delete experiments that match this Comet query string",
+        type=str,
+        default=None,
+    )
+
+
+def delete_assets(parsed_args, remaining=None):
+    # Called via `cometx delete-assets ...`
+    try:
+        delete_cli(parsed_args)
+    except KeyboardInterrupt:
+        print("Canceled by CONTROL+C")
+    except Exception as exc:
+        if parsed_args.debug:
+            raise
+        else:
+            print("ERROR: " + str(exc))
+
+
+def delete_cli(parsed_args):
+    comet_path = (
+        parsed_args.COMET_PATH.split("/") if parsed_args.COMET_PATH is not None else []
+    )
+
+    if not comet_path:
+        workspace = None
+        project_name = None
+        experiment_key = None
+    elif len(comet_path) == 1:
+        workspace = comet_path[0]
+        project_name = None
+        experiment_key = None
+    elif len(comet_path) == 2:
+        workspace, project_name = comet_path
+        experiment_key = None
+    elif len(comet_path) == 3:
+        workspace, project_name, experiment_key = comet_path
+    else:
+        raise Exception("invalid COMET_PATH: %r" % parsed_args.COMET_PATH)
+
+    api = API()
+
+    if experiment_key:
+        experiments = [api.get_experiment(workspace, project_name, experiment_key)]
+    elif parsed_args.query is not None:
+        experiments = get_query_experiments(
+            api, parsed_args.query, workspace, project_name
+        )
+    else:
+        experiments = api.get_experiments(workspace, project_name)
+
+    delete_experiment_assets(api, experiments, parsed_args.type)
+
+
+def delete_experiment_assets(api, experiments, asset_type):
+    count = 0
+    for experiment in experiments:
+        print("Looking in %s..." % experiment.url)
+        assets = experiment.get_asset_list(asset_type)
+        for asset in assets:
+            api._client.delete_experiment_asset(experiment.id, asset["assetId"])
+            count += 1
+    print("Deleted %d assets of type %r" % (count, asset_type))
+
+
+def main(args):
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    get_parser_arguments(parser)
+    parsed_args = parser.parse_args(args)
+    delete_assets(parsed_args)
+
+
+if __name__ == "__main__":
+    # Called via `python -m cometx.cli.delete ...`
+    main(sys.argv[1:])
---
+++ packages/cometx/cometx-3.0.1.tar.gz/cometx-3.0.1/cometx/cli/reproduce.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ****************************************
+#                              __
+#   _________  ____ ___  ___  / /__  __
+#  / ___/ __ \/ __ `__ \/ _ \/ __/ |/_/
+# / /__/ /_/ / / / / / /  __/ /__>  <
+# \___/\____/_/ /_/ /_/\___/\__/_/|_|
+#
+#
+#  Copyright (c) 2022 Cometx Development
+#      Team. All rights reserved.
+# ****************************************
+
+import argparse
+import os
+import sys
+
+from cometx.framework.comet.download_manager import DownloadManager, clean_comet_path
+
+ADDITIONAL_ARGS = False
+
+
+def get_parser_arguments(parser):
+    parser.add_argument(
+        "COMET_PATH",
+        help=(
+            "The Comet identifier, such as 'WORKSPACE', 'WORKSPACE/PROJECT', or "
+            + "'WORKSPACE/PROJECT/EXPERIMENT'. Leave empty for all workspaces."
+        ),
+        type=str,
+    )
+    parser.add_argument(
+        "OUTPUT_DIR",
+        help=("The directory to copy files in order to reproduce the experiment"),
+        type=str,
+    )
+    parser.add_argument(
+        "--run",
+        help=("Run the reproducable script"),
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--executable",
+        help=("Run the reproducable script"),
+        type=str,
+        default="python",
+    )
+
+
+def reproduce(parsed_args, remaining=None):
+    comet_path = (
+        parsed_args.COMET_PATH.split("/") if parsed_args.COMET_PATH is not None else []
+    )
+
+    if len(comet_path) != 3:
+        raise Exception(
+            "invalid COMET_PATH: %r; requires workspace/project/experiment"
+            % parsed_args.COMET_PATH
+        )
+    else:
+        comet_path = clean_comet_path("/".join(comet_path))
+
+    manager = DownloadManager()
+    manager.root = parsed_args.OUTPUT_DIR
+    manager.debug = False
+    manager.use_name = False
+    manager.flat = True
+    manager.force = False
+    manager.filename = None
+    manager.overwrite = True
+    manager.summary = {key: 0 for key in manager.RESOURCE_FUNCTIONS.keys()}
+    manager.summary["artifacts"] = 0
+    manager.summary["model-registry"] = 0
+
+    experiment = manager.api.get(comet_path)
+    if experiment is None:
+        raise Exception(
+            "invalid experiment: %r; not found, or not available" % comet_path
+        )
+
+    os.makedirs(parsed_args.OUTPUT_DIR, exist_ok=True)
+    manager.download_code(experiment)
+    manager.download_git(experiment)
+    manager.download_requirements(experiment)
+    for asset_filename in ["conda-spec.txt", "conda-info.yml", "conda-environment.yml"]:
+        manager.download_asset(experiment, asset_filename)
+
+    shell_commands = ""
+    # "cd %s\n" % os.path.abspath(parsed_args.OUTPUT_DIR)
+    if os.path.exists(os.path.join(parsed_args.OUTPUT_DIR, "conda-spec.txt")):
+        shell_commands += "conda create --name reproduced-env --file conda-spec.txt\n"
+        shell_commands += "conda activate reproduced-env\n"
+    if os.path.exists(os.path.join(parsed_args.OUTPUT_DIR, "requirements.txt")):
+        shell_commands += "pip install -r requirements.txt\n"
+    if os.path.exists(os.path.join(parsed_args.OUTPUT_DIR, "git_diff.patch")):
+        shell_commands += "git apply git_diff.patch\n"
+    if os.path.exists(os.path.join(parsed_args.OUTPUT_DIR, "git_metadata.json")):
+        shell_commands += manager.get_git_text(experiment)
+        script = "../script.py"
+    else:
+        script = "script.py"
+
+    shell_commands += "%s %s\n" % (parsed_args.executable, script)
+
+    shell_script_name = os.path.join(parsed_args.OUTPUT_DIR, "script.sh")
+    with open(shell_script_name, "w") as fp:
+        fp.write(shell_commands)
+
+    os.system("chmod +x %s" % shell_script_name)
+
+    print("Shell command saved in: %s" % shell_script_name)
+    if parsed_args.run:
+        print("Running...")
+        os.system("cd %s; %s" % (parsed_args.OUTPUT_DIR, shell_script_name))
+    else:
+        print("To run, cd into %s and execute the script." % parsed_args.OUTPUT_DIR)
+
+
+def main(args):
+    # Called via `cometx reproduce ...`
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    get_parser_arguments(parser)
+    parsed_args = parser.parse_args(args)
+
+    reproduce(parsed_args)
+
+
+if __name__ == "__main__":
+    # Called via `python -m cometx.cli.reproduce ...`
+    # Called via `cometx reproduce ...`
+    main(sys.argv[1:])
