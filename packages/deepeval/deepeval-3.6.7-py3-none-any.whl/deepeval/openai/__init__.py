be72668d2a967f277725227817b076ab3dcf6c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 06:05:31 +0000

    Add package deepeval/3.6.7/deepeval-3.6.7-py3-none-any.whl

---
+++ packages/deepeval/deepeval-3.6.7-py3-none-any.whl/deepeval/openai/__init__.py
@@ -0,0 +1,19 @@
+try:
+    import openai  # noqa: F401
+except ImportError:
+    raise ModuleNotFoundError(
+        "Please install OpenAI to use this feature: 'pip install openai'"
+    )
+
+
+try:
+    from openai import OpenAI, AsyncOpenAI  # noqa: F401
+except ImportError:
+    OpenAI = None  # type: ignore
+    AsyncOpenAI = None  # type: ignore
+
+
+if OpenAI or AsyncOpenAI:
+    from deepeval.openai.patch import patch_openai_classes
+
+    patch_openai_classes()
