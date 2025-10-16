0f279de3a6758555ad00b56d600a3b6bd63bfc4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 15:07:46 +0000

    Add package metronome-sdk/3.0.0/metronome_sdk-3.0.0.tar.gz

---
+++ packages/metronome-sdk/metronome_sdk-3.0.0.tar.gz/metronome_sdk-3.0.0/src/metronome/types/v1/customers/alert_retrieve_params.py
@@ -0,0 +1,34 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Iterable
+from typing_extensions import Literal, Required, TypedDict
+
+__all__ = ["AlertRetrieveParams", "GroupValue"]
+
+
+class AlertRetrieveParams(TypedDict, total=False):
+    alert_id: Required[str]
+    """The Metronome ID of the threshold notification"""
+
+    customer_id: Required[str]
+    """The Metronome ID of the customer"""
+
+    group_values: Iterable[GroupValue]
+    """Only present for `spend_threshold_reached` notifications.
+
+    Retrieve the notification for a specific group key-value pair.
+    """
+
+    plans_or_contracts: Literal["PLANS", "CONTRACTS"]
+    """
+    When parallel threshold notifications are enabled during migration, this flag
+    denotes whether to fetch notifications for plans or contracts.
+    """
+
+
+class GroupValue(TypedDict, total=False):
+    key: Required[str]
+
+    value: Required[str]
