2c4c036e189571244e9368b59259401979cf7af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 07:05:07 +0000

    Add package epfo-scrapper/0.1.0/epfo_scrapper-0.1.0-py3-none-any.whl

---
+++ packages/epfo-scrapper/epfo_scrapper-0.1.0-py3-none-any.whl/epfo_scrapper/__init__.py
@@ -0,0 +1,5 @@
+from .extractor import EPFOScrapper
+from .token_fetcher import TokenFetcher
+from .utils import pretty_print_json  # export the utility
+
+__all__ = ["EPFOScrapper", "TokenFetcher", "pretty_print_json"]
