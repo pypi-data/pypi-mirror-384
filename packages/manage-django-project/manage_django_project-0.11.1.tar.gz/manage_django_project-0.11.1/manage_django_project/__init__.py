1eee8b366685973a29ea9044908771f300651d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 17:11:48 +0000

    Add package manage-django-project/0.11.1/manage_django_project-0.11.1.tar.gz

---
+++ packages/manage-django-project/manage_django_project-0.11.1.tar.gz/manage_django_project-0.11.1/manage_django_project/__init__.py
@@ -0,0 +1,8 @@
+"""
+    manage_django_project
+    Helper to develop Django projects.
+"""
+
+# See https://packaging.python.org/en/latest/specifications/version-specifiers/
+__version__ = '0.11.1'
+__author__ = 'Jens Diemer <git@jensdiemer.de>'
