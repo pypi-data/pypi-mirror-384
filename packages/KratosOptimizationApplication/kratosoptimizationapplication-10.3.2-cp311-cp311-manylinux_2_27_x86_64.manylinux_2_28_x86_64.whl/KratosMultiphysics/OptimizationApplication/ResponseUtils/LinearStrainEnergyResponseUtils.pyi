5bbf82151b24c4fa757776539ac131790bbc30c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 18:06:26 +0000

    Add package KratosOptimizationApplication/10.3.2/kratosoptimizationapplication-10.3.2-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/KratosOptimizationApplication/kratosoptimizationapplication-10.3.2-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/KratosMultiphysics/OptimizationApplication/ResponseUtils/LinearStrainEnergyResponseUtils.pyi
@@ -0,0 +1,7 @@
+import Kratos
+import Kratos.Expression
+
+def CalculateGradient(list_of_gradient_variables: Kratos.DoubleVariable | Kratos.Array1DVariable3, list_of_gradient_required_model_parts: Kratos.ModelPart, list_of_gradient_computed_model_parts: Kratos.ModelPart, list_of_container_expressions: list[Kratos.Expression.NodalExpression | Kratos.Expression.ConditionExpression | Kratos.Expression.ElementExpression], perturbation_size: float) -> None:
+    """CalculateGradient(list_of_gradient_variables: Union[Kratos.DoubleVariable, Kratos.Array1DVariable3], list_of_gradient_required_model_parts: Kratos.ModelPart, list_of_gradient_computed_model_parts: Kratos.ModelPart, list_of_container_expressions: list[Union[Kratos.Expression.NodalExpression, Kratos.Expression.ConditionExpression, Kratos.Expression.ElementExpression]], perturbation_size: float) -> None"""
+def CalculateValue(arg0: Kratos.ModelPart) -> float:
+    """CalculateValue(arg0: Kratos.ModelPart) -> float"""
