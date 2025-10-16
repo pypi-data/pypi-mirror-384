d3b465cab2cdb231a29b790c6b45b297ee62c43b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 03:19:35 +0000

    Add package constructional-diversity/0.0.10/constructional_diversity-0.0.10.tar.gz

---
+++ packages/constructional-diversity/constructional_diversity-0.0.10.tar.gz/constructional_diversity-0.0.10/constructional_diversity/__init__.py
@@ -0,0 +1,7 @@
+from .extractor import extract_verb_dependency
+
+
+__all__ = ['extract_verb_dependency'] 
+
+__version__ = '0.0.10'
+__author__ = 'Haerim Hwang'
---
+++ packages/constructional-diversity/constructional_diversity-0.0.10.tar.gz/constructional_diversity-0.0.10/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open('README.md', encoding='utf-8') as f:
+  long_description = f.read()
+
+setup(
+  name='constructional-diversity',
+  version='0.0.10',
+  description='This is a package with which users can analyze constructional diversity, constructional complexity, and verbal diversity.', 
+  long_description=long_description, 
+  long_description_content_type = 'text/markdown', 
+  author='Haerim Hwang',
+  author_email='yayhaerim@gmail.com',
+  url='https://pypi.org/project/constructional-diversity', 
+  license='MIT', 
+  python_requires='>=3.9', 
+  install_requires=[    "more_itertools",
+    "spacy",
+    "numpy",
+    "pandas",
+    "torch", 
+    "openpyxl"], 
+  #packages=[], 
+  package_data={'':['*']}, 
+  keywords=[], 
+
+)
