1aa9adfcbc2ee208eda7613a90c8a6485a6d1cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 06:04:59 +0000

    Add package hgraph/0.4.85/hgraph-0.4.85-py3-none-any.whl

---
+++ packages/hgraph/hgraph-0.4.85-py3-none-any.whl/hgraph/adaptors/data_frame/_data_frame_operators.py
@@ -0,0 +1,99 @@
+from typing import TypeVar
+
+import polars as pl
+
+from hgraph._types import (
+    TS,
+    Frame,
+    COMPOUND_SCALAR,
+    CompoundScalar,
+    TSD,
+    COMPOUND_SCALAR_1,
+    COMPOUND_SCALAR_2,
+    compound_scalar,
+    KEYABLE_SCALAR,
+    TS_SCHEMA,
+    TSB,
+)
+from hgraph._wiring import compute_node
+
+__all__ = ("join", "filter_frame", "filter_cs", "filter_exp", "filter_exp_seq", "group_by", "ungroup", "sorted_")
+
+ON_TYPE = TypeVar("ON_TYPE", str, tuple[str, ...], pl.Expr)
+
+
+def _compute_fields(mapping, scalars):
+    lhs: CompoundScalar = mapping[COMPOUND_SCALAR].py_type
+    rhs = mapping[COMPOUND_SCALAR_1].py_type
+    suffix = scalars["suffix"]
+    on = scalars["on"]
+    if type(on) is str:
+        on = [on]
+
+    lhs_schema = lhs.__meta_data_schema__
+    rhs_schema = rhs.__meta_data_schema__
+
+    schema = {k: v.py_type for k, v in lhs_schema.items()}
+    for k, v in rhs_schema.items():
+        if k in lhs_schema and k not in on:
+            schema[f"{k}{suffix}"] = v.py_type
+        else:
+            schema[k] = v.py_type
+
+    return compound_scalar(**schema)
+
+
+@compute_node(resolvers={COMPOUND_SCALAR_2: _compute_fields})
+def join(
+    lhs: TS[Frame[COMPOUND_SCALAR]],
+    rhs: TS[Frame[COMPOUND_SCALAR_1]],
+    on: ON_TYPE,
+    how: str = "inner",
+    suffix: str = "_right",
+) -> TS[Frame[COMPOUND_SCALAR_2]]:
+    """
+    Join two data frames together.
+    """
+    lhs: pl.DataFrame = lhs.value
+    return lhs.join(rhs.value, on=on, how=how, suffix=suffix)
+
+
+# TODO: find a new name for 'filter' to disambiguate with the filter_ operator/
+@compute_node
+def filter_frame(ts: TS[Frame[COMPOUND_SCALAR]], **predicate: TSB[TS_SCHEMA]) -> TS[Frame[COMPOUND_SCALAR]]:
+    kwargs = {k: v for k, v in predicate.value.items() if v is not None}
+    return ts.value.filter(**kwargs)
+
+
+@compute_node
+def filter_cs(ts: TS[Frame[COMPOUND_SCALAR]], predicate: TS[COMPOUND_SCALAR]) -> TS[Frame[COMPOUND_SCALAR]]:
+    kwargs = {k: v for k, v in predicate.value.to_dict().items() if v is not None}
+    return ts.value.filter(**kwargs)
+
+
+@compute_node
+def filter_exp(ts: TS[Frame[COMPOUND_SCALAR]], predicate: pl.Expr) -> TS[Frame[COMPOUND_SCALAR]]:
+    return ts.value.filter(predicate)
+
+
+@compute_node
+def filter_exp_seq(ts: TS[Frame[COMPOUND_SCALAR]], predicate: tuple[pl.Expr, ...]) -> TS[Frame[COMPOUND_SCALAR]]:
+    return ts.value.filter(predicate)
+
+
+@compute_node(resolvers={KEYABLE_SCALAR: lambda m, s: m[COMPOUND_SCALAR].py_type.__meta_data_schema__[s["by"]]})
+def group_by(ts: TS[Frame[COMPOUND_SCALAR]], by: str) -> TSD[KEYABLE_SCALAR, TS[Frame[COMPOUND_SCALAR]]]:
+    return {k: v for (k,), v in ts.value.group_by(by)}
+
+
+@compute_node
+def ungroup(ts: TSD[KEYABLE_SCALAR, TS[Frame[COMPOUND_SCALAR]]]) -> TS[Frame[COMPOUND_SCALAR]]:
+    v = [v.value for v in ts.valid_values() if v is not None and len(v.value) > 0]
+    if v:
+        return pl.concat(v)
+
+
+@compute_node
+def sorted_(ts: TS[Frame[COMPOUND_SCALAR]], by: str, descending: bool = False) -> TS[Frame[COMPOUND_SCALAR]]:
+    if not ts.value.is_empty():
+        return ts.value.sort(by, descending=descending)
---
+++ packages/hgraph/hgraph-0.4.85-py3-none-any.whl/hgraph/debug/_inspector_state.py
@@ -0,0 +1,39 @@
+import os
+from collections import defaultdict
+from dataclasses import dataclass, field
+from datetime import datetime
+
+import psutil
+from perspective import Table
+
+from hgraph import CompoundScalar
+from hgraph._impl._runtime._node import _SenderReceiverState
+from hgraph.adaptors.perspective import PerspectiveTablesManager
+from hgraph.debug._inspector_observer import InspectionObserver
+
+
+@dataclass
+class InspectorState(CompoundScalar):
+    observer: InspectionObserver = None
+    manager: PerspectiveTablesManager = None
+    table: Table = None
+    total_cycle_table: Table = None
+
+    process: psutil.Process = field(default_factory=lambda: psutil.Process(os.getpid()))
+
+    requests: _SenderReceiverState = field(default_factory=_SenderReceiverState)
+    last_request_process_time: datetime = None
+
+    graph_subscriptions: dict = field(default_factory=lambda: dict())  # graph_id -> item_id
+    node_subscriptions: dict = field(default_factory=lambda: defaultdict(set))  # node_id -> item_id
+    node_item_subscriptions: dict = field(default_factory=lambda: defaultdict(set))  # node_id -> {item_ids}
+    found_items: set = field(default_factory=set)
+
+    value_data: list = field(default_factory=list)
+    value_removals: set = field(default_factory=set)
+    tick_data: dict = field(default_factory=dict)
+    perf_data: list = field(default_factory=list)
+    total_data_prev: list = field(default_factory=dict)
+    total_data: list = field(default_factory=lambda: defaultdict(list))
+    last_publish_time: datetime = None
+    inspector_time: float = 0.0
