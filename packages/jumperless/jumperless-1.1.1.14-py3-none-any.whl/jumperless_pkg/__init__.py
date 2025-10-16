17241279f04ca1a11e1545a9a36d64c5517d10f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 16:13:54 +0000

    Add package jumperless/1.1.1.14/jumperless-1.1.1.14-py3-none-any.whl

---
+++ packages/jumperless/jumperless-1.1.1.14-py3-none-any.whl/jumperless_pkg/__init__.py
@@ -0,0 +1,15 @@
+"""
+Jumperless App
+
+A Python app to control your Jumperless breadboard, update firmware, 
+sync with Wokwi projects, flash Arduino code, 
+and probably some other stuff I'm forgetting right now.
+
+Homepage: https://jumperless.org
+"""
+
+__version__ = "1.1.1.14"
+__author__ = "Kevin Santo Cappuccio"
+__email__ = "KevinC@ppucc.io"
+__license__ = "GPL-3.0-or-later"
+
