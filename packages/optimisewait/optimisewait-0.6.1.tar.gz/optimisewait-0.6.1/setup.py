004ff2004300eb52a160514cfe6b3c62fefaff25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 17:11:51 +0000

    Add package optimisewait/0.6.1/optimisewait-0.6.1.tar.gz

---
+++ packages/optimisewait/optimisewait-0.6.1.tar.gz/optimisewait-0.6.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="optimisewait",
+    version="0.6.1",
+    packages=find_packages(),
+    install_requires=[
+        "pyautogui>=0.9.53",
+    ],
+    author="Alex M",
+    description="A Python utility for automated image detection and clicking",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AMAMazing/optimisewait",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
