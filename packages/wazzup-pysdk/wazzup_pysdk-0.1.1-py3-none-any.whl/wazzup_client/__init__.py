80a7ae3037071069e5dd8ed277e07c6a14774335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 17:11:52 +0000

    Add package wazzup-pysdk/0.1.1/wazzup_pysdk-0.1.1-py3-none-any.whl

---
+++ packages/wazzup-pysdk/wazzup_pysdk-0.1.1-py3-none-any.whl/wazzup_client/__init__.py
@@ -0,0 +1,29 @@
+"""Wazzup SDK package."""
+
+from __future__ import annotations
+
+from .exceptions import ErrorCategory, WazzupAPIError
+from .pagination import paginate, paginate_async
+from .public import WazzupPublicClient
+from .rate_limiter import RateLimiter, TokenBucket
+from .retry import RetryOptions
+from .tech import WazzupTechClient
+from .legacy_client import WazzupLegacyClient
+from .client import WazzupClient
+
+__all__ = (
+    "__version__",
+    "ErrorCategory",
+    "RateLimiter",
+    "RetryOptions",
+    "TokenBucket",
+    "WazzupAPIError",
+    "WazzupLegacyClient",
+    "WazzupClient",
+    "WazzupPublicClient",
+    "WazzupTechClient",
+    "paginate",
+    "paginate_async",
+)
+
+__version__ = "0.1.0"
