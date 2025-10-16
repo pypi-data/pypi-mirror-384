535b3b789ce5256682ab966e13b1859f53fcadf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 07:05:48 +0000

    Add package robotic/0.3.4.dev1/robotic-0.3.4.dev1-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/robotic/robotic-0.3.4.dev1-cp39-cp39-manylinux2014_x86_64.whl/robotic/include/rai/Optim/newton.h
@@ -0,0 +1,40 @@
+/*  ------------------------------------------------------------------
+    Copyright (c) 2011-2024 Marc Toussaint
+    email: toussaint@tu-berlin.de
+
+    This code is distributed under the MIT License.
+    Please see <root-path>/LICENSE for details.
+    --------------------------------------------------------------  */
+
+#pragma once
+
+#include "options.h"
+#include "../Core/array.h"
+
+int optNewton(arr& x, ScalarFunction& f, rai::OptOptions opt=DEFAULT_OPTIONS);
+
+struct OptNewton {
+  ScalarFunction& f;
+  arr& x;
+  rai::OptOptions opt;
+
+  enum StopCriterion { stopNone=0, stopDeltaConverge, stopTinyFSteps, stopTinyXSteps, stopCritEvals, stopStepFailed, stopLineSearchSteps };
+
+  OptNewton(arr& _x, ScalarFunction& _f, const rai::OptOptions& _opt);
+  ~OptNewton();
+  OptNewton& setBounds(const arr& _bounds);
+  void reinit(const arr& _x);
+
+  StopCriterion step();
+  StopCriterion run(uint maxIt = 1000);
+
+ public:
+  arr bounds;
+  double fx;
+  arr gx, Hx;
+  double alpha, beta;
+  int inner_iters=0, evals=0, numTinyFSteps=0, numTinyXSteps=0;
+  StopCriterion stopCriterion;
+  bool rootFinding=false;
+  double timeNewton=0., timeEval=0.;
+};
---
+++ packages/robotic/robotic-0.3.4.dev1-cp39-cp39-manylinux2014_x86_64.whl/robotic/include/rai/Optim/primalDual.h
@@ -0,0 +1,48 @@
+/*  ------------------------------------------------------------------
+    Copyright (c) 2011-2024 Marc Toussaint
+    email: toussaint@tu-berlin.de
+
+    This code is distributed under the MIT License.
+    Please see <root-path>/LICENSE for details.
+    --------------------------------------------------------------  */
+
+#pragma once
+
+#include "options.h"
+#include "newton.h"
+#include "lagrangian.h"
+
+struct PrimalDualProblem : ScalarFunction {
+  rai::LagrangianProblem L;
+
+  //duality gap parameter (log barrier parameter) of the primal dual equation system
+  double mu;
+
+  uint n_eq=0, n_ineq=0;
+  arr x_lambda; //last evaluation
+  double dualityMeasure=1.;
+  bool primalFeasible=false;
+
+  PrimalDualProblem(const arr& x, const shared_ptr<NLP>& P, const rai::OptOptions& opt);
+
+  double f(arr& r, arr& R, const arr& x); ///< CORE METHOD: the unconstrained scalar function F
+
+  void updateMu();
+};
+
+//==============================================================================
+//
+// Solvers
+//
+
+struct OptPrimalDual {
+  arr& x;
+  PrimalDualProblem PD;
+  OptNewton newton;
+  const rai::OptOptions& opt;
+  uint its=0;
+
+  OptPrimalDual(arr& x, arr& dual, const shared_ptr<NLP>& P, const rai::OptOptions& opt);
+  ~OptPrimalDual();
+  uint run(uint maxIt=1000);
+};
