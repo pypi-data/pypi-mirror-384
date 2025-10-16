bddd6f4ed00a2d03246bf00caa509a6a07069e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 07:05:09 +0000

    Add package elims/0.0.1/elims-0.0.1-py3-none-any.whl

---
+++ packages/elims/elims-0.0.1-py3-none-any.whl/elims/__init__.py
@@ -0,0 +1,5 @@
+"""elims module."""
+
+__author__ = """Fabien Meyer"""
+__email__ = "fabien-meyer@outlook.com"
+__version__ = "0.0.1"
---
+++ packages/elims/elims-0.0.1-py3-none-any.whl/elims/elims.py
@@ -0,0 +1,14 @@
+"""elims main."""
+
+
+def say_hello(name: str) -> str:
+    """Hello.
+
+    Args:
+        name (str): Name to greet.
+
+    Returns:
+        str: Greeting message.
+
+    """
+    return f"Hello {name}!"
