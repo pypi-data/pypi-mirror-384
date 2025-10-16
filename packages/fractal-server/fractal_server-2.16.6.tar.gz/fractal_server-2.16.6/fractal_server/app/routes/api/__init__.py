d4120cda34d5a98636274a4e8cfd209044cc383e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 16:13:47 +0000

    Add package fractal-server/2.16.6/fractal_server-2.16.6.tar.gz

---
+++ packages/fractal-server/fractal_server-2.16.6.tar.gz/fractal_server-2.16.6/fractal_server/app/routes/api/__init__.py
@@ -0,0 +1,28 @@
+"""
+`api` module
+"""
+from fastapi import APIRouter
+from fastapi import Depends
+
+from ....config import get_settings
+from ....syringe import Inject
+from fractal_server.app.models import UserOAuth
+from fractal_server.app.routes.auth import current_active_superuser
+
+
+router_api = APIRouter()
+
+
+@router_api.get("/alive/")
+async def alive():
+    settings = Inject(get_settings)
+    return dict(
+        alive=True,
+        version=settings.PROJECT_VERSION,
+    )
+
+
+@router_api.get("/settings/")
+async def view_settings(user: UserOAuth = Depends(current_active_superuser)):
+    settings = Inject(get_settings)
+    return settings.model_dump()
---
+++ packages/fractal-server/fractal_server-2.16.6.tar.gz/fractal_server-2.16.6/fractal_server/runner/v2/_slurm_ssh.py
@@ -0,0 +1,87 @@
+# Copyright 2022 (C) Friedrich Miescher Institute for Biomedical Research and
+# University of Zurich
+#
+# Original authors:
+# Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
+# Tommaso Comparin <tommaso.comparin@exact-lab.it>
+# Marco Franzon <marco.franzon@exact-lab.it>
+#
+# This file is part of Fractal and was originally developed by eXact lab S.r.l.
+# <exact-lab.it> under contract with Liberali Lab from the Friedrich Miescher
+# Institute for Biomedical Research and Pelkmans Lab from the University of
+# Zurich.
+"""
+Slurm Backend
+
+This backend runs fractal workflows in a SLURM cluster.
+"""
+from pathlib import Path
+
+from ...ssh._fabric import FractalSSH
+from ..executors.slurm_common.get_slurm_config import get_slurm_config
+from ..executors.slurm_ssh.runner import SlurmSSHRunner
+from ..set_start_and_last_task_index import set_start_and_last_task_index
+from .runner import execute_tasks_v2
+from fractal_server.app.models.v2 import DatasetV2
+from fractal_server.app.models.v2 import WorkflowV2
+from fractal_server.logger import set_logger
+from fractal_server.types import AttributeFilters
+
+logger = set_logger(__name__)
+
+
+def process_workflow(
+    *,
+    workflow: WorkflowV2,
+    dataset: DatasetV2,
+    workflow_dir_local: Path,
+    job_id: int,
+    workflow_dir_remote: Path | None = None,
+    first_task_index: int | None = None,
+    last_task_index: int | None = None,
+    logger_name: str,
+    job_attribute_filters: AttributeFilters,
+    job_type_filters: dict[str, bool],
+    user_id: int,
+    # SLURM-ssh-specific
+    fractal_ssh: FractalSSH,
+    slurm_account: str | None = None,
+    worker_init: str | None = None,
+) -> None:
+    """
+    Process workflow (SLURM backend public interface)
+    """
+
+    # Set values of first_task_index and last_task_index
+    num_tasks = len(workflow.task_list)
+    first_task_index, last_task_index = set_start_and_last_task_index(
+        num_tasks,
+        first_task_index=first_task_index,
+        last_task_index=last_task_index,
+    )
+
+    if isinstance(worker_init, str):
+        worker_init = worker_init.split("\n")
+
+    with SlurmSSHRunner(
+        fractal_ssh=fractal_ssh,
+        root_dir_local=workflow_dir_local,
+        root_dir_remote=workflow_dir_remote,
+        slurm_account=slurm_account,
+        common_script_lines=worker_init,
+    ) as runner:
+        execute_tasks_v2(
+            wf_task_list=workflow.task_list[
+                first_task_index : (last_task_index + 1)
+            ],
+            dataset=dataset,
+            job_id=job_id,
+            runner=runner,
+            workflow_dir_local=workflow_dir_local,
+            workflow_dir_remote=workflow_dir_remote,
+            logger_name=logger_name,
+            get_runner_config=get_slurm_config,
+            job_attribute_filters=job_attribute_filters,
+            job_type_filters=job_type_filters,
+            user_id=user_id,
+        )
