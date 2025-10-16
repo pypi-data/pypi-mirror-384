7eef81e89c46a387b3e1d66e28a733f6e3d37176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 17:12:11 +0000

    Add package plex-generate-previews/2.3.0/plex_generate_previews-2.3.0-py3-none-any.whl

---
+++ packages/plex-generate-previews/plex_generate_previews-2.3.0-py3-none-any.whl/plex_generate_previews/__init__.py
@@ -0,0 +1,11 @@
+"""
+Plex Video Preview Generator
+
+A tool for generating video preview thumbnails for Plex Media Server.
+Supports GPU acceleration (NVIDIA, AMD, Intel, WSL2) and CPU processing.
+"""
+
+from ._version import __version__
+
+__author__ = "stevezau"
+__description__ = "Generate video preview thumbnails for Plex Media Server"
---
+++ packages/plex-generate-previews/plex_generate_previews-2.3.0-py3-none-any.whl/plex_generate_previews/_version.py
@@ -0,0 +1,4 @@
+"""Version information for plex-generate-previews."""
+
+__version__ = "2.3.0"
+__version_tuple__ = (2, 3, 0)
