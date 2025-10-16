f889225ebdea3c8670c41313ac0d23c75df9fc7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 06:05:03 +0000

    Add package pennylane-catalyst/0.13.0/pennylane_catalyst-0.13.0-cp312-abi3-manylinux_2_28_aarch64.whl

---
+++ packages/pennylane-catalyst/pennylane_catalyst-0.13.0-cp312-abi3-manylinux_2_28_aarch64.whl/catalyst/include/Quantum/Transforms/Passes.h
@@ -0,0 +1,39 @@
+
+// Copyright 2022-2023 Xanadu Quantum Technologies Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <memory>
+
+#include "mlir/Pass/Pass.h"
+
+namespace catalyst {
+
+std::unique_ptr<mlir::Pass> createQuantumBufferizationPass();
+std::unique_ptr<mlir::Pass> createQuantumConversionPass();
+std::unique_ptr<mlir::Pass> createEmitCatalystPyInterfacePass();
+std::unique_ptr<mlir::Pass> createCopyGlobalMemRefPass();
+std::unique_ptr<mlir::Pass> createAdjointLoweringPass();
+std::unique_ptr<mlir::Pass> createRemoveChainedSelfInversePass();
+std::unique_ptr<mlir::Pass> createAnnotateFunctionPass();
+std::unique_ptr<mlir::Pass> createSplitMultipleTapesPass();
+std::unique_ptr<mlir::Pass> createMergeRotationsPass();
+std::unique_ptr<mlir::Pass> createDecomposeLoweringPass();
+std::unique_ptr<mlir::Pass> createDisentangleCNOTPass();
+std::unique_ptr<mlir::Pass> createDisentangleSWAPPass();
+std::unique_ptr<mlir::Pass> createIonsDecompositionPass();
+std::unique_ptr<mlir::Pass> createLoopBoundaryOptimizationPass();
+
+} // namespace catalyst
