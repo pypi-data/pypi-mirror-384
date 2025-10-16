5ea4b0860449c5e9718273338eb33d4e6362941b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 04:04:57 +0000

    Add package ko-lm-dataformat/0.3.1/ko_lm_dataformat-0.3.1-py3-none-any.whl

---
+++ packages/ko-lm-dataformat/ko_lm_dataformat-0.3.1-py3-none-any.whl/ko_lm_dataformat/sentence_splitter.py
@@ -0,0 +1,38 @@
+import importlib
+import logging
+from typing import List
+
+from .sentence_cleaner import clean_sentence
+
+
+logger = logging.getLogger(__name__)
+
+
+class SentenceSplitterBase:
+    """Base Class for sentence splitter"""
+
+    def __init__(self):
+        self.splitter = None
+
+    def split(self, document: str, clean_sent: bool) -> List[str]:
+        raise NotImplementedError
+
+
+class KssV1SentenceSplitter(SentenceSplitterBase):
+    def __init__(self):
+        super().__init__()
+        try:
+            self.splitter = importlib.import_module("kss")
+            assert importlib.metadata.version("kss") == "1.3.1"
+        except ImportError:
+            logger.warning(
+                "You need to install kss to use KssV1SentenceSplitter!\n"
+                ">>> pip install cython\n"
+                ">>> pip install kss==1.3.1\n"
+            )
+
+    def split(self, document: str, clean_sent: bool = False) -> List[str]:
+        sentences = self.splitter.split_sentences(document)
+        if clean_sent:
+            sentences = [clean_sentence(sent) for sent in sentences]
+        return sentences
