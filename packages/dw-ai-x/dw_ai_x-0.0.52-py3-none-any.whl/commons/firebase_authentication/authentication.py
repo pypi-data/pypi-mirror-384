9d476be1e5f427745e08b9aa5f3a779cdc88609d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 05:06:41 +0000

    Add package dw-ai-x/0.0.52/dw_ai_x-0.0.52-py3-none-any.whl

---
+++ packages/dw-ai-x/dw_ai_x-0.0.52-py3-none-any.whl/commons/firebase_authentication/authentication.py
@@ -0,0 +1,19 @@
+from firebase_admin import auth
+from fastapi import Header, HTTPException
+
+
+async def get_authenticated_user_uuid(authorization: str = Header(None)):
+    """Retrieve the authenticated user's UUID from the provided token."""
+
+    if not authorization or not authorization.startswith("Bearer "):
+        raise HTTPException(
+            status_code=401, detail="Missing or invalid authentication token"
+        )
+
+    # Extract the token from the header
+    token = authorization.split(" ")[1]
+    try:
+        decoded_token = auth.verify_id_token(token)
+        return decoded_token.get("uid")
+    except auth.InvalidIdTokenError:
+        raise HTTPException(status_code=401, detail="Invalid authentication token")
