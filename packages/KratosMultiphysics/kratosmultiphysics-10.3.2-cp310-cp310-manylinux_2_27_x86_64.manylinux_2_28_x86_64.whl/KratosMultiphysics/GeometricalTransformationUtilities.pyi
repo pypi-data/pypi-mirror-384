20317c1890e387f5eaef331b0f50f8640c1a95ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 18:06:25 +0000

    Add package KratosMultiphysics/10.3.2/kratosmultiphysics-10.3.2-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/KratosMultiphysics/kratosmultiphysics-10.3.2-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/KratosMultiphysics/GeometricalTransformationUtilities.pyi
@@ -0,0 +1,6 @@
+import Kratos
+
+def CalculateRotationMatrix(arg0: float, arg1: Kratos.Matrix, arg2: Kratos.Vector, arg3: Kratos.Vector) -> None:
+    """CalculateRotationMatrix(arg0: float, arg1: Kratos.Matrix, arg2: Kratos.Vector, arg3: Kratos.Vector) -> None"""
+def CalculateTranslationMatrix(arg0: float, arg1: Kratos.Matrix, arg2: Kratos.Vector) -> None:
+    """CalculateTranslationMatrix(arg0: float, arg1: Kratos.Matrix, arg2: Kratos.Vector) -> None"""
