3392c59d84d25f646dbf2122daf3b105c3563bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 08:06:02 +0000

    Add package cansible/6.0.0rc14/cansible-6.0.0rc14.tar.gz

---
+++ packages/cansible/cansible-6.0.0rc14.tar.gz/cansible-6.0.0rc14/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='cansible',
+    version='6.0.0rc14',
+    setup_requires='setupmeta',
+    packages=['cansible'],
+    install_requires=[
+        'cli2',
+        'ansible',
+    ],
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    url='https://yourlabs.io/oss/cli2',
+    include_package_data=True,
+    license='MIT',
+    keywords='cli',
+    python_requires='>=3.6',
+    entry_points={
+        'pytest11': [
+            'cansible-fixtures = cansible.pytest',
+        ],
+    },
+)
