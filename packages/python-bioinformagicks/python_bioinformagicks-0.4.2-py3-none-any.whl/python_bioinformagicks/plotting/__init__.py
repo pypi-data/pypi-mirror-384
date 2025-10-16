20bb916b8000ff6dbd5417e8a38719127881584e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 04:05:29 +0000

    Add package python-bioinformagicks/0.4.2/python_bioinformagicks-0.4.2-py3-none-any.whl

---
+++ packages/python-bioinformagicks/python_bioinformagicks-0.4.2-py3-none-any.whl/python_bioinformagicks/plotting/__init__.py
@@ -0,0 +1,9 @@
+from ._plot_split_embedding import plot_split_embedding 
+from ._plot_grouped_proportions import plot_grouped_proportions
+from ._plot_gprofiler_results import plot_gprofiler_results
+
+__all__ = [
+	"plot_split_embedding",
+	"plot_grouped_proportions",
+	"plot_gprofiler_results",
+]
