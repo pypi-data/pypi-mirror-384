5554c4a443a24926b5f43bbdc299b538e85ca20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 15:07:39 +0000

    Add package cirq-superstaq/0.5.53/cirq_superstaq-0.5.53.tar.gz

---
+++ packages/cirq-superstaq/cirq_superstaq-0.5.53.tar.gz/cirq_superstaq-0.5.53/cirq_superstaq/serialization_test.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import cirq
+
+import cirq_superstaq as css
+
+
+def test_serialization() -> None:
+    qubits = cirq.LineQubit.range(2)
+    circuit = cirq.Circuit(cirq.CX(*qubits), css.ZX(*qubits), cirq.ms(1.23).on(*qubits))
+
+    serialized_circuit = css.serialization.serialize_circuits(circuit)
+    assert isinstance(serialized_circuit, str)
+    assert css.serialization.deserialize_circuits(serialized_circuit) == [circuit]
+
+    circuits = [circuit, circuit]
+    serialized_circuits = css.serialization.serialize_circuits(circuits)
+    assert isinstance(serialized_circuits, str)
+    assert css.serialization.deserialize_circuits(serialized_circuits) == circuits
