9084d7f53e5186bb44e9f3fb8b6bc6cf846a06d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 03:20:27 +0000

    Add package embedl-hub/2025.10.3/embedl_hub-2025.10.3-py3-none-any.whl

---
+++ packages/embedl-hub/embedl_hub-2025.10.3-py3-none-any.whl/embedl_hub/tracking/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2025 Embedl AB
+
+
+from embedl_hub.tracking.client import Client as _Client
+from embedl_hub.tracking.rest_api import RunType
+
+global_client = _Client()
+
+set_project = global_client.set_project
+set_experiment = global_client.set_experiment
+start_run = global_client.start_run
+log_param = global_client.log_param
+log_metric = global_client.log_metric
+
+
+__all__ = [
+    "set_project",
+    "set_experiment",
+    "start_run",
+    "log_param",
+    "log_metric",
+    "RunType",
+    "global_client",
+]
