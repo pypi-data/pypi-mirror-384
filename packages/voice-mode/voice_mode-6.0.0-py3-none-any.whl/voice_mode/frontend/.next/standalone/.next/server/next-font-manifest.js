f5e88e353d15ea4e4252833fd9367e7e70f30d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 05:06:55 +0000

    Add package voice-mode/6.0.0/voice_mode-6.0.0-py3-none-any.whl

---
+++ packages/voice-mode/voice_mode-6.0.0-py3-none-any.whl/voice_mode/frontend/.next/standalone/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/tmp/build-via-sdist-mmv28dur/voice_mode-6.0.0/voice_mode/frontend/app/layout":["static/media/39883d31a7792467-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
