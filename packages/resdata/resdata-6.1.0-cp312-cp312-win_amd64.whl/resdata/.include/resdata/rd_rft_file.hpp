fd060440e4fd814d73cc0bc1594134768d11649f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 17:12:23 +0000

    Add package resdata/6.1.0/resdata-6.1.0-cp312-cp312-win_amd64.whl

---
+++ packages/resdata/resdata-6.1.0-cp312-cp312-win_amd64.whl/resdata/.include/resdata/rd_rft_file.hpp
@@ -0,0 +1,33 @@
+#ifndef ERT_RD_RFT_FILE_H
+#define ERT_RD_RFT_FILE_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <stdbool.h>
+
+#include <ert/util/stringlist.hpp>
+
+#include <resdata/rd_rft_node.hpp>
+#include <ert/util/vector.hpp>
+#include <ert/util/int_vector.hpp>
+
+typedef struct rd_rft_file_struct rd_rft_file_type;
+
+rd_rft_file_type *rd_rft_file_alloc_case(const char *case_input);
+bool rd_rft_file_case_has_rft(const char *case_input);
+rd_rft_file_type *rd_rft_file_alloc(const char *);
+void rd_rft_file_free(rd_rft_file_type *);
+int rd_rft_file_get_size__(const rd_rft_file_type *rft_file,
+                           const char *well_pattern, time_t recording_time);
+int rd_rft_file_get_size(const rd_rft_file_type *rft_file);
+rd_rft_node_type *
+rd_rft_file_get_well_time_rft(const rd_rft_file_type *rft_file,
+                              const char *well, time_t recording_time);
+rd_rft_node_type *rd_rft_file_iget_node(const rd_rft_file_type *rft_file,
+                                        int index);
+int rd_rft_file_get_num_wells(const rd_rft_file_type *rft_file);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
