ca627ceeec4e51a270f3b03fb6ab0c9b07034ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 08:06:02 +0000

    Add package gemini-webapi/1.16.0/gemini_webapi-1.16.0.tar.gz

---
+++ packages/gemini-webapi/gemini_webapi-1.16.0.tar.gz/gemini_webapi-1.16.0/src/gemini_webapi/utils/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+from asyncio import Task
+
+from .decorators import running
+from .upload_file import upload_file, parse_file_name
+from .rotate_1psidts import rotate_1psidts
+from .get_access_token import get_access_token
+from .load_browser_cookies import load_browser_cookies
+from .logger import logger, set_log_level
+
+
+rotate_tasks: dict[str, Task] = {}
