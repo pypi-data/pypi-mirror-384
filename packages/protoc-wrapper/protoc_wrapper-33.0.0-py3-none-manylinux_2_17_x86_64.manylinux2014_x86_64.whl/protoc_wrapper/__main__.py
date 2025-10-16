eb2ab834dbeeaf735176c3614fae571181177d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 14:06:49 +0000

    Add package protoc-wrapper/33.0.0/protoc_wrapper-33.0.0-py3-none-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/protoc-wrapper/protoc_wrapper-33.0.0-py3-none-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/protoc_wrapper/__main__.py
@@ -0,0 +1,13 @@
+import os, sys
+
+def main():
+    argv = [os.path.join(os.path.dirname(__file__), "protoc"), *sys.argv[1:]]
+    if os.name == "posix":
+        os.execv(argv[0], argv)
+    else:
+        import subprocess
+        sys.exit(subprocess.call(argv))
+
+
+if __name__ == "__main__":
+    main()
