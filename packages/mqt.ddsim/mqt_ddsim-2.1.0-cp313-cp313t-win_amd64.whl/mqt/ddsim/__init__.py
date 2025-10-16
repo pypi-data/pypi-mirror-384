6d037fa77d79046bd2b20661fb6ba781a9175fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 05:05:23 +0000

    Add package mqt.ddsim/2.1.0/mqt_ddsim-2.1.0-cp313-cp313t-win_amd64.whl

---
+++ packages/mqt.ddsim/mqt_ddsim-2.1.0-cp313-cp313t-win_amd64.whl/mqt/ddsim/__init__.py
@@ -0,0 +1,71 @@
+# Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+# Copyright (c) 2025 Munich Quantum Software Company GmbH
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+"""MQT DDSIM Python Package."""
+
+from __future__ import annotations
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_11_2():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'mqt_ddsim.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_11_2()
+del _delvewheel_patch_1_11_2
+# end delvewheel patch
+
+import sys
+
+# under Windows, make sure to add the appropriate DLL directory to the PATH
+if sys.platform == "win32":
+
+    def _dll_patch() -> None:
+        """Add the DLL directory to the PATH."""
+        import os
+        import sysconfig
+        from pathlib import Path
+
+        site_packages = Path(sysconfig.get_paths()["purelib"])
+        bin_dir = site_packages / "mqt" / "core" / "bin"
+        os.add_dll_directory(str(bin_dir))
+
+    _dll_patch()
+    del _dll_patch
+
+from ._version import version as __version__
+from .provider import DDSIMProvider
+from .pyddsim import (
+    CircuitSimulator,
+    DeterministicNoiseSimulator,
+    HybridSimulator,
+    HybridSimulatorMode,
+    PathSimulator,
+    PathSimulatorConfiguration,
+    PathSimulatorMode,
+    StochasticNoiseSimulator,
+    UnitarySimulator,
+    UnitarySimulatorMode,
+)
+
+__all__ = [
+    "CircuitSimulator",
+    "DDSIMProvider",
+    "DeterministicNoiseSimulator",
+    "HybridSimulator",
+    "HybridSimulatorMode",
+    "PathSimulator",
+    "PathSimulatorConfiguration",
+    "PathSimulatorMode",
+    "StochasticNoiseSimulator",
+    "UnitarySimulator",
+    "UnitarySimulatorMode",
+    "__version__",
+]
