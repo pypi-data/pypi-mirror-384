f9b0c1d7c71874f93128873f99cf0c730aa82226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 06:05:09 +0000

    Add package mqt-debugger/1.2.0/mqt_debugger-1.2.0-cp310-cp310-win_amd64.whl

---
+++ packages/mqt-debugger/mqt_debugger-1.2.0-cp310-cp310-win_amd64.whl/mqt/debugger/dap/messages/disconnect_dap_message.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2024 - 2025 Chair for Design Automation, TUM
+# Copyright (c) 2025 Munich Quantum Software Company GmbH
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+"""Represents the 'disconnect' DAP request."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+import mqt.debugger
+
+from .dap_message import DAPMessage
+
+if TYPE_CHECKING:
+    from .. import DAPServer
+
+
+class DisconnectDAPMessage(DAPMessage):
+    """Represents the 'disconnect' DAP request."""
+
+    message_type_name: str = "disconnect"
+
+    def __init__(self, message: dict[str, Any]) -> None:
+        """Initializes the 'DisconnectDAPMessage' instance.
+
+        Args:
+            message (dict[str, Any]): The object representing the 'disconnect' request.
+        """
+        super().__init__(message)
+
+    def validate(self) -> None:
+        """Validates the 'DisconnectDAPMessage' instance."""
+
+    def handle(self, server: DAPServer) -> dict[str, Any]:
+        """Performs the action requested by the 'disconnect' DAP request.
+
+        Args:
+            server (DAPServer): The DAP server that received the request.
+
+        Returns:
+            dict[str, Any]: The response to the request.
+        """
+        mqt.debugger.destroy_ddsim_simulation_state(server.simulation_state)
+        return super().handle(server)
