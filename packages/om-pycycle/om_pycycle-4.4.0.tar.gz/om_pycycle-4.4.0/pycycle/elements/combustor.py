7f0daefb7d69106788f5bcb57a61150fed7a80d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 07:05:21 +0000

    Add package om-pycycle/4.4.0/om_pycycle-4.4.0.tar.gz

---
+++ packages/om-pycycle/om_pycycle-4.4.0.tar.gz/om_pycycle-4.4.0/pycycle/elements/combustor.py
@@ -0,0 +1,181 @@
+""" Class definition for Combustor."""
+
+import numpy as np
+
+import openmdao.api as om
+
+from pycycle.thermo.thermo import Thermo, ThermoAdd
+
+from pycycle.thermo.cea.species_data import Properties, janaf
+
+from pycycle.elements.duct import PressureLoss
+
+from pycycle.flow_in import FlowIn
+from pycycle.passthrough import PassThrough
+from pycycle.element_base import Element
+
+
+
+class Combustor(Element):
+    """
+    A combustor that adds a fuel to an incoming flow mixture and burns it
+
+    --------------
+    Flow Stations
+    --------------
+    Fl_I
+    Fl_O
+
+    -------------
+    Design
+    -------------
+        inputs
+        --------
+        Fl_I:FAR
+        dPqP
+        MN
+
+        outputs
+        --------
+        Wfuel
+
+
+    -------------
+    Off-Design
+    -------------
+        inputs
+        --------
+        Fl_I:FAR
+        dPqP
+        area
+
+        outputs
+        --------
+        Wfuel
+
+    """
+
+    def initialize(self):
+
+        self.options.declare('statics', default=True,
+                             desc='If True, calculate static properties.')
+        self.options.declare('fuel_type', default="JP-7",
+                             desc='Type of fuel.')
+        
+        self.default_des_od_conns = [
+            #  (design src, off-design target)
+            ('Fl_O:stat:area', 'area')
+        ]
+
+        super().initialize()
+
+    def pyc_setup_output_ports(self): 
+
+        thermo_method = self.options['thermo_method']
+        thermo_data = self.options['thermo_data']
+        fuel_type = self.options['fuel_type']
+
+
+        self.thermo_add_comp = ThermoAdd(method=thermo_method, mix_mode='reactant',
+                                         thermo_kwargs={'spec':thermo_data,
+                                                        'inflow_composition':self.Fl_I_data['Fl_I'], 
+                                                        'mix_composition':fuel_type})
+        
+        # self.Fl_O_data['Fl_O'] = self.thermo_add_comp.output_port_data()
+        self.copy_flow(self.thermo_add_comp, 'Fl_O')
+
+    def setup(self):
+        thermo_method = self.options['thermo_method']
+        thermo_data = self.options['thermo_data']
+
+        inflow_composition = self.Fl_I_data['Fl_I']
+        air_fuel_composition = self.Fl_O_data['Fl_O']
+        design = self.options['design']
+        statics = self.options['statics']
+
+
+        # Create combustor flow station
+        in_flow = FlowIn(fl_name='Fl_I')
+        self.add_subsystem('in_flow', in_flow, promotes=['Fl_I:tot:*', 'Fl_I:stat:*'])
+
+        self.add_subsystem('mix_fuel', self.thermo_add_comp,
+                           promotes=['Fl_I:stat:W', ('mix:ratio', 'Fl_I:FAR'), 'Fl_I:tot:composition', 'Fl_I:tot:h', ('mix:W','Wfuel'), 'Wout'])
+
+        # Pressure loss
+        prom_in = [('Pt_in', 'Fl_I:tot:P'),'dPqP']
+        self.add_subsystem('p_loss', PressureLoss(), promotes_inputs=prom_in)
+
+        # Calculate vitiated flow station properties
+        vit_flow = Thermo(mode='total_hP', fl_name='Fl_O:tot', 
+                          method=thermo_method, 
+                          thermo_kwargs={'composition':air_fuel_composition, 
+                                         'spec':thermo_data})
+        self.add_subsystem('vitiated_flow', vit_flow, promotes_outputs=['Fl_O:*'])
+        self.connect("mix_fuel.mass_avg_h", "vitiated_flow.h")
+        self.connect("mix_fuel.composition_out", "vitiated_flow.composition")
+        self.connect("p_loss.Pt_out","vitiated_flow.P")
+
+        if statics:
+            if design:
+                # Calculate static properties.
+
+                out_stat = Thermo(mode='static_MN', fl_name='Fl_O:stat', 
+                                  method=thermo_method, 
+                                  thermo_kwargs={'composition':air_fuel_composition, 
+                                                 'spec':thermo_data})
+                prom_in = ['MN']
+                prom_out = ['Fl_O:stat:*']
+                self.add_subsystem('out_stat', out_stat, promotes_inputs=prom_in,
+                                   promotes_outputs=prom_out)
+
+                self.connect("mix_fuel.composition_out", "out_stat.composition")
+                self.connect('Fl_O:tot:S', 'out_stat.S')
+                self.connect('Fl_O:tot:h', 'out_stat.ht')
+                self.connect('Fl_O:tot:P', 'out_stat.guess:Pt')
+                self.connect('Fl_O:tot:gamma', 'out_stat.guess:gamt')
+                self.connect('Wout','out_stat.W')
+
+            else:
+                # Calculate static properties.
+                out_stat = Thermo(mode='static_A', fl_name='Fl_O:stat', 
+                                  method=thermo_method, 
+                                  thermo_kwargs={'composition':air_fuel_composition, 
+                                                 'spec':thermo_data})
+                prom_in = ['area']
+                prom_out = ['Fl_O:stat:*']
+                self.add_subsystem('out_stat', out_stat, promotes_inputs=prom_in,
+                                   promotes_outputs=prom_out)
+                self.connect("mix_fuel.composition_out", "out_stat.composition")
+
+                self.connect('Fl_O:tot:S', 'out_stat.S')
+                self.connect('Fl_O:tot:h', 'out_stat.ht')
+                self.connect('Fl_O:tot:P', 'out_stat.guess:Pt')
+                self.connect('Fl_O:tot:gamma', 'out_stat.guess:gamt')
+                self.connect('Wout','out_stat.W')
+
+        else:
+            self.add_subsystem('W_passthru', PassThrough('Wout', 'Fl_O:stat:W', 1.0, units= "lbm/s"),
+                               promotes=['*'])
+
+
+        super().setup()
+
+
+if __name__ == "__main__":
+
+    p = om.Problem()
+    p.model = om.Group()
+    p.model.add_subsystem('comp', MixFuel(), promotes=['*'])
+
+    p.model.add_subsystem('d1', om.IndepVarComp('Fl_I:stat:W', val=1.0, units='lbm/s', desc='weight flow'),
+                          promotes=['*'])
+    p.model.add_subsystem('d2', om.IndepVarComp('Fl_I:FAR', val=0.2, desc='Fuel to air ratio'), promotes=['*'])
+    p.model.add_subsystem('d3', om.IndepVarComp('Fl_I:tot:h', val=1.0, units='Btu/lbm', desc='total enthalpy'),
+                          promotes=['*'])
+    p.model.add_subsystem('d4', om.IndepVarComp('fuel_Tt', val=518.0, units='degR', desc='fuel temperature'),
+                          promotes=['*'])
+
+    p.setup(check=False, force_alloc_complex=True)
+    p.run_model()
+
+    p.check_partials(compact_print=True, method='cs')
---
+++ packages/om-pycycle/om_pycycle-4.4.0.tar.gz/om_pycycle-4.4.0/pycycle/elements/flight_conditions.py
@@ -0,0 +1,145 @@
+import openmdao.api as om
+
+from pycycle.thermo.cea import species_data
+from pycycle.thermo.cea import thermo_data
+from pycycle.constants import THERMO_DEFAULT_COMPOSITIONS
+from pycycle.elements.ambient import Ambient
+from pycycle.elements.flow_start import FlowStart
+from pycycle.thermo.thermo import ThermoAdd, Thermo
+from pycycle.element_base import Element
+
+
+class FlightConditions(Element):
+    """Determines total and static flow properties given an altitude and Mach number using the input atmosphere model"""
+
+    def initialize(self):
+        self.options.declare('composition', default=None,
+                              desc='composition of the flow. If None, default for thermo package is used')
+        self.options.declare('reactant', default=False, types=(bool, str), 
+                              desc='If False, flow matches base composition. If a string, then that reactant '
+                                   'is mixed into the flow at at the ratio set by the `mix_ratio` input')
+        self.options.declare('mix_ratio_name', default='mix:ratio', 
+                             desc='The name of the input that governs the mix ratio of the reactant to the primary flow')
+
+        super().initialize()
+        
+    def pyc_setup_output_ports(self): 
+        composition = self.options['composition']
+        thermo_method = self.options['thermo_method']
+        thermo_data = self.options['thermo_data']
+        reactant = self.options['reactant']
+
+        if reactant is not False: 
+            # just make a local ThermoAdd, since the FS will make the real one for us later
+            thermo_add = ThermoAdd(method=thermo_method, mix_mode='reactant', 
+                                   thermo_kwargs={'spec':thermo_data, 
+                                                  'inflow_composition':composition, 
+                                                  'mix_composition':reactant, })
+          
+            self.init_output_flow('Fl_O', thermo_add)
+
+        else: 
+            if composition is None: 
+                composition = THERMO_DEFAULT_COMPOSITIONS[thermo_method]
+            self.init_output_flow('Fl_O', composition)
+        
+
+    def setup(self):
+        thermo_method = self.options['thermo_method']
+        thermo_data = self.options['thermo_data']
+        reactant = self.options['reactant']
+        mix_ratio_name = self.options['mix_ratio_name']
+
+        # composition = self.Fl_O_data['Fl_O']
+        composition = self.options['composition']
+
+        self.add_subsystem('ambient', Ambient(), promotes=('alt', 'dTs'))  # inputs
+
+        conv = self.add_subsystem('conv', om.Group(), promotes=['*'])
+        if reactant is not False:
+            proms = ['Fl_O:*', 'MN', 'W', mix_ratio_name]
+        else:
+            proms = ['Fl_O:*', 'MN', 'W']
+        fs_start = conv.add_subsystem('fs', FlowStart(thermo_method=thermo_method,
+                                                      thermo_data=thermo_data, 
+                                                      composition=composition, 
+                                                      reactant=reactant, 
+                                                      mix_ratio_name=mix_ratio_name), 
+                                      promotes=proms)
+
+        # need to manually call this in this setup, because we have an element within an element
+        fs_start.pyc_setup_output_ports() 
+
+        balance = conv.add_subsystem('balance', om.BalanceComp())
+        balance.add_balance('Tt', val=500.0, lower=1e-4, units='degR', desc='Total temperature', eq_units='degR')
+        balance.add_balance('Pt', val=14.696, lower=1e-4, units='psi', desc='Total pressure', eq_units='psi')
+        # sub.set_order(['fs','balance'])
+
+        newton = conv.nonlinear_solver = om.NewtonSolver()
+        newton.options['atol'] = 1e-10
+        newton.options['rtol'] = 1e-10
+        newton.options['maxiter'] = 10
+        newton.options['iprint'] = -1
+        newton.options['solve_subsystems'] = True
+        newton.options['reraise_child_analysiserror'] = False
+        newton.linesearch = om.BoundsEnforceLS()
+        newton.linesearch.options['bound_enforcement'] = 'scalar'
+
+        newton.linesearch.options['iprint'] = -1
+        # newton.linesearch.options['solve_subsystems'] = True
+
+        conv.linear_solver = om.DirectSolver(assemble_jac=True)
+
+        self.connect('ambient.Ps', 'balance.rhs:Pt')
+        self.connect('ambient.Ts', 'balance.rhs:Tt')
+
+        self.connect('balance.Pt', 'fs.P')
+        self.connect('balance.Tt', 'fs.T')
+
+        self.connect('Fl_O:stat:P', 'balance.lhs:Pt')
+        self.connect('Fl_O:stat:T', 'balance.lhs:Tt')
+
+        # self.set_order(['ambient', 'subgroup'])
+
+        super().setup()
+
+
+if __name__ == "__main__":
+
+    p1 = om.Problem()
+    p1.model = om.Group()
+
+    des_vars = p1.model.add_subsystem('des_vars', om.IndepVarComp())
+    des_vars.add_output('W', 0.0, units='lbm/s')
+    des_vars.add_output('alt', 1., units='ft')
+    des_vars.add_output('MN', 0.5)
+    des_vars.add_output('dTs', 0.0, units='degR')
+
+
+    fc = p1.model.add_subsystem("fc", FlightConditions(thermo_data=thermo_data.wet_air))
+
+    p1.model.connect('des_vars.W', 'fc.W')
+    p1.model.connect('des_vars.alt', 'fc.alt')
+    p1.model.connect('des_vars.MN', 'fc.MN')
+    p1.model.connect('des_vars.dTs', 'fc.dTs')
+
+    p1.setup()
+
+    # p1.root.list_connections()
+
+    p1['des_vars.alt'] = 17868.79060515557
+    p1['des_vars.MN'] = 2.101070288213628
+    p1['des_vars.dTs'] = 0.0
+    p1['des_vars.W'] = 1.0
+
+    p1.run_model()
+
+    print('Ts_atm: ', p1['fc.ambient.Ts'])
+    print('Ts_set: ', p1['fc.Fl_O:stat:T'])
+    print('Ps_atm: ', p1['fc.ambient.Ps'])
+    print('Ps_set: ', p1['fc.Fl_O:stat:P'])
+    print('rhos_atm: ', p1['fc.ambient.rhos']*32.175)
+    print('rhos_set: ', p1['fc.Fl_O:stat:rho'])
+    print('W', p1['fc.Fl_O:stat:W'])
+
+    print('Pt: ', p1['fc.Fl_O:tot:P'])
