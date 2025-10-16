062ab5fa1c32fe587c52c94adb7ab5039f5374e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-16 15:06:19 +0000

    Add package discord-rpc/5.5b1/discord_rpc-5.5b1-py3-none-any.whl

---
+++ packages/discord-rpc/discord_rpc-5.5b1-py3-none-any.whl/discordrpc/__init__.py
@@ -0,0 +1,11 @@
+from .presence import RPC
+from .button import Button
+from .exceptions import *
+from .types import *
+from .utils import *
+
+__title__ = "Discord RPC"
+__version__ = "5.5b1"
+__authors__ = "Senophyx"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2021-2025 Senophyx"
