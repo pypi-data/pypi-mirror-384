d45672ac9441a6dfa4ab31922aac5081b70d382b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 08:07:51 +0000

    Add package dsf-label-sdk/1.1.9/dsf_label_sdk-1.1.9.tar.gz

---
+++ packages/dsf-label-sdk/dsf_label_sdk-1.1.9.tar.gz/dsf_label_sdk-1.1.9/dsf_label_sdk/__init__.py
@@ -0,0 +1,27 @@
+# dsf_label_sdk/__init__.py
+
+__version__ = '1.1.9'
+__author__ = 'Jaime Alexander Jimenez'
+__email__ = 'contacto@softwarefinanzas.com.co'
+
+from .client import LabelSDK
+from .exceptions import (
+    LabelSDKError,
+    ValidationError, 
+    LicenseError,
+    APIError,
+    RateLimitError 
+)
+from .models import Field, Config, EvaluationResult
+
+__all__ = [
+    'LabelSDK',
+    'Field',
+    'Config',
+    'EvaluationResult',
+    'LabelSDKError',
+    'ValidationError',
+    'LicenseError',
+    'APIError',
+    'RateLimitError' 
+]
