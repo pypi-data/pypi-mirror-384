db0e786dea262b7a62b9f3b036ec6be1a33a7dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 15:19:50 +0000

    Add package windows-use/0.6.5/windows_use-0.6.5-py3-none-any.whl

---
+++ packages/windows-use/windows_use-0.6.5-py3-none-any.whl/windows_use/agent/views.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel,Field
+    
+class AgentResult(BaseModel):
+    content:str|None=None
+    error:str|None=None
+
+class Action(BaseModel):
+    name:str
+    params: dict=Field(default_factory=dict)
+
+class AgentData(BaseModel):
+    evaluate: str
+    thought: str
+    action: Action
