51c2f454b93f14ec514bb2d13de0e767f31b83c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 04:07:22 +0000

    Add package ray-utilities/0.6.0/ray_utilities-0.6.0.tar.gz

---
+++ packages/ray-utilities/ray_utilities-0.6.0.tar.gz/ray_utilities-0.6.0/ray_utilities/callbacks/tuner/_log_result_grouping.py
@@ -0,0 +1,24 @@
+non_metric_results = {
+    "comment",
+    "cli_args/comment",
+    "run_id",
+    "experiment_name",
+    "experiment_group",
+    "experiment_key",
+    "trainable_name",
+    "env_runners/environments/seeds",
+    "env_runners/environments/seeds/seed_sequence",
+    "evaluation/env_runners/environments/seeds",
+    "evaluation/env_runners/environments/seeds/seed_sequence",
+    # New log style
+    "training/environments/seeds",
+    "training/environments/seeds/seed_sequence",
+    "evaluation/environments/seeds",
+    "evaluation/environments/seeds/seed_sequence",
+}
+
+exclude_results = {
+    "cli_args/test",
+    "cli_args/num_jobs",
+    "node_ip",
+}
