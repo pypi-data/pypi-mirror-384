9360cb081ccd4a536c331dd5c82f17c113cff613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 16:08:51 +0000

    Add package passagemath-gap/10.6.31rc1/passagemath_gap-10.6.31rc1-cp313-cp313-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/passagemath-gap/passagemath_gap-10.6.31rc1-cp313-cp313-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/gap/pkg/smallclassnr/lib/smallclassnr.gd
@@ -0,0 +1,11 @@
+###############################################################################
+##
+## Declarations
+##
+DeclareGlobalFunction( "SmallClassNrGroupsAvailable" );
+DeclareGlobalFunction( "NrSmallClassNrGroups" );
+DeclareGlobalFunction( "SmallClassNrGroup" );
+DeclareGlobalFunction( "IteratorSmallClassNrGroups" );
+DeclareGlobalFunction( "AllSmallClassNrGroups" );
+DeclareGlobalFunction( "OneSmallClassNrGroup" );
+DeclareAttribute( "IdClassNr", IsGroup );
