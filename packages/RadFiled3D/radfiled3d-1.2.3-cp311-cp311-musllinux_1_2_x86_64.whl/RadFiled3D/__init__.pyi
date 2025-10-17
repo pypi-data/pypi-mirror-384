11a601367f6fc17ee9a059f90b75f68442bd5316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 04:07:04 +0000

    Add package RadFiled3D/1.2.3/radfiled3d-1.2.3-cp311-cp311-musllinux_1_2_x86_64.whl

---
+++ packages/RadFiled3D/radfiled3d-1.2.3-cp311-cp311-musllinux_1_2_x86_64.whl/RadFiled3D/__init__.pyi
@@ -0,0 +1,10 @@
+import ctypes
+import os
+
+# Module imports:
+
+
+
+lib_path = os.path.join(os.path.dirname(__file__), 'RadFiled3D.cpython-311-x86_64-linux-musl.so')
+lib = ctypes.CDLL(lib_path)
+
