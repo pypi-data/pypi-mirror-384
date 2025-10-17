ce31259c6ec9321eaea2e21a77f3741a4cbaf694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 03:19:32 +0000

    Add package PyMuPDFPro/1.26.5/pymupdfpro-1.26.5-cp39-abi3-macosx_10_9_x86_64.whl

---
+++ packages/PyMuPDFPro/pymupdfpro-1.26.5-cp39-abi3-macosx_10_9_x86_64.whl/pymupdf/_pro_build.py
@@ -0,0 +1,8 @@
+
+version = '1.26.5'
+version_tuple = (1, 26, 5)
+pymupdf_version = '1.26.5'
+pymupdf_version_tuple = (1, 26, 5)
+mupdf_version = '1.26.10'
+mupdf_version_tuple = (1, 26, 10)
+platform_python_implementation = 'CPython'
