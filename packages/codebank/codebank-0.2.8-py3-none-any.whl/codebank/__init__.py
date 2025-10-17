e59dafefc0732816d91682fff20d667b3246c5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 05:07:15 +0000

    Add package codebank/0.2.8/codebank-0.2.8-py3-none-any.whl

---
+++ packages/codebank/codebank-0.2.8-py3-none-any.whl/codebank/__init__.py
@@ -0,0 +1,6 @@
+from .mllab import one, two, three, four, five, fivea, six, seven, eight, nine
+from .wtlab import list,hotspot,css,calc,form,event,effect,grid,panel,time,food,cd,session,webapp,form2,fact,formimg,welcome,lang,array,summul,cricket,frame
+
+
+from .dllab import xor,digit,xray,speech,traffic,fraud,rbm,lstm,speech1
+from .iotlab import blink,zigbee,gsm,bluetooth,wifi,irsensor,ardras,cloud
