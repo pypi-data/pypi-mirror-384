d9752e1cd2bbb3973d0570cdb4441e0079b76c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 05:08:02 +0000

    Add package svg-ultralight/0.50.1/svg_ultralight-0.50.1-py3-none-any.whl

---
+++ packages/svg-ultralight/svg_ultralight-0.50.1-py3-none-any.whl/svg_ultralight/attrib_hints.py
@@ -0,0 +1,13 @@
+"""Type hints for pass-through arguments to lxml constructors.
+
+:author: Shay Hill
+:created: 2025-07-09
+"""
+
+from collections.abc import Mapping
+
+# Types svg_ultralight can format to pass through to lxml constructors.
+ElemAttrib = str | float | None
+
+# Type for an optional dictionary of element attributes.
+OptionalElemAttribMapping = Mapping[str, ElemAttrib] | None
