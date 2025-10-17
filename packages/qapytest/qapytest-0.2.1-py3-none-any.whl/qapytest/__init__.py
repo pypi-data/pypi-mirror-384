7cc9e84c6ff05a59b5aaef43ed3b1d348c2c3840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 03:19:45 +0000

    Add package qapytest/0.2.1/qapytest-0.2.1-py3-none-any.whl

---
+++ packages/qapytest/qapytest-0.2.1-py3-none-any.whl/qapytest/__init__.py
@@ -0,0 +1,20 @@
+"""QAPyTest is a powerful package for QA specialists built on top of Pytest."""
+
+from qapytest._attach import attach
+from qapytest._client_http import GraphQLClient, HttpClient
+from qapytest._json_validation import validate_json
+from qapytest._redis import RedisClient
+from qapytest._soft_assert import soft_assert
+from qapytest._sql import SqlClient
+from qapytest._step import step
+
+__all__ = [
+    "GraphQLClient",
+    "HttpClient",
+    "RedisClient",
+    "SqlClient",
+    "attach",
+    "soft_assert",
+    "step",
+    "validate_json",
+]
