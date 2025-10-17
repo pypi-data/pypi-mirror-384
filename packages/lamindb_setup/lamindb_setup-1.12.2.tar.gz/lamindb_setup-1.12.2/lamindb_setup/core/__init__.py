c1bf14c490371490190a5a057de428cf752ee1f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 03:19:27 +0000

    Add package lamindb_setup/1.12.2/lamindb_setup-1.12.2.tar.gz

---
+++ packages/lamindb_setup/lamindb_setup-1.12.2.tar.gz/lamindb_setup-1.12.2/lamindb_setup/core/__init__.py
@@ -0,0 +1,21 @@
+"""Core setup library.
+
+Settings:
+
+.. autosummary::
+   :toctree:
+
+   SetupSettings
+   UserSettings
+   InstanceSettings
+   StorageSettings
+
+"""
+
+from . import django, upath
+from ._deprecated import deprecated
+from ._docs import doc_args
+from ._settings import SetupSettings
+from ._settings_instance import InstanceSettings
+from ._settings_storage import StorageSettings
+from ._settings_user import UserSettings
---
+++ packages/lamindb_setup/lamindb_setup-1.12.2.tar.gz/lamindb_setup-1.12.2/tests/hub-local/conftest.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import os
+
+from laminhub_rest.dev import (
+    SupabaseResources,
+    remove_lamin_local_settings,
+    seed_local_test,
+)
+
+supabase_resources = SupabaseResources()
+
+
+def pytest_configure():
+    os.environ["LAMIN_ENV"] = "local"
+    os.environ["LAMIN_CLOUD_VERSION"] = "0.1"
+    os.environ["LAMIN_TEST_INSTANCE_SCHEMA_STR"] = ""
+    os.environ["LAMIN_TEST_INSTANCE_PUBLIC"] = "false"
+    remove_lamin_local_settings()
+    supabase_resources.start_local()
+    supabase_resources.reset_local()
+    supabase_resources.migrate()
+    seed_local_test()
+
+
+def pytest_unconfigure():
+    if supabase_resources.edge_function_process:
+        supabase_resources.stop_local_edge_functions()
