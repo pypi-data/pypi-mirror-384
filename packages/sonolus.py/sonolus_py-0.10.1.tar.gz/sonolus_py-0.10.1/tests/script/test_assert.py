adde8298da877f9f6820aa309e023348b6229613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 14:06:32 +0000

    Add package sonolus.py/0.10.1/sonolus_py-0.10.1.tar.gz

---
+++ packages/sonolus.py/sonolus_py-0.10.1.tar.gz/sonolus_py-0.10.1/tests/script/test_assert.py
@@ -0,0 +1,65 @@
+"""Tests for assert statements.
+
+PYTEST_DONT_REWRITE
+"""
+
+from sonolus.script.debug import assert_false, assert_true
+from tests.script.conftest import run_compiled
+
+
+def test_assertion_succeeds():
+    def fn():
+        assert True, "Message"
+        return 1
+
+    assert run_compiled(fn) == 1
+
+
+def test_assertion_fails():
+    def fn():
+        assert False, "Message"  # noqa: B011, PT015
+        # noinspection PyUnreachableCode
+        return 1
+
+    assert run_compiled(fn, dev=True) == 0
+
+
+def test_asserting_does_nothing_if_not_dev_mode():
+    def fn():
+        assert False, "Message"  # noqa: B011, PT015
+        assert True, "Message"
+        return 1
+
+    assert run_compiled(fn, dev=False) == 1
+
+
+def test_assert_true_succeeds():
+    def fn():
+        assert_true(True)
+        return 1
+
+    assert run_compiled(fn) == 1
+
+
+def test_assert_true_fails():
+    def fn():
+        assert_true(False)
+        return 1
+
+    assert run_compiled(fn, dev=True) == 0
+
+
+def test_assert_false_succeeds():
+    def fn():
+        assert_false(False)
+        return 1
+
+    assert run_compiled(fn) == 1
+
+
+def test_assert_false_fails():
+    def fn():
+        assert_false(True)
+        return 1
+
+    assert run_compiled(fn, dev=True) == 0
