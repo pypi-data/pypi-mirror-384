1cc86f62bf77a77d427317ba0b3076cb0118ce10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 08:07:26 +0000

    Add package gen3-tracker/0.0.7rc26/gen3_tracker-0.0.7rc26.tar.gz

---
+++ packages/gen3-tracker/gen3_tracker-0.0.7rc26.tar.gz/gen3_tracker-0.0.7rc26/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+# import os
+# print(os.getcwd())
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='gen3_tracker',
+    version='0.0.7rc26',
+    description='A CLI for adding version control to Gen3 data submission projects.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='walsbr',
+    author_email='walsbr@ohsu.edu',
+    url='https://github.com/ACED-IDP/gen3_util',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    install_requires=requirements,
+    include_package_data=True,
+    package_data={  # Optional
+        '': ['*.yaml'],
+    },
+    extras_require={
+        'dtale': ['dtale'],
+    },
+    entry_points={
+        'console_scripts': [
+            'g3t=gen3_tracker.cli:cli',
+        ],
+    },
+)
