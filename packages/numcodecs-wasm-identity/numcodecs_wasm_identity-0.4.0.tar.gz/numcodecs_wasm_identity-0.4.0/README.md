4286de0b3e9ed51faa1e4196d8aaf84fc769eacb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 16:09:30 +0000

    Add package numcodecs-wasm-identity/0.4.0/numcodecs_wasm_identity-0.4.0.tar.gz

---
+++ packages/numcodecs-wasm-identity/numcodecs_wasm_identity-0.4.0.tar.gz/numcodecs_wasm_identity-0.4.0/README.md
@@ -0,0 +1,27 @@
+[![CI Status]][workflow] [![PyPi Release]][pypi] [![Read the Docs]][rtdocs]
+
+[CI Status]: https://img.shields.io/github/actions/workflow/status/juntyr/numcodecs-rs/ci.yml?branch=main
+[workflow]: https://github.com/juntyr/numcodecs-rs/actions/workflows/ci.yml?query=branch%3Amain
+
+[PyPi Release]: https://img.shields.io/pypi/v/numcodecs-wasm-identity.svg
+[pypi]: https://pypi.python.org/pypi/numcodecs-wasm-identity
+
+[Read the Docs]: https://img.shields.io/readthedocs/numcodecs-wasm?label=readthedocs
+[rtdocs]: https://numcodecs-wasm.readthedocs.io/en/stable/api/numcodecs_wasm_identity/
+
+# numcodecs-wasm-identity
+
+[`numcodecs`] compression for the [`numcodecs-identity`] codec compiled to WebAssembly.
+
+[`numcodecs`]: https://numcodecs.readthedocs.io/en/stable/
+[`numcodecs-identity`]: https://docs.rs/numcodecs-identity/
+
+## License
+
+Licensed under the Mozilla Public License, Version 2.0 ([LICENSE](LICENSE) or https://www.mozilla.org/en-US/MPL/2.0/).
+
+## Funding
+
+The `numcodecs-wasm-identity` package has been developed as part of [ESiWACE3](https://www.esiwace.eu), the third phase of the Centre of Excellence in Simulation of Weather and Climate in Europe.
+
+Funded by the European Union. This work has received funding from the European High Performance Computing Joint Undertaking (JU) under grant agreement No 101093054.
---
+++ packages/numcodecs-wasm-identity/numcodecs_wasm_identity-0.4.0.tar.gz/numcodecs_wasm_identity-0.4.0/src/numcodecs_wasm_identity/__init__.pyi
@@ -0,0 +1,88 @@
+__all__ = ['Identity']
+
+import numcodecs.abc
+
+class Identity(numcodecs.abc.Codec):
+    r"""
+    Identity codec which applies the identity function, i.e. passes through the
+    input unchanged during encoding and decoding.
+    
+    Parameters
+    ----------
+    _version : ..., optional, default = "1.0.0"
+        The codec's encoding format version. Do not provide this parameter explicitly.
+    """
+
+    def __init__(self, _version='1.0.0'): ...
+
+    codec_id = 'identity.rs'
+
+    def decode(self, buf, out=None):
+        r"""
+        Decode the data in `buf`.
+        
+        Parameters
+        ----------
+        buf : Buffer
+            Encoded data. May be any object supporting the new-style buffer
+            protocol.
+        out : Buffer, optional
+            Writeable buffer to store decoded data. N.B. if provided, this buffer must
+            be exactly the right size to store the decoded data.
+        
+        Returns
+        -------
+        dec : Buffer
+            Decoded data. May be any object supporting the new-style
+            buffer protocol.
+        """
+        ...
+
+    def encode(self, buf):
+        r"""
+        Encode the data in `buf`.
+        
+        Parameters
+        ----------
+        buf : Buffer
+            Data to be encoded. May be any object supporting the new-style
+            buffer protocol.
+        
+        Returns
+        -------
+        enc : Buffer
+            Encoded data. May be any object supporting the new-style buffer
+            protocol.
+        """
+        ...
+
+    @classmethod
+    def from_config(cls, config):
+        r"""
+        Instantiate the codec from a configuration [`dict`][dict].
+        
+        Parameters
+        ----------
+        config : dict
+            Configuration of the codec.
+        
+        Returns
+        -------
+        codec : Self
+            Instantiated codec.
+        """
+        ...
+
+    def get_config(self):
+        r"""
+        Returns the configuration of the codec.
+        
+        [`numcodecs.registry.get_codec(config)`][numcodecs.registry.get_codec]
+        can be used to reconstruct this codec from the returned config.
+        
+        Returns
+        -------
+        config : dict
+            Configuration of the codec.
+        """
+        ...
