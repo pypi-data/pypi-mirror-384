37816dfa755db889bbc1354a69d1a6806c34a8c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 05:07:40 +0000

    Add package plain.sessions/0.34.1/plain_sessions-0.34.1.tar.gz

---
+++ packages/plain.sessions/plain_sessions-0.34.1.tar.gz/plain_sessions-0.34.1/plain/sessions/chores.py
@@ -0,0 +1,13 @@
+from plain.chores import register_chore
+from plain.utils import timezone
+
+from .models import Session
+
+
+@register_chore("sessions")
+def clear_expired() -> str:
+    """
+    Delete sessions that have expired.
+    """
+    result = Session.query.filter(expires_at__lt=timezone.now()).delete()
+    return f"{result[0]} expired sessions deleted"
