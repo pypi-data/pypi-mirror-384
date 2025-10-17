a3dedc1dabe4f64c63ab16893b24f3185e9a742c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 03:19:46 +0000

    Add package gckc-py-teaching/0.0.0/gckc_py_teaching-0.0.0-py3-none-any.whl

---
+++ packages/gckc-py-teaching/gckc_py_teaching-0.0.0-py3-none-any.whl/gckc-py-teaching/__init__.py
@@ -0,0 +1,11 @@
+"""
+GCKC Py Teaching —— 惯成科创 Python 编程教学依赖软件包
+"""
+
+__version__ = "0.0.0"
+__author__ = "GCKC"
+__email__ = "1974498278@qq.com"
+
+from .demo import add
+
+__all__ = ["__version__", "__author__", "add"]
---
+++ packages/gckc-py-teaching/gckc_py_teaching-0.0.0-py3-none-any.whl/gckc-py-teaching/demo.py
@@ -0,0 +1,7 @@
+"""
+GCKC Py Teaching 测试 Demo 模块
+"""
+
+
+def add(num1, num2):
+    return num1 + num2
