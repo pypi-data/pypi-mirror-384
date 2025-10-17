c0995cbad4cdcf824550f691f52768e1d8591cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 14:06:33 +0000

    Add package ironoc-pytest-conor-heffron/2.1.4/ironoc_pytest_conor_heffron-2.1.4.tar.gz

---
+++ packages/ironoc-pytest-conor-heffron/ironoc_pytest_conor_heffron-2.1.4.tar.gz/ironoc_pytest_conor_heffron-2.1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+import os
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name="ironoc-pytest",
+    version="2.1.4",
+    install_requires=required,
+)
