7ebd353bde7ccd005a4acab8829432196fd33180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 03:19:44 +0000

    Add package aikido-zen/1.2.25/aikido_zen-1.2.25-py3-none-any.whl

---
+++ packages/aikido-zen/aikido_zen-1.2.25-py3-none-any.whl/aikido_zen/thread/process_worker.py
@@ -0,0 +1,31 @@
+import multiprocessing
+import time
+
+from aikido_zen.helpers.logging import logger
+from aikido_zen.thread import thread_cache
+
+# Renew the cache from this background worker every 15 seconds
+RENEW_CACHE_EVERY_X_SEC = 15
+
+
+def aikido_process_worker_thread():
+    """
+    process worker -> When a web server like gUnicorn makes new processes, and those have multiple threads,
+    Aikido process worker is linked to those new processes, so in essence it's 1 extra thread. This thread
+    is responsible for syncing statistics, route data, configuration, ...
+    """
+    # Get the current process
+    current_process = multiprocessing.current_process()
+
+    while True:
+        # Print information about the process
+        logger.debug(
+            f"Process ID: {current_process.pid}, Name: {current_process.name} - process_worker renewing thread cache."
+        )
+
+        # Renew the cache every 5 seconds
+        try:
+            thread_cache.renew()
+        except Exception as e:
+            logger.warn("An error occurred during data synchronization: %s", e)
+        time.sleep(RENEW_CACHE_EVERY_X_SEC)
