28c7a110591a8b8aebb1ae8c449d571d9349abae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 17:07:21 +0000

    Add package common-stats-thaichim/0.0.2/common_stats_thaichim-0.0.2.tar.gz

---
+++ packages/common-stats-thaichim/common_stats_thaichim-0.0.2.tar.gz/common_stats_thaichim-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+setup(
+    name='common_stats-thaichim',
+    version='0.0.2',
+    description='A simple library for basic statistical calculations',
+    long_description=open('USAGE.md').read(),
+    long_description_content_type='text/markdown',
+    author='Nawawan Thaichim',
+    author_email='woonzy09@email.com',
+    packages=find_packages(),
+    install_requires=[
+        "pytest"
+    ],
+    license='MIT',
+    python_requires='>=3.7',
+    ext_modules=cythonize("common_stats/statistics.pyx", compiler_directives={'language_level' : "3"}), 
+    
+)
