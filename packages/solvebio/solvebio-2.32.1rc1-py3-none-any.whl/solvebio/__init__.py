c21b9a1f4f0006d7b49acd4b135c2489174c56c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 07:07:02 +0000

    Add package solvebio/2.32.1rc1/solvebio-2.32.1rc1-py3-none-any.whl

---
+++ packages/solvebio/solvebio-2.32.1rc1-py3-none-any.whl/solvebio/__init__.py
@@ -0,0 +1,274 @@
+# -*- coding: utf-8 -*-
+"""
+SolveBio Python Client
+~~~~~~~~~~~~~~~~~~~~~~
+
+This is the Python client & library for the SolveBio API.
+
+Have questions or comments? email us at: support@solvebio.com
+"""
+__docformat__ = 'restructuredtext'
+
+import os as _os
+import errno
+import logging as _logging
+import warnings
+from typing import Literal
+from .help import open_help as _open_help
+
+# Capture warnings (specifically from urllib3)
+try:
+    _logging.captureWarnings(True)
+except:
+    # Python 2.6 doesn't support this
+    pass
+
+
+def help():
+    _open_help('/docs')
+
+
+def _init_logging():
+    loglevel_base = _os.environ.get('SOLVEBIO_LOGLEVEL', 'WARN')
+    loglevel_stream = _os.environ.get('SOLVEBIO_LOGLEVEL', 'WARN')
+    logfile = _os.environ.get('SOLVEBIO_LOGFILE', '~/.solvebio/solvebio.log')
+    loglevel_file = _os.environ.get('SOLVEBIO_LOGLEVEL', 'DEBUG')
+
+    base_logger = _logging.getLogger("solvebio")
+    base_logger.setLevel(loglevel_base)
+
+    # clear handlers if any exist
+    handlers = base_logger.handlers[:]
+    for handler in handlers:
+        base_logger.removeHandler(handler)
+        handler.close()
+
+    if loglevel_stream:
+        stream_handler = _logging.StreamHandler()
+        stream_handler.setLevel(loglevel_stream)
+        stream_fmt = _logging.Formatter('[SolveBio] %(message)s')
+        stream_handler.setFormatter(stream_fmt)
+        base_logger.addHandler(stream_handler)
+
+    if logfile:
+        logfile_path = _os.path.expanduser(logfile)
+        logdir = _os.path.dirname(logfile_path)
+
+        if not _os.path.isdir(logdir):
+            # Handle a race condition here when running
+            # multiple services that import this package.
+            try:
+                _os.makedirs(logdir)
+            except OSError as err:
+                # Re-raise anything other than 'File exists'.
+                if err.errno != errno.EEXIST:
+                    raise err
+
+        file_handler = _logging.FileHandler(logfile_path)
+        file_handler.setLevel(loglevel_file)
+        file_fmt = _logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(file_fmt)
+        base_logger.addHandler(file_handler)
+
+    try:
+        base_logger.addHandler(_logging.NullHandler())
+    except:
+        class NullHandler(_logging.Handler):
+            def emit(self, record):
+                pass
+
+        base_logger.addHandler(NullHandler())
+
+
+_init_logging()
+
+
+"""
+This is a cached value of client._host, and is kept in for backwards compatibility
+Use this with caution, as you should prefer relying on get_api_host() instead
+"""
+api_host = None
+
+
+def _set_cached_api_host(host):
+    global api_host
+    api_host = host
+
+
+from .version import VERSION  # noqa
+from .errors import SolveError
+
+
+_deprecation_warning_shown = False
+
+
+def print_deprecation_notice():
+    """
+    Shows the deprecation warning for the SolveBio Python client.
+    Only shows once per session to avoid repetition.
+    """
+    global _deprecation_warning_shown
+
+    if _deprecation_warning_shown:
+        return
+
+    _deprecation_warning_shown = True
+
+    message = """
+        !!! Deprecation Notice
+
+        The SolveBio Python client is deprecated and will no longer be maintained after March 31, 2026.
+
+        We recommend migrating to the QuartzBio python client:
+        https://github.com/quartzbio/quartzbio-python
+
+        """
+
+    if api_host is not None:
+        message += f"\nOr using the QuartzBio REST API: {api_host.replace('.api', '')}/swagger"
+
+    # Show warning and also print to ensure visibility
+    warnings.warn(
+        message,
+        DeprecationWarning,
+        stacklevel=2,
+    )
+    print(f"{message}")
+
+
+from .query import Query, BatchQuery, Filter, GenomicFilter
+from .global_search import GlobalSearch
+from .annotate import Annotator, Expression
+from .client import client, SolveClient
+from .resource import (
+    Application,
+    Beacon,
+    BeaconSet,
+    Dataset,
+    DatasetCommit,
+    DatasetExport,
+    DatasetField,
+    DatasetImport,
+    DatasetMigration,
+    DatasetTemplate,
+    DatasetRestoreTask,
+    DatasetSnapshotTask,
+    Group,
+    Manifest,
+    Object,
+    User,
+    Vault,
+    VaultSyncTask,
+    ObjectCopyTask,
+    SavedQuery,
+    Task
+)
+
+
+def login(
+    api_host: str = None,
+    api_key: str = None,
+    access_token: str = None,
+    name: str = None,
+    version: str = None,
+    debug: bool = False,
+):
+    """
+    Function to login to the QuartzBio/EDP API when using EDP in a python script.
+    Note that another function is used when CLI command `quartzbio login` is used!
+    EDP checks user credentials & host URL from multiple sources, in the following order:
+
+    1) Parameters provided (e.g. the parameters of this function)
+    2) Environment variables (if the above parameters weren't provided)
+    3) quartzbio credentials file stored in the user's HOME directory
+        (if parameters and environment variables weren't found)
+
+    :param api_host: the QuartzBio EDP instance's URL to access.
+    :param access_token: your user's access token, which you can generate at the EDP website
+        (user menu > `Personal Access Tokens`)
+    :param api_key: Your API key. You can use this instead of providing an access token
+    :param name: name
+    :param version: version
+
+    Example:
+        .. code-block:: python
+
+            import quartzbio
+            quartzbio.login(
+                api_host="https://solvebio.api.az.aws.quartz.bio",
+                api_key=YOUR_API_KEY
+            )
+    """
+    token_type: Literal["Bearer", "Token"] = None
+    token: str = None
+
+    if access_token:
+        token_type = "Bearer"
+        token = access_token
+    elif api_key:
+        token_type = "Token"
+        token = api_key
+
+    if api_host or token or debug:
+        client.set_credentials(
+            api_host, token, token_type=token_type, raise_on_missing=not debug, debug=debug
+        )
+        # Show deprecation notice after credentials are set
+        print_deprecation_notice()
+
+    client.set_user_agent(name=name, version=version)
+
+
+def whoami():
+    try:
+        user = client.whoami()
+    except Exception as e:
+        print("{} (code: {})".format(e.message, e.status_code))
+    else:
+        return user
+
+
+def get_api_host():
+    global api_host
+    api_host = client._host
+
+    return client._host
+
+
+get_api_host()
+
+
+__all__ = [
+    'Annotator',
+    'Application',
+    'Expression',
+    'BatchQuery',
+    'Beacon',
+    'BeaconSet',
+    'Dataset',
+    'DatasetCommit',
+    'DatasetField',
+    'DatasetExport',
+    'DatasetImport',
+    'DatasetMigration',
+    'DatasetTemplate',
+    'DatasetRestoreTask',
+    'DatasetSnapshotTask',
+    'Filter',
+    'GenomicFilter',
+    'GlobalSearch',
+    'Group',
+    'Manifest',
+    'Object',
+    'ObjectCopyTask',
+    'Query',
+    'SavedQuery',
+    'SolveClient',
+    'SolveError',
+    'VaultSyncTask',
+    'Task',
+    'Vault',
+    'User',
+    'VERSION'
+]
---
+++ packages/solvebio/solvebio-2.32.1rc1-py3-none-any.whl/solvebio/cli/auth.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+import solvebio
+from ..client import client
+from .credentials import get_credentials
+from .credentials import save_credentials
+from .credentials import delete_credentials
+
+
+def login_and_save_credentials(*args):
+    """
+    CLI command to login and persist credentials to a file
+    """
+    args = args[0]
+
+    solvebio.login(
+        api_host=args.api_host,
+        api_key=args.api_key,
+        access_token=args.access_token,
+        # name=args.name,
+        # version=args.version,
+        debug=args.debug,
+    )
+
+    # Print information about the current user
+    if not client.is_logged_in():
+        print("login: client is not logged in!")
+        solvebio.print_deprecation_notice()
+
+        # Verify if user has provided the wrong credentials file
+        if client._host:
+            if suggested_host := client.validate_host_is_www_url(client._host):
+                print(
+                    f"Provided API host is: `{client._host}`. "
+                    f"Did you perhaps mean `{suggested_host}`?"
+                )
+        return
+
+    user = client.whoami()
+    print_user(user)
+
+    # fixme: how to detect if login was successful
+    save_credentials(
+        user["email"].lower(),
+        client._auth.token,
+        client._auth.token_type,
+        solvebio.get_api_host(),
+    )
+    print("Updated local credentials file.")
+
+
+def logout(*args):
+    """
+    Delete's the user's locally-stored credentials.
+    """
+    if get_credentials():
+        delete_credentials()
+        print("You have been logged out.")
+    else:
+        print("You are not logged-in.")
+    solvebio.print_deprecation_notice()
+
+
+def whoami(*args, **kwargs):
+    """
+    Prints information about the current user.
+    Assumes the user is already logged-in.
+    """
+    try:
+        user = client.whoami()
+    except Exception as e:
+        print("{} (code: {})".format(e.message, e.status_code))
+    else:
+        print_user(user)
+
+
+def print_user(user):
+    """
+    Prints information about the current user.
+    """
+    email = user["email"]
+    domain = user["account"]["domain"]
+    print(
+        f'You are logged-in to the "{domain}" domain as {email}'
+        f" (server: {solvebio.get_api_host()})."
+    )
