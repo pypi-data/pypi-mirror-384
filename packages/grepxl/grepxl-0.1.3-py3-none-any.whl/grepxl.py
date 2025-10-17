369c6c436a9bf8ec66099ba280bfc50b83941cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 04:07:44 +0000

    Add package grepxl/0.1.3/grepxl-0.1.3-py3-none-any.whl

---
+++ packages/grepxl/grepxl-0.1.3-py3-none-any.whl/grepxl.py
@@ -0,0 +1,44 @@
+import argparse
+from importlib.metadata import version
+
+import pandas as pd
+from rich import print
+from rich_tools import df_to_table
+
+def main():
+    """
+    Display rows of a dataframe from an Excel file that match a given pattern.
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('pattern', help='Regular expression pattern to search for')
+    parser.add_argument('excel', help='Excel file')
+    parser.add_argument('--version', action='version', version=version('grepxl'))
+    args = parser.parse_args()
+
+    pattern = args.pattern
+    excel = args.excel
+
+    data = pd.read_excel(excel, engine='calamine').dropna(axis='columns', how='all')
+    search = grep(pattern, data)
+    print(df_to_table(search, show_index=False))
+
+
+def grep(pattern:str, data:pd.DataFrame) -> pd.DataFrame:
+    """
+    Filter rows in a DataFrame that contain the given pattern in any column.
+    """
+    mask = None
+    for col in data.columns:
+        update = data[col].astype(str).str.contains(pattern)
+        
+        if mask is None:
+            mask = update
+        else:
+            mask = mask | update
+
+    return data[mask]
+
+
+if __name__ == "__main__":
+    main()
