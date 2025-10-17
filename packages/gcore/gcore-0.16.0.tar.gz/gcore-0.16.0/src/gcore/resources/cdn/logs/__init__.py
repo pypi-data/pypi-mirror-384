49c752b4b3a2648e8901adebf7693f4a4c4484f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 18:19:00 +0000

    Add package gcore/0.16.0/gcore-0.16.0.tar.gz

---
+++ packages/gcore/gcore-0.16.0.tar.gz/gcore-0.16.0/src/gcore/resources/cdn/logs/__init__.py
@@ -0,0 +1,19 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .logs import (
+    LogsResource,
+    AsyncLogsResource,
+    LogsResourceWithRawResponse,
+    AsyncLogsResourceWithRawResponse,
+    LogsResourceWithStreamingResponse,
+    AsyncLogsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "LogsResource",
+    "AsyncLogsResource",
+    "LogsResourceWithRawResponse",
+    "AsyncLogsResourceWithRawResponse",
+    "LogsResourceWithStreamingResponse",
+    "AsyncLogsResourceWithStreamingResponse",
+]
