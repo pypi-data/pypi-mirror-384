c6c84f9947943554d77261d37c784188e219b958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 06:07:27 +0000

    Add package lgatr/1.3.1/lgatr-1.3.1-py3-none-any.whl

---
+++ packages/lgatr/lgatr-1.3.1-py3-none-any.whl/lgatr/__init__.py
@@ -0,0 +1,15 @@
+from .interface.spurions import get_num_spurions, get_spurions
+from .interface.scalar import embed_scalar, extract_scalar
+from .interface.vector import embed_vector, extract_vector
+from .interface.pseudoscalar import embed_pseudoscalar, extract_pseudoscalar
+from .interface.axialvector import embed_axialvector, extract_axialvector
+from .layers.attention.config import SelfAttentionConfig, CrossAttentionConfig
+from .layers.mlp.config import MLPConfig
+from .nets.lgatr import LGATr
+from .nets.conditional_lgatr import ConditionalLGATr
+from .primitives.config import gatr_config
+
+# ---- version ----
+from importlib.metadata import version as _pkg_version
+
+__version__ = _pkg_version("lgatr")
