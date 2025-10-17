b551109738d5f1cedbb6b53325170453b1c068a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 08:07:52 +0000

    Add package pbi-core/0.6.5/pbi_core-0.6.5-py3-none-any.whl

---
+++ packages/pbi-core/pbi_core-0.6.5-py3-none-any.whl/pbi_core/ssas/trace/__init__.py
@@ -0,0 +1,4 @@
+from .errors import NoQueryError
+from .main import Performance, PerformanceTrace, get_performance
+
+__all__ = ["NoQueryError", "Performance", "PerformanceTrace", "get_performance"]
