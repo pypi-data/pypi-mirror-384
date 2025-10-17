f4da3458475ecc46f4f360dc4151dcfa6a5c834a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 05:07:41 +0000

    Add package epitran/1.29.0/epitran-1.29.0.tar.gz

---
+++ packages/epitran/epitran-1.29.0.tar.gz/epitran-1.29.0/epitran/bin/uigtransliterate.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import fileinput
+import epitran
+
+epi = epitran.Epitran('uig-Arab')
+for line in fileinput.input():
+    s = epi.transliterate(line.strip().decode('utf-8'))
+    print(s.encode('utf-8'))
