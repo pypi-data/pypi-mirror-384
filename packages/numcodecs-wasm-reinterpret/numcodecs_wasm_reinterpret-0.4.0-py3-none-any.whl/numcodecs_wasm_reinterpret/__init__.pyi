1f0072eadbc1718d31017ac7179c15eab92b1ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 16:08:55 +0000

    Add package numcodecs-wasm-reinterpret/0.4.0/numcodecs_wasm_reinterpret-0.4.0-py3-none-any.whl

---
+++ packages/numcodecs-wasm-reinterpret/numcodecs_wasm_reinterpret-0.4.0-py3-none-any.whl/numcodecs_wasm_reinterpret/__init__.pyi
@@ -0,0 +1,96 @@
+__all__ = ['Reinterpret']
+
+import numcodecs.abc
+
+class Reinterpret(numcodecs.abc.Codec):
+    r"""
+    Codec to reinterpret data between different compatible types.
+    
+    Note that no conversion happens, only the meaning of the bits changes.
+    
+    Reinterpreting to bytes, or to a same-sized unsigned integer type, or
+    without the changing the dtype are supported.
+    
+    Parameters
+    ----------
+    decode_dtype : ...
+        Dtype of the decoded data
+    encode_dtype : ...
+        Dtype of the encoded data.
+    _version : ..., optional, default = "1.0.0"
+        The codec's encoding format version. Do not provide this parameter explicitly.
+    """
+
+    def __init__(self, decode_dtype, encode_dtype, _version='1.0.0'): ...
+
+    codec_id = 'reinterpret.rs'
+
+    def decode(self, buf, out=None):
+        r"""
+        Decode the data in `buf`.
+        
+        Parameters
+        ----------
+        buf : Buffer
+            Encoded data. May be any object supporting the new-style buffer
+            protocol.
+        out : Buffer, optional
+            Writeable buffer to store decoded data. N.B. if provided, this buffer must
+            be exactly the right size to store the decoded data.
+        
+        Returns
+        -------
+        dec : Buffer
+            Decoded data. May be any object supporting the new-style
+            buffer protocol.
+        """
+        ...
+
+    def encode(self, buf):
+        r"""
+        Encode the data in `buf`.
+        
+        Parameters
+        ----------
+        buf : Buffer
+            Data to be encoded. May be any object supporting the new-style
+            buffer protocol.
+        
+        Returns
+        -------
+        enc : Buffer
+            Encoded data. May be any object supporting the new-style buffer
+            protocol.
+        """
+        ...
+
+    @classmethod
+    def from_config(cls, config):
+        r"""
+        Instantiate the codec from a configuration [`dict`][dict].
+        
+        Parameters
+        ----------
+        config : dict
+            Configuration of the codec.
+        
+        Returns
+        -------
+        codec : Self
+            Instantiated codec.
+        """
+        ...
+
+    def get_config(self):
+        r"""
+        Returns the configuration of the codec.
+        
+        [`numcodecs.registry.get_codec(config)`][numcodecs.registry.get_codec]
+        can be used to reconstruct this codec from the returned config.
+        
+        Returns
+        -------
+        config : dict
+            Configuration of the codec.
+        """
+        ...
