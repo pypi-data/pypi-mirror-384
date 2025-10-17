bc6c577b76c7432490eed903b430fbe171bad373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 04:07:00 +0000

    Add package tibanna/5.6.0b4/tibanna-5.6.0b4-py3-none-any.whl

---
+++ packages/tibanna/tibanna-5.6.0b4-py3-none-any.whl/tibanna/lambdas/update_cost_awsem.py
@@ -0,0 +1,20 @@
+from tibanna.update_cost import update_cost
+from tibanna.vars import AWS_REGION
+
+
+config = {
+    'function_name': 'update_cost_awsem',
+    'function_module': 'service',
+    'function_handler': 'handler',
+    'handler': 'service.handler',
+    'region': AWS_REGION,
+    'runtime': 'python3.11',
+    'role': 'tibanna_lambda_init_role',
+    'description': 'update costs of a workflow run',
+    'timeout': 800,
+    'memory_size': 256
+}
+
+
+def handler(event, context):
+    return update_cost(event)
