79525020d55e2c1db52df93e400a1fca25103cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 16:09:12 +0000

    Add package bosa-core-binary/0.9.2b2/bosa_core_binary-0.9.2b2-cp312-cp312-win_amd64.whl

---
+++ packages/bosa-core-binary/bosa_core_binary-0.9.2b2-cp312-cp312-win_amd64.whl/bosa_core/authentication/token/repository/sqlalchemy/models.pyi
@@ -0,0 +1,11 @@
+from _typeshed import Incomplete
+from bosa_core.authentication.database import DatabaseAdapter as DatabaseAdapter
+
+class DBToken(DatabaseAdapter.base):
+    """Token SQLAlchemy model."""
+    __tablename__: str
+    id: Incomplete
+    user_id: Incomplete
+    client_id: Incomplete
+    is_revoked: Incomplete
+    user: Incomplete
