71c5db22ef426002b5cd06ceb29316c7750b7b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 06:08:05 +0000

    Add package cloudsss/0.1.0/cloudsss-0.1.0.tar.gz

---
+++ packages/cloudsss/cloudsss-0.1.0.tar.gz/cloudsss-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cloudsss",
+    version="0.1.0",
+    author="Ranjeev Ramprasad",
+    author_email="ranjeevramprasad@gmail.com",
+    description="A simple example package",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+)
