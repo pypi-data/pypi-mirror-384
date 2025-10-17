6cebc326fcbc09ee9545c8fa777a4c59890d9cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 04:07:31 +0000

    Add package amulet-anvil/1.0.2a2/amulet_anvil-1.0.2a2-cp312-cp312-macosx_10_15_universal2.whl

---
+++ packages/amulet-anvil/amulet_anvil-1.0.2a2-cp312-cp312-macosx_10_15_universal2.whl/amulet/anvil/__init__.pyi
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import typing
+
+import amulet.nbt
+from amulet.anvil.dimension import AnvilDimension, AnvilDimensionLayer
+from amulet.anvil.region import AnvilRegion, RegionDoesNotExist
+
+from . import _amulet_anvil, _version, dimension, region
+
+__all__: list[str] = [
+    "AnvilDimension",
+    "AnvilDimensionLayer",
+    "AnvilRegion",
+    "RawChunkType",
+    "RegionDoesNotExist",
+    "compiler_config",
+    "dimension",
+    "region",
+]
+
+def _init() -> None: ...
+
+RawChunkType: typing.TypeAlias = dict[str, amulet.nbt.NamedTag]
+__version__: str
+compiler_config: dict
