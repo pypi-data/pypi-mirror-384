154b4b009ba0912ff64bff03b81c5c3162b2ac23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 06:07:53 +0000

    Add package catLang/0.0.2/catlang-0.0.2.tar.gz

---
+++ packages/catLang/catlang-0.0.2.tar.gz/catlang-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="catLang",
+    version="0.0.2",
+    packages=find_packages(),
+    install_requires=[
+        "regex>=1.0.0"  # your library depends on regex
+    ],
+    author="catLang",
+    description="CatLang: Custom transpiller from catLang code into Python code.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourname/catLang",  # optional
+    classifiers=[
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
