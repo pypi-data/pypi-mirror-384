cf9923dc0bd6a2cbcd66e23b9e8c5a05ee761f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 15:19:19 +0000

    Add package imsosorrybutinc/0.7.1/imsosorrybutinc-0.7.1-cp314-cp314t-musllinux_1_2_x86_64.whl

---
+++ packages/imsosorrybutinc/imsosorrybutinc-0.7.1-cp314-cp314t-musllinux_1_2_x86_64.whl/uwuifier/__init__.py
@@ -0,0 +1,21 @@
+""":3."""
+
+from ._uwuifier import (
+    char_replace,
+    emoji,
+    nyaify,
+    stutter,
+    tildify,
+    uwuify,
+    word_replace,
+)
+
+__all__ = [
+    "char_replace",
+    "emoji",
+    "nyaify",
+    "stutter",
+    "tildify",
+    "uwuify",
+    "word_replace",
+]
