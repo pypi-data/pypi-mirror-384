096b5b8a49051310666a6e3eed90c069ca17b2ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 05:08:06 +0000

    Add package async-tkinter-loop/0.10.2/async_tkinter_loop-0.10.2.tar.gz

---
+++ packages/async-tkinter-loop/async_tkinter_loop-0.10.2.tar.gz/async_tkinter_loop-0.10.2/async_tkinter_loop/mixins.py
@@ -0,0 +1,25 @@
+import sys
+import tkinter as tk
+
+from async_tkinter_loop import async_mainloop
+
+
+class AsyncTk:
+    def async_mainloop(self: tk.Tk) -> None:
+        async_mainloop(self)
+
+
+class AsyncCTk(AsyncTk):
+    def async_mainloop(self) -> None:
+        # Based on the code from CustomTkinter by Tom Schimansky
+        # Source https://github.com/TomSchimansky/CustomTkinter/blob/d719950f80eb2768db96bd4cc627523e99603b1b/customtkinter/windows/ctk_tk.py#L155
+        if not self._window_exists:
+            if sys.platform.startswith("win"):
+                self._windows_set_titlebar_color(self._get_appearance_mode())
+
+                if not self._withdraw_called_before_window_exists and not self._iconify_called_before_window_exists:
+                    self.deiconify()
+
+            self._window_exists = True
+
+        super().async_mainloop()
