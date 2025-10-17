87ad1026b43e67ed8ec329b63ba4850201e0d37d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 15:18:14 +0000

    Add package anchorbrowser/0.1.1/anchorbrowser-0.1.1-py3-none-any.whl

---
+++ packages/anchorbrowser/anchorbrowser-0.1.1-py3-none-any.whl/anchorbrowser/types/sessions/mouse_down_response.py
@@ -0,0 +1,11 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+
+from ..._models import BaseModel
+
+__all__ = ["MouseDownResponse"]
+
+
+class MouseDownResponse(BaseModel):
+    status: Optional[str] = None
