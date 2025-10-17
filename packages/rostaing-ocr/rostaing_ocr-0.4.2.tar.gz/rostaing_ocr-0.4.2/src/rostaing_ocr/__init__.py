10d3cb5abf3ca1594920d716f93bc874a7c666fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 06:08:17 +0000

    Add package rostaing-ocr/0.4.2/rostaing_ocr-0.4.2.tar.gz

---
+++ packages/rostaing-ocr/rostaing_ocr-0.4.2.tar.gz/rostaing_ocr-0.4.2/src/rostaing_ocr/__init__.py
@@ -0,0 +1,6 @@
+# Fichier : src/rostaing_ocr/__init__.py
+
+from .rostaing_ocr import RostaingOCR
+
+# Optionnel: définir ce qui est exporté quand on fait 'from rostaing_ocr import *'
+__all__ = ['RostaingOCR']
