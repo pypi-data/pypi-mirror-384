95b224a50088596e9ba34724027e414904f676cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 17:06:37 +0000

    Add package jamlib/2.5.4/jamlib-2.5.4.tar.gz

---
+++ packages/jamlib/jamlib-2.5.4.tar.gz/jamlib-2.5.4/src/jam/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+JAM - Universal auth* library
+
+Source code: https://github.com/lyaguxafrog/jam
+Documentation: https://jam.makridenko.ru
+"""
+
+from jam.__abc_instances__ import BaseJam
+from jam.instance import Jam
+
+
+__version__ = "2.5.3"
+__all__ = ["Jam", "BaseJam"]
