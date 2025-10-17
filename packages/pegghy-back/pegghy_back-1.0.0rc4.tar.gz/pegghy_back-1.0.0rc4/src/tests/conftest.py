78a0a6f8b9f0c919c3fbcc800a71d701eeebadcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 17:07:00 +0000

    Add package pegghy-back/1.0.0rc4/pegghy_back-1.0.0rc4.tar.gz

---
+++ packages/pegghy-back/pegghy_back-1.0.0rc4.tar.gz/pegghy_back-1.0.0rc4/src/tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from src.pegghy_back.app import app
+
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    app.config["SERVER_NAME"] = "TEST"
+    app.config["DATA_FOLDER"] = "./data/"
+    client = app.test_client()
+    client.headers = {"Content-type": "application/json", "Accept": "application/json"}
+    yield client
