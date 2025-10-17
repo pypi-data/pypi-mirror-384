049bb8ffb14eddf70922302730766ac2f773a8c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 18:18:15 +0000

    Add package bikram/2.1.4/bikram-2.1.4.tar.gz

---
+++ packages/bikram/bikram-2.1.4.tar.gz/bikram-2.1.4/docs/history.rst
@@ -0,0 +1,37 @@
+=======
+History
+=======
+
+2.1.3 to 2.1.4 (2025-10-17)
+---------------------------
+* Few changes have been made to make the date map accurate as reported by several contributors.
+* 2082 and 2083 BS Date changes and confirmed as per the Nepal RajPatra
+* 2.1.4 - Bump to push the change log
+
+2.1.0 to 2.1.3 (2024-06-20)
+---------------------------
+* Few changes have been made to make the date map accurate as reported by several contributors.
+* 2062 BS Date changes and confirmed as per the Nepal RajPatra
+* 2081 BS Date changes and confirmed as per the calendar published in 2081
+* out of bound checks as suggested by Dipendra Nath (dipee), FIRST_AD_YEAR, LAST_AD_YEAR constanst introduced.
+* 2.1.3 - Bump to push the change log
+
+1.0.0 (2018-03-15)
+------------------
+
+* Added usage guide and module docs.
+* Added :code:`pipenv` support.
+* Removed :code:`tox`.
+* Removed support for python versions older than 3.6.
+
+
+0.1.2 (2016-11-13)
+------------------
+
+* Minor test fixes.
+
+
+0.1.0 (2016-11-13)
+------------------
+
+* First release on PyPI.
---
+++ packages/bikram/bikram-2.1.4.tar.gz/bikram-2.1.4/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("docs/history.rst") as history_file:
+    history = history_file.read()
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name="bikram",
+    version="2.1.4",
+    description="Utilities to work with Bikram/Vikram Samwat dates.",
+    long_description=readme + "\n\n" + history,
+    authors=["keshaB Paudel", "aerawatcorp", "acpmasquerade"],
+    author_email="aerawatcorp@gmail.com",
+    url="https://github.com/aerawatcorp/bikram",
+    packages=[
+        "bikram",
+    ],
+    package_dir={"bikram": "bikram"},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords="bikram bikram samwat vikram samwat nepali date",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+    ],
+    test_suite="tests",
+    tests_require=test_requirements,
+)
