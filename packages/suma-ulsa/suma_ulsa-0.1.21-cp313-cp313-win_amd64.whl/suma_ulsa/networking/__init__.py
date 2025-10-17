5b8d3b675a667ca4312ed05d61413369ea9bf89f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 05:07:19 +0000

    Add package suma-ulsa/0.1.21/suma_ulsa-0.1.21-cp313-cp313-win_amd64.whl

---
+++ packages/suma-ulsa/suma_ulsa-0.1.21-cp313-cp313-win_amd64.whl/suma_ulsa/networking/__init__.py
@@ -0,0 +1,13 @@
+from .networking import (
+    SubnetCalculator,
+    VLSMCalculator,
+    SubnetRow,
+)
+
+__all__ = [
+    "SubnetCalculator",
+    "SubnetRow",
+    "VLSMCalculator"
+]
+if False:
+    from .networking import *  # Ayuda a linters
