40eaabe1c6aa52e7cf119b74b69d24742c1c004e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 18:18:53 +0000

    Add package tremolo/0.4.8/tremolo-0.4.8.tar.gz

---
+++ packages/tremolo/tremolo-0.4.8.tar.gz/tremolo-0.4.8/tremolo/exceptions.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2023 nggit
+
+from .lib.http_exceptions import (  # noqa: F401
+    TremoloException,
+    HTTPException,
+    BadRequest,
+    Unauthorized,
+    Forbidden,
+    NotFound,
+    MethodNotAllowed,
+    RequestTimeout,
+    PreconditionFailed,
+    PayloadTooLarge,
+    RangeNotSatisfiable,
+    ExpectationFailed,
+    TooManyRequests,
+    InternalServerError,
+    ServiceUnavailable,
+    WebSocketException,
+    WebSocketClientClosed,
+    WebSocketServerClosed
+)
+
+
+class ASGIException(TremoloException):
+    message = 'ASGIException'
+
+
+class LifespanError(ASGIException):
+    pass
+
+
+class LifespanProtocolUnsupported(ASGIException):
+    message = 'ASGI Lifespan Protocol is not supported by your application'
