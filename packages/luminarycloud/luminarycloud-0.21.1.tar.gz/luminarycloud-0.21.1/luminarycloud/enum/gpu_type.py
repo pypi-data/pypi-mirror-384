32586742026c1a4d260fed3ede48106a0e3c8066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 14:06:57 +0000

    Add package luminarycloud/0.21.1/luminarycloud-0.21.1.tar.gz

---
+++ packages/luminarycloud/luminarycloud-0.21.1.tar.gz/luminarycloud-0.21.1/luminarycloud/enum/gpu_type.py
@@ -0,0 +1,12 @@
+from enum import IntEnum
+from .._proto.api.v0.luminarycloud.simulation import simulation_pb2 as simulationpb
+
+
+class GPUType(IntEnum):
+    """
+    Represents a GPU type.
+    """
+
+    UNSPECIFIED = simulationpb.SimulationOptions.GPU_TYPE_UNSPECIFIED
+    V100 = simulationpb.SimulationOptions.GPU_TYPE_V100
+    A100 = simulationpb.SimulationOptions.GPU_TYPE_A100
