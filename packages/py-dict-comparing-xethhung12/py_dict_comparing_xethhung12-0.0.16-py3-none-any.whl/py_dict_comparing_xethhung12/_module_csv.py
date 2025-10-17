107812ba7ddbe203dc58c40cfc68bbcff84e7b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 15:19:14 +0000

    Add package py-dict-comparing-xethhung12/0.0.16/py_dict_comparing_xethhung12-0.0.16-py3-none-any.whl

---
+++ packages/py-dict-comparing-xethhung12/py_dict_comparing_xethhung12-0.0.16-py3-none-any.whl/py_dict_comparing_xethhung12/_module_csv.py
@@ -0,0 +1,20 @@
+
+import csv
+from typing import Generator, List
+
+def load_win_based_CSV(path) -> List[dict]:
+    with open(path, "r", encoding="utf-8-sig") as f:
+        return list(load_win_based_CSV_generator(path))
+        # reader = csv.DictReader(f, delimiter='|', quotechar='"')
+        # lines = []
+        # for row in reader:
+        #     lines.append(row)
+        # print(f"Complete loading the file[{path}]")
+        # return lines
+
+def load_win_based_CSV_generator(path) -> Generator[dict, None, None]:
+    with open(path, "r", encoding="utf-8-sig") as f:
+        reader = csv.DictReader(f, delimiter='|', quotechar='"')
+        for row in reader:
+            yield row
+    print(f"Complete loading the file[{path}]")
