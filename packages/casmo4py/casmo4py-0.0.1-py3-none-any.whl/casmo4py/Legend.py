facdc6809ec8d7eb25a737755242ff87c5fa384c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 06:07:15 +0000

    Add package casmo4py/0.0.1/casmo4py-0.0.1-py3-none-any.whl

---
+++ packages/casmo4py/casmo4py-0.0.1-py3-none-any.whl/casmo4py/Legend.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+""" """
+
+
+class Legend:
+    def __init__(self):
+        """
+        Args:
+            bla (test.type): does blub
+        """
+
+        print("hello Legend")
+
+
+if __name__ == "__main__":
+    m = Legend()
---
+++ packages/casmo4py/casmo4py-0.0.1-py3-none-any.whl/casmo4py/Protocol.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+""" """
+
+
+class Protocol:
+    def __init__(self):
+        """
+        Args:
+            bla (test.type): does blub
+        """
+
+        print("hello protocol")
+
+
+if __name__ == "__main__":
+    m = Protocol()
