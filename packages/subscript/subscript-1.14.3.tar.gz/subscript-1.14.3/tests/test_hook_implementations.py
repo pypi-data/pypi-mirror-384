c2f03288d6490c92a0e773f388007aa3c320dca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 17:07:23 +0000

    Add package subscript/1.14.3/subscript-1.14.3.tar.gz

---
+++ packages/subscript/subscript-1.14.3.tar.gz/subscript-1.14.3/tests/test_hook_implementations.py
@@ -0,0 +1,75 @@
+import pytest
+import rstcheck_core.checker
+from ert.plugins.plugin_manager import ErtPluginManager
+
+import subscript.hook_implementations.forward_model_steps
+
+EXPECTED_STEPS = [
+    "CHECK_SWATINIT",
+    "CASEGEN_UPCARS",
+    "CSV2OFMVOL",
+    "CSV_STACK",
+    "ECLCOMPRESS",
+    "ECLDIFF2ROFF",
+    "ECLGRID2ROFF",
+    "ECLINIT2ROFF",
+    "ECLRST2ROFF",
+    "GRAV_SUBS_MAPS",
+    "GRAV_SUBS_POINTS",
+    "INTERP_RELPERM",
+    "MERGE_RFT_ERTOBS",
+    "MERGE_UNRST_FILES",
+    "OFMVOL2CSV",
+    "PARAMS2CSV",
+    "PRTVOL2CSV",
+    "SUNSCH",
+    "WELLTEST_DPDS",
+]
+
+# Avoid category inflation. Add to this list when it makes sense:
+ACCEPTED_STEP_CATEGORIES = ["modelling", "utility"]
+
+
+def test_hooks_are_installed_in_erts_plugin_manager():
+    """Test that we have the correct set of steps installed,
+    nothing more, nothing less"""
+    plugin_m = ErtPluginManager(
+        plugins=[subscript.hook_implementations.forward_model_steps]
+    )
+
+    available_fm_steps = [step().name for step in plugin_m.forward_model_steps]
+    assert set(EXPECTED_STEPS) == set(available_fm_steps)
+
+
+@pytest.mark.integration
+def test_executables_exists():
+    """Test executables requested exist in $PATH"""
+    plugin_m = ErtPluginManager(
+        plugins=[subscript.hook_implementations.forward_model_steps]
+    )
+
+    steps = [step() for step in plugin_m.forward_model_steps]
+    for step in steps:
+        assert step.executable is not None
+
+
+def test_hook_implementations_docs():
+    """For each installed job, we require the associated
+    description string to be nonempty, and valid RST markup"""
+
+    plugin_m = ErtPluginManager(
+        plugins=[subscript.hook_implementations.forward_model_steps]
+    )
+
+    steps = [step() for step in plugin_m.forward_model_steps]
+
+    for step in steps:
+        docs = step.documentation()
+        assert docs is not None, f"{step.name} has no docs"
+        assert docs.description, f"{step.name} has no description"
+        assert not list(rstcheck_core.checker.check_source(docs.description)), (
+            f"{step.name} has invalid RST syntax"
+        )
+        category = docs.category
+        assert category != "other"
+        assert category.split(".")[0] in ACCEPTED_STEP_CATEGORIES
---
+++ packages/subscript/subscript-1.14.3.tar.gz/subscript-1.14.3/tests/utils.py
@@ -0,0 +1,68 @@
+import contextlib
+import subprocess
+import time
+from os import getcwd
+
+
+def run_simulator(simulator, data_file_path):
+    """Run the given simulator (Eclipse100 or OPM-flow)
+    on a DATA file
+
+    Will write to cwd. Caller is responsible for starting
+    in a suitable directory.
+
+    If the simulator fails, the stdout and stderr will be printed.
+
+    Args:
+        simulator (string): Path to a working reservoir simulator
+            executable
+        data_file_path (str): Location of DATA file
+    Returns:
+        None
+    """
+    simulator_option = []
+    if "eclrun" in simulator:
+        simulator_option = ["eclipse"]
+    if "flow" in simulator:
+        simulator_option = ["--parsing-strictness=low"]
+
+    result = subprocess.run(  # pylint: disable=subprocess-run-check
+        [simulator, *simulator_option, data_file_path],
+        capture_output=True,
+        check=False,
+    )
+    if (
+        "eclrun" in simulator
+        and _error_count_from_ecl_stdout(
+            result.stdout.decode() + result.stderr.decode()
+        )
+        > 0
+    ):
+        # eclrun returns returncode 0 matter what.
+        result.returncode = 1
+
+    if (
+        result.returncode != 0
+        and "ecl" in simulator
+        and "LICENSE FAILURE" in result.stdout.decode() + result.stderr.decode()
+    ):
+        print("Eclipse failed due to license server issues. Retrying in 30 seconds.")
+        time.sleep(30)
+        result = subprocess.run(
+            [simulator, *simulator_option, data_file_path],
+            capture_output=True,
+        )
+
+    if result.returncode != 0:
+        print(result.stdout.decode())
+        print(result.stderr.decode())
+        raise AssertionError(f"reservoir simulator failed in {getcwd()}")
+
+
+def _error_count_from_ecl_stdout(stdouterr: str):
+    error_count = 0
+    for line in stdouterr.splitlines():
+        if line.startswith(" Errors"):
+            with contextlib.suppress(ValueError):
+                error_count = int(line.split("Errors")[1].strip())
+    return error_count
