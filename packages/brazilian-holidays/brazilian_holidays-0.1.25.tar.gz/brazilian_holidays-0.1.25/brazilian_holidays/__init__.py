c7bf8f5b2f45b46678d118a65c3b8cf33f476b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 07:07:22 +0000

    Add package brazilian-holidays/0.1.25/brazilian_holidays-0.1.25.tar.gz

---
+++ packages/brazilian-holidays/brazilian_holidays-0.1.25.tar.gz/brazilian_holidays-0.1.25/brazilian_holidays/__init__.py
@@ -0,0 +1,7 @@
+# https://stackoverflow.com/questions/1739924/python-reload-component-y-imported-with-from-x-import-y
+# Não importar classes diretamente!
+
+from brazilian_holidays.datas import Calendario, Holidays
+
+#__version__ = '0.1.1'
+#__author__ = 'Michel Metran'
