ab99ce88507ac0c37f70f1b182eeca41b3e9264c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 17:07:13 +0000

    Add package torappu-excel/2.6.65/torappu_excel-2.6.65.tar.gz

---
+++ packages/torappu-excel/torappu_excel-2.6.65.tar.gz/torappu_excel-2.6.65/src/torappu_excel/torappu/act_multi_v3_map_mode_type.py
@@ -0,0 +1,8 @@
+from ..common import CustomIntEnum
+
+
+class ActMultiV3MapModeType(CustomIntEnum):
+    NONE = "NONE", 0
+    NORMAL = "NORMAL", 1
+    FOOTBALL = "FOOTBALL", 2
+    DEFENCE = "DEFENCE", 3
---
+++ packages/torappu-excel/torappu_excel-2.6.65.tar.gz/torappu_excel-2.6.65/src/torappu_excel/torappu/act_multi_v3_match_pos_type.py
@@ -0,0 +1,7 @@
+from ..common import CustomIntEnum
+
+
+class ActMultiV3MatchPosType(CustomIntEnum):
+    NORMAL = "NORMAL", 0
+    COACH = "COACH", 1
+    STUDENT = "STUDENT", 2
