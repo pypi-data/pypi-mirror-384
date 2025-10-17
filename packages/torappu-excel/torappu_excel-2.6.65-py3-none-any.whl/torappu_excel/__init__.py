3a2944457a3c079355ef35b00cc254e10390b7c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 17:07:13 +0000

    Add package torappu-excel/2.6.65/torappu_excel-2.6.65-py3-none-any.whl

---
+++ packages/torappu-excel/torappu_excel-2.6.65-py3-none-any.whl/torappu_excel/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "2.6.65"
+
+client_version = "2.6.61"
+data_version = "25-10-13-16-21-56_229ee9"
+major_version = 396
+
+from .constants import ExcelTableManager as ExcelTableManager  # noqa: E402
+from .torappu.player_data_model import PlayerDataModel as PlayerDataModel  # noqa: E402
