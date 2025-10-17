7ee5babd034440cf0a07aab8527c005bd3aca83e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 08:07:52 +0000

    Add package plato-learn/1.31/plato_learn-1.31-py3-none-any.whl

---
+++ packages/plato-learn/plato_learn-1.31-py3-none-any.whl/plato/models/huggingface.py
@@ -0,0 +1,28 @@
+"""
+Obtaining a model from HuggingFace.
+"""
+
+from transformers import AutoConfig, AutoModelForCausalLM
+
+from plato.config import Config
+
+
+class Model:
+    """The CausalLM model loaded from HuggingFace."""
+
+    @staticmethod
+    def get(model_name=None, **kwargs):  # pylint: disable=unused-argument
+        """Returns a named model from HuggingFace."""
+        config_kwargs = {
+            "cache_dir": None,
+            "revision": "main",
+            "use_auth_token": None,
+        }
+
+        config = AutoConfig.from_pretrained(model_name, **config_kwargs)
+
+        return AutoModelForCausalLM.from_pretrained(
+            model_name,
+            config=config,
+            cache_dir=Config().params["model_path"] + "/huggingface",
+        )
