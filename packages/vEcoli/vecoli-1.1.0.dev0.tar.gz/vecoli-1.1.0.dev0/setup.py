6fa49aa3d8fb9cf9940b21b26d02240c6e11a603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 07:07:20 +0000

    Add package vEcoli/1.1.0.dev0/vecoli-1.1.0.dev0.tar.gz

---
+++ packages/vEcoli/vecoli-1.1.0.dev0.tar.gz/vecoli-1.1.0.dev0/setup.py
@@ -0,0 +1,44 @@
+import os
+import numpy as np
+from Cython.Build import cythonize
+from setuptools import Extension, find_packages, setup
+
+# List of your Cython extension modules
+extensions = [
+    Extension(
+        "wholecell.utils._build_sequences",
+        [os.path.join("wholecell", "utils", "_build_sequences.pyx")],
+        include_dirs=[np.get_include()],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    ),
+    Extension(
+        "wholecell.utils.mc_complexation",
+        [os.path.join("wholecell", "utils", "mc_complexation.pyx")],
+        include_dirs=[np.get_include()],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    ),
+    Extension(
+        "wholecell.utils._fastsums",
+        [os.path.join("wholecell", "utils", "_fastsums.pyx")],
+        include_dirs=[np.get_include()],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    ),
+]
+
+setup(
+    name="vecoli",  # PyPI distribution name
+    version="0.1.0",
+    packages=find_packages(),          # Includes all Python packages in repo
+    ext_modules=cythonize(
+        extensions, 
+        compiler_directives={"language_level": "3"}
+    ),
+    include_dirs=[np.get_include()]
+)
+
+# Use cythonize on the extensions list
+# setup(
+#     name="Vivarium Ecoli Extensions",
+#     ext_modules=cythonize(extensions),
+#     include_dirs=[np.get_include()],
+# )
