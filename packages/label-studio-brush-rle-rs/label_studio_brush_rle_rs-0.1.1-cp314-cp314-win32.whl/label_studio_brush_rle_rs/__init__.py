2eaf5cee265d776678e815bada55dccbdaa8cc73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 06:07:06 +0000

    Add package label-studio-brush-rle-rs/0.1.1/label_studio_brush_rle_rs-0.1.1-cp314-cp314-win32.whl

---
+++ packages/label-studio-brush-rle-rs/label_studio_brush_rle_rs-0.1.1-cp314-cp314-win32.whl/label_studio_brush_rle_rs/__init__.py
@@ -0,0 +1,5 @@
+from .label_studio_brush_rle_rs import *
+
+__doc__ = label_studio_brush_rle_rs.__doc__
+if hasattr(label_studio_brush_rle_rs, "__all__"):
+    __all__ = label_studio_brush_rle_rs.__all__
