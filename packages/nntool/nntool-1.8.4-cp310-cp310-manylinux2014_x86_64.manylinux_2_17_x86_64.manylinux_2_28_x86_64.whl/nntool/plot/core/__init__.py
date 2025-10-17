a6d152bbd665a092a0686076b70e201f858102ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 15:18:52 +0000

    Add package nntool/1.8.4/nntool-1.8.4-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/nntool/nntool-1.8.4-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/nntool/plot/core/__init__.py
@@ -0,0 +1,9 @@
+﻿import cythonpackage
+cythonpackage.init(__name__)
+from .latexify import latexify, savefig, is_latexify_enabled
+
+__all__ = [
+    "latexify",
+    "savefig",
+    "is_latexify_enabled",
+]
