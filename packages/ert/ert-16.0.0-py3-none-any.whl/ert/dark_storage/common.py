a1972ee893408c07cced34eea23ba4c2e2323593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 17:07:27 +0000

    Add package ert/16.0.0/ert-16.0.0-py3-none-any.whl

---
+++ packages/ert/ert-16.0.0-py3-none-any.whl/ert/dark_storage/common.py
@@ -0,0 +1,21 @@
+import logging
+import os
+
+from ert.dark_storage.exceptions import InternalServerError
+from ert.storage import ErtStorageException, Storage, open_storage
+
+logger = logging.getLogger(__name__)
+
+
+_storage: Storage | None = None
+
+
+def get_storage() -> Storage:
+    global _storage
+    if _storage is None:
+        try:
+            return (_storage := open_storage(os.environ["ERT_STORAGE_ENS_PATH"]))
+        except ErtStorageException as err:
+            raise InternalServerError(f"{err!s}") from err
+    _storage.refresh()
+    return _storage
---
+++ packages/ert/ert-16.0.0-py3-none-any.whl/ert/gui/tools/plot/plot_widget.py
@@ -0,0 +1,167 @@
+import sys
+import traceback
+from typing import TYPE_CHECKING, Union
+
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+from matplotlib.backends.backend_qt5agg import (  # type: ignore
+    FigureCanvas,
+    NavigationToolbar2QT,
+)
+from matplotlib.figure import Figure
+from PyQt6.QtCore import QStringListModel, Qt
+from PyQt6.QtCore import pyqtSignal as Signal
+from PyQt6.QtCore import pyqtSlot as Slot
+from PyQt6.QtGui import QAction, QIcon
+from PyQt6.QtWidgets import QComboBox, QVBoxLayout, QWidget, QWidgetAction
+
+from .plot_api import EnsembleObject
+
+if TYPE_CHECKING:
+    from .plottery import PlotContext
+    from .plottery.plots.cesp import CrossEnsembleStatisticsPlot
+    from .plottery.plots.distribution import DistributionPlot
+    from .plottery.plots.ensemble import EnsemblePlot
+    from .plottery.plots.gaussian_kde import GaussianKDEPlot
+    from .plottery.plots.histogram import HistogramPlot
+    from .plottery.plots.statistics import StatisticsPlot
+    from .plottery.plots.std_dev import StdDevPlot
+
+
+class CustomNavigationToolbar(NavigationToolbar2QT):
+    customizationTriggered = Signal()
+    layerIndexChanged = Signal(int)
+
+    def __init__(
+        self,
+        canvas: FigureCanvas,
+        parent: QWidget | None,
+        coordinates: bool = True,
+    ) -> None:
+        super().__init__(canvas, parent, coordinates)  # type: ignore
+
+        gear = QIcon("img:edit.svg")
+        customize_action = QAction(gear, "Customize", self)
+        customize_action.setToolTip("Customize plot settings")
+        customize_action.triggered.connect(self.customizationTriggered)
+
+        layer_combobox = QComboBox()
+        self._model = QStringListModel()
+        layer_combobox.setModel(self._model)
+        layer_combobox.currentIndexChanged.connect(self.layerIndexChanged)
+
+        for action in self.actions():
+            if str(action.text()).lower() == "subplots":
+                self.removeAction(action)
+
+            if str(action.text()).lower() == "customize":
+                self.insertAction(action, customize_action)
+                self.removeAction(action)
+
+            # insert the layer widget before the coordinates widget
+            if isinstance(action, QWidgetAction):
+                self._layer_action = self.insertWidget(action, layer_combobox)
+                self._layer_action.setVisible(False)
+
+    @Slot(bool)
+    def showLayerWidget(self, show: bool) -> None:
+        self._layer_action.setVisible(show)
+
+    @Slot()
+    def resetLayerWidget(
+        self,
+    ) -> None:
+        self._layer_action.defaultWidget().setCurrentIndex(0)
+
+    @Slot(int)
+    def updateLayerWidget(self, layers: int) -> None:
+        if layers != len(self._model.stringList()):
+            self._model.setStringList([f"Layer {i}" for i in range(layers)])
+            self.resetLayerWidget()
+
+
+class PlotWidget(QWidget):
+    customizationTriggered = Signal()
+    layerIndexChanged = Signal(int)
+    updateLayerWidget = Signal(int)
+    resetLayerWidget = Signal()
+    showLayerWidget = Signal(bool)
+
+    def __init__(
+        self,
+        name: str,
+        plotter: Union[
+            "EnsemblePlot",
+            "StatisticsPlot",
+            "HistogramPlot",
+            "GaussianKDEPlot",
+            "DistributionPlot",
+            "CrossEnsembleStatisticsPlot",
+            "StdDevPlot",
+        ],
+        parent: QWidget | None = None,
+    ) -> None:
+        QWidget.__init__(self, parent)
+
+        self._name = name
+        self._plotter = plotter
+        self._figure = Figure()
+        self._figure.set_layout_engine("tight")
+        self._canvas = FigureCanvas(self._figure)
+        self._canvas.setParent(self)
+        self._canvas.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
+        self._canvas.setFocus()
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self._canvas)
+        self._toolbar = CustomNavigationToolbar(self._canvas, self)
+        self._toolbar.customizationTriggered.connect(self.customizationTriggered)
+        self._toolbar.layerIndexChanged.connect(self.layerIndexChanged)
+        self.updateLayerWidget.connect(self._toolbar.updateLayerWidget)
+        self.resetLayerWidget.connect(self._toolbar.resetLayerWidget)
+        self.showLayerWidget.connect(self._toolbar.showLayerWidget)
+
+        vbox.addWidget(self._toolbar)
+        self.setLayout(vbox)
+
+        self._dirty = True
+        self._active = False
+        self.resetPlot()
+
+    def resetPlot(self) -> None:
+        self._figure.clear()
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    def updatePlot(
+        self,
+        plot_context: "PlotContext",
+        ensemble_to_data_map: dict[EnsembleObject, pd.DataFrame],
+        observations: pd.DataFrame,
+        std_dev_images: dict[str, npt.NDArray[np.float32]],
+    ) -> None:
+        self.resetPlot()
+        try:
+            self._plotter.plot(
+                self._figure,
+                plot_context,
+                ensemble_to_data_map,
+                observations,
+                std_dev_images,
+            )
+            self._canvas.draw()
+        except Exception as e:
+            exc_type, _, exc_tb = sys.exc_info()
+            sys.stderr.write("-" * 80 + "\n")
+            traceback.print_tb(exc_tb)
+            if exc_type is not None:
+                sys.stderr.write(f"Exception type: {exc_type.__name__}\n")
+            sys.stderr.write(f"{e}\n")
+            sys.stderr.write("-" * 80 + "\n")
+            sys.stderr.write(
+                "An error occurred during plotting. "
+                "This stack trace is helpful for diagnosing the problem."
+            )
