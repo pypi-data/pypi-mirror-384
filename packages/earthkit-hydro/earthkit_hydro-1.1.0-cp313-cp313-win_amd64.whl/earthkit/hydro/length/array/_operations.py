91d2107c9da476f3929debca84cba6d4cf448917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 17:07:04 +0000

    Add package earthkit-hydro/1.1.0/earthkit_hydro-1.1.0-cp313-cp313-win_amd64.whl

---
+++ packages/earthkit-hydro/earthkit_hydro-1.1.0-cp313-cp313-win_amd64.whl/earthkit/hydro/length/array/_operations.py
@@ -0,0 +1,35 @@
+from earthkit.hydro._utils.decorators import mask, multi_backend
+from earthkit.hydro._utils.locations import locations_to_1d
+from earthkit.hydro.length.array import __operations as _operations
+
+
+@multi_backend(allow_jax_jit=False)
+def min(xp, river_network, field, locations, upstream, downstream, return_type):
+    if field is None:
+        field = xp.ones(river_network.n_nodes)
+    locations, _, _ = locations_to_1d(xp, river_network, locations)
+    return_type = river_network.return_type if return_type is None else return_type
+    if return_type not in ["gridded", "masked"]:
+        raise ValueError("return_type must be either 'gridded' or 'masked'.")
+    decorated_func = mask(return_type == "gridded")(_operations.min)
+    return decorated_func(xp, river_network, field, locations, upstream, downstream)
+
+
+@multi_backend(allow_jax_jit=False)
+def max(xp, river_network, field, locations, upstream, downstream, return_type):
+    if field is None:
+        field = xp.ones(river_network.n_nodes)
+    locations, _, _ = locations_to_1d(xp, river_network, locations)
+    return_type = river_network.return_type if return_type is None else return_type
+    if return_type not in ["gridded", "masked"]:
+        raise ValueError("return_type must be either 'gridded' or 'masked'.")
+    decorated_func = mask(return_type == "gridded")(_operations.max)
+    return decorated_func(xp, river_network, field, locations, upstream, downstream)
+
+
+def to_source(*args, **kwargs):
+    raise NotImplementedError
+
+
+def to_sink(*args, **kwargs):
+    raise NotImplementedError
