2a944f58d6901a978c082df1269acead90cc8e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 04:07:00 +0000

    Add package pydnevnikruapi/0.2.0/pydnevnikruapi-0.2.0-py3-none-any.whl

---
+++ packages/pydnevnikruapi/pydnevnikruapi-0.2.0-py3-none-any.whl/pydnevnikruapi/constants.py
@@ -0,0 +1,6 @@
+BASE_URL = "https://api.dnevnik.ru/v2/"
+LOGIN_URL = "https://login.dnevnik.ru/login/"
+RETURN_URL = (
+    "https://login.dnevnik.ru/oauth2?response_type="
+    "token&client_id=b8006d75-70a9-4291-885c-13d8511bb2ae&scope=CommonInfo,EducationalInfo"
+)
