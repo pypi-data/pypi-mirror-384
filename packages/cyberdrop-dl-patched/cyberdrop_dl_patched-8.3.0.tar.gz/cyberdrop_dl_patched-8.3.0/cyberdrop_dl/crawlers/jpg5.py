530db4718fa0ee0653b18148b8a3e43294dc7c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 15:17:45 +0000

    Add package cyberdrop-dl-patched/8.3.0/cyberdrop_dl_patched-8.3.0.tar.gz

---
+++ packages/cyberdrop-dl-patched/cyberdrop_dl_patched-8.3.0.tar.gz/cyberdrop_dl_patched-8.3.0/cyberdrop_dl/crawlers/jpg5.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL
+
+from ._chevereto import CheveretoCrawler
+
+
+class JPG5Crawler(CheveretoCrawler):
+    SUPPORTED_DOMAINS = "selti-delivery.ru", "jpg6.su"
+    DOMAIN = "jpg5.su"
+    FOLDER_DOMAIN = "JPG5"
+    PRIMARY_URL = AbsoluteHttpURL("https://jpg6.su")
+    CHEVERETO_SUPPORTS_VIDEO = False
+    OLD_DOMAINS = (
+        "host.church",
+        "jpg.homes",
+        "jpg.church",
+        "jpg.fish",
+        "jpg.fishing",
+        "jpg.pet",
+        "jpeg.pet",
+        "jpg1.su",
+        "jpg2.su",
+        "jpg3.su",
+        "jpg4.su",
+        "jpg5.su",
+    )
+
+    _RATE_LIMIT = 1, 1
+
+    @classmethod
+    def transform_url(cls, url: AbsoluteHttpURL) -> AbsoluteHttpURL:
+        url = super().transform_url(url)
+        if cls.is_subdomain(url):
+            # old jpg5 subdomains are still valid. ex: simp4.jpg5.su
+            return url.with_host(url.host.replace("jpg6.su", "jpg5.su"))
+        return url
+
+
+def fix_db_referer(referer: str) -> str:
+    url = AbsoluteHttpURL(referer)
+    return str(JPG5Crawler.transform_url(url))
