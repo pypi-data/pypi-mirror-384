b87e1add5a3d002ae25adfd74c76037fd61bd1de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 05:08:05 +0000

    Add package arcade-salesforce/2.0.1/arcade_salesforce-2.0.1-py3-none-any.whl

---
+++ packages/arcade-salesforce/arcade_salesforce-2.0.1-py3-none-any.whl/arcade_salesforce/constants.py
@@ -0,0 +1,23 @@
+SALESFORCE_API_VERSION = "v63.0"
+
+ASSOCIATION_REFERENCE_FIELDS = [
+    "AccountId",
+    "OwnerId",
+    "AssociatedToWhom",
+    "ContactId",
+]
+
+GLOBALLY_IGNORED_FIELDS = [
+    "attributes",
+    "CleanStatus",
+    "CreatedById",
+    "CreatedDate",
+    "IsDeleted",
+    "LastModifiedById",
+    "LastModifiedDate",
+    "LastReferencedDate",
+    "LastViewedDate",
+    "PhotoUrl",
+    "SystemModstamp",
+    "WhatId",
+]
