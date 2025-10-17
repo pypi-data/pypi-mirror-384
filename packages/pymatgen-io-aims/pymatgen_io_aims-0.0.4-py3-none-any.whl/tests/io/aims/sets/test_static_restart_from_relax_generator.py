6187350694fbffe1607858ada4a988d33c253b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 14:06:51 +0000

    Add package pymatgen-io-aims/0.0.4/pymatgen_io_aims-0.0.4-py3-none-any.whl

---
+++ packages/pymatgen-io-aims/pymatgen_io_aims-0.0.4-py3-none-any.whl/tests/io/aims/sets/test_static_restart_from_relax_generator.py
@@ -0,0 +1,84 @@
+"""The test of input sets generating from restart information"""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+from pymatgen.io.aims.sets.core import StaticSetGenerator
+
+from ..conftest import O2, Si, comp_system  # noqa: TID252
+
+TEST_FILES_DIR = Path(__file__).parents[3] / "files/"
+
+SPECIES_DIR = TEST_FILES_DIR / "io/aims/species_directory"
+REF_PATH = (TEST_FILES_DIR / "io/aims/aims_input_generator_ref").resolve()
+
+
+def test_static_from_relax_si(tmp_path):
+    user_params = {"species_dir": str(SPECIES_DIR / "light")}
+    comp_system(
+        Si,
+        user_params,
+        "static-from-prev-si",
+        tmp_path,
+        REF_PATH,
+        StaticSetGenerator,
+        properties=["energy", "forces", "stress"],
+        prev_dir=f"{REF_PATH}/relax-si/",
+    )
+
+
+def test_static_from_relax_si_no_kgrid(tmp_path):
+    user_params = {"species_dir": str(SPECIES_DIR / "light")}
+    comp_system(
+        Si,
+        user_params,
+        "static-from-prev-no-kgrid-si",
+        tmp_path,
+        REF_PATH,
+        StaticSetGenerator,
+        properties=["energy", "forces", "stress"],
+        prev_dir=f"{REF_PATH}/relax-no-kgrid-si/",
+    )
+
+
+def test_static_from_relax_default_species_dir(tmp_path):
+    user_params = {"species_dir": str(SPECIES_DIR / "light")}
+    comp_system(
+        Si,
+        user_params,
+        "static-from-prev-si",
+        tmp_path,
+        REF_PATH,
+        StaticSetGenerator,
+        properties=["energy", "forces", "stress"],
+        prev_dir=f"{REF_PATH}/relax-si/",
+    )
+
+
+def test_static_from_relax_o2(tmp_path):
+    user_params = {"species_dir": str(SPECIES_DIR / "light")}
+    comp_system(
+        O2,
+        user_params,
+        "static-from-prev-o2",
+        tmp_path,
+        REF_PATH,
+        StaticSetGenerator,
+        properties=["energy", "forces", "stress"],
+        prev_dir=f"{REF_PATH}/relax-o2/",
+    )
+
+
+def test_static_from_relax_default_species_dir_o2(tmp_path):
+    user_params = {"species_dir": str(SPECIES_DIR / "light")}
+    comp_system(
+        O2,
+        user_params,
+        "static-from-prev-o2",
+        tmp_path,
+        REF_PATH,
+        StaticSetGenerator,
+        properties=["energy", "forces", "stress"],
+        prev_dir=f"{REF_PATH}/relax-o2/",
+    )
