05d079e01384c3c1500a1938171a2dac469987e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 05:07:13 +0000

    Add package vector-vis-graph/0.8.1/vector_vis_graph-0.8.1-py3-none-any.whl

---
+++ packages/vector-vis-graph/vector_vis_graph-0.8.1-py3-none-any.whl/vector_vis_graph/__init__.py
@@ -0,0 +1,11 @@
+from vector_vis_graph.vvg import horizontal_vvg, natural_vvg
+from vector_vis_graph.weight_calculation import WeightMethod
+
+__version__ = "0.8.1"
+
+
+__all__ = [
+    "natural_vvg",
+    "horizontal_vvg",
+    "WeightMethod",
+]
