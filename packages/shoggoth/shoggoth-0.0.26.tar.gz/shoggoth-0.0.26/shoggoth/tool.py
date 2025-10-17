e7050da03398eaf1a6e1f7e8358ac4da5ebbe01a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 17:06:29 +0000

    Add package shoggoth/0.0.26/shoggoth-0.0.26.tar.gz

---
+++ packages/shoggoth/shoggoth-0.0.26.tar.gz/shoggoth-0.0.26/shoggoth/tool.py
@@ -0,0 +1,54 @@
+import argparse
+import os
+from shoggoth.files import asset_dir, root_dir
+import urllib.request
+import zipfile
+
+
+def version_is_up_to_date() -> bool:
+    """ Parses the asset pack version """
+    if not (asset_dir/'version.txt').exists():
+        return False
+    with (asset_dir/'version.txt').open('r') as f:
+        if not f.read().startswith('0.3.1'):
+            return False
+    return True
+
+def run():
+    os.environ["KIVY_NO_ARGS"] = "1"
+    os.environ["KIVY_IMAGE"] = "sdl2, pil"
+
+    parser = argparse.ArgumentParser(description='Shoggoth Card Creator')
+    parser.add_argument('-v', '--view', metavar='FILE', help='Open in viewer mode with specified file')
+    args = parser.parse_args()
+
+    # ensure directories exist
+    root_dir.mkdir(parents=True, exist_ok=True)
+
+    # ensure assets directory exists
+    if not asset_dir.is_dir() or not version_is_up_to_date():
+        print("Asset pack not found. Downloading assets...")
+        # download assets
+        url = 'https://www.dropbox.com/scl/fi/o15w058davy5b290iik55/assets-0-3-2.zip?rlkey=upil4ea1fn2igiumar2bldqhw&st=1zudjql2&dl=1'
+        filehandle, _ = urllib.request.urlretrieve(url)
+        with zipfile.ZipFile(filehandle, 'r') as file:
+            file.extractall(root_dir)
+        print("Assets downloaded successfully.")
+    else:
+        print("Asset pack up to date.")
+
+    if args.view:
+        # Start in viewer mode
+        from shoggoth.viewer import ViewerApp
+        app = ViewerApp(args.view)
+    else:
+        # Start in normal mode
+        from shoggoth.main import ShoggothApp
+        app = ShoggothApp()
+    app.run()
+
+
+if __name__ == '__main__':
+    run()
+
+print('tool done running')
