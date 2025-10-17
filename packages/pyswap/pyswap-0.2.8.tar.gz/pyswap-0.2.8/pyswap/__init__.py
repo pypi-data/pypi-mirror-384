30ff95992fe62e8cc1df230c1cde9873662ccc83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 04:07:09 +0000

    Add package pyswap/0.2.8/pyswap-0.2.8.tar.gz

---
+++ packages/pyswap/pyswap-0.2.8.tar.gz/pyswap-0.2.8/pyswap/__init__.py
@@ -0,0 +1,32 @@
+# ruff: noqa: F401
+from pyswap import components, db, gis, testcase
+
+# # This will be better if called as `from pyswap.db import HDF5`, makes it clear
+# # where it is coming from
+from pyswap.core import io, plot
+from pyswap.log import _setup_logger, set_log_level
+from pyswap.model.model import Model, run_parallel
+from pyswap.utils.loaders import load_bbc, load_crp, load_dra, load_swp
+
+# Version information
+import importlib.metadata
+__version__ = importlib.metadata.version("pyswap")
+
+__all__ = [
+    "components",
+    "gis",
+    "db",
+    "plot",
+    "testcase",
+    "io",
+    "load_swp",
+    "load_dra",
+    "load_crp",
+    "load_bbc",
+    "set_log_level",
+    "Model",
+    "run_parallel",
+]
+
+
+_setup_logger()
