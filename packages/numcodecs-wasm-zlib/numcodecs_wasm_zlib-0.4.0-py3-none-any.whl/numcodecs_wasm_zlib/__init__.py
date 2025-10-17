4efffeb0c6cf0ea5492be64d68a606e4f50f3892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 16:09:01 +0000

    Add package numcodecs-wasm-zlib/0.4.0/numcodecs_wasm_zlib-0.4.0-py3-none-any.whl

---
+++ packages/numcodecs-wasm-zlib/numcodecs_wasm_zlib-0.4.0-py3-none-any.whl/numcodecs_wasm_zlib/__init__.py
@@ -0,0 +1,15 @@
+__all__ = ["Zlib"]
+
+import importlib.resources
+import sys
+
+from numcodecs_wasm import WasmCodecMeta
+
+
+class Zlib(
+    metaclass=WasmCodecMeta,
+    wasm=importlib.resources.files(
+        sys.modules[__name__]
+    ).joinpath("codec.wasm").read_bytes(),
+):
+    pass
