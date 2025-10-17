b34b89c1ba6ac035180db492aca2a590ff7b12d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 18:19:11 +0000

    Add package Fount-Core/0.1.6.6/fount_core-0.1.6.6-py3-none-any.whl

---
+++ packages/Fount-Core/fount_core-0.1.6.6-py3-none-any.whl/fount/artifact.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Artifacts:
+    id: str
+
+    @classmethod
+    def get_datasets(cls, _transport) -> List[dict]:
+        return _transport.get_datasets()
+
+    @classmethod
+    def get_models(cls, _transport) -> List[dict]:
+        return _transport.get_model()
+
+    @classmethod
+    def get_jobs(cls, _transport, job_type) -> List[dict]:
+        return _transport.get_jobs(job_type)
+
+    @classmethod
+    def get_predictions(cls, _transport, job_id) -> List[dict]:
+        return _transport.get_predictions(job_id)
