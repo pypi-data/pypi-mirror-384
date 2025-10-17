4a259ea6bfc489bae5ce3f2644a386532bcddf22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 16:09:36 +0000

    Add package nd-prover/1.0.1/nd_prover-1.0.1.tar.gz

---
+++ packages/nd-prover/nd_prover-1.0.1.tar.gz/nd_prover-1.0.1/nd_prover/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '1.0.1'
+__author__ = 'Daniyal Akif'
+__email__ = 'daniyalakif@gmail.com'
+__license__ = 'MIT'
+__description__ = 'An interactive Fitch-style proof checker'
+__url__ = 'https://github.com/daniyal1249/nd-prover'
+
+
+from .cli import *
