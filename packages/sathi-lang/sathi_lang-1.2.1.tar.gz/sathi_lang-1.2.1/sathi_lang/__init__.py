5c0f4fc3a13b42bf098ef47cbf198377b7ccdc34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 17:06:36 +0000

    Add package sathi-lang/1.2.1/sathi_lang-1.2.1.tar.gz

---
+++ packages/sathi-lang/sathi_lang-1.2.1.tar.gz/sathi_lang-1.2.1/sathi_lang/__init__.py
@@ -0,0 +1,18 @@
+"""
+Sathi Language Package
+----------------------
+
+A Nepali-inspired, human-friendly programming language
+developed by Nirajan Ghimire (https://www.nirajang.com.np).
+
+Usage:
+    sathi <file.sathi>
+
+Example:
+    sathi examples/hello.sathi
+"""
+
+__version__ = "1.2.0"
+__author__ = "Nirajan Ghimire"
+__email__ = "nirajanghimire20@gmail.com"
+__license__ = "MIT"
