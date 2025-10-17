7a1144c197e647ae03aebcf3705b3f3fd1a49232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-17 15:17:50 +0000

    Add package flask-inputfilter/0.7.5/flask_inputfilter-0.7.5-cp310-cp310-musllinux_1_2_i686.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.7.5-cp310-cp310-musllinux_1_2_i686.whl/flask_inputfilter/enums/image_format_enum.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from enum import Enum
+
+
+class ImageFormatEnum(Enum):
+    JPEG = "JPEG"
+    PNG = "PNG"
+    GIF = "GIF"
+    BMP = "BMP"
+    TIFF = "TIFF"
+    WEBP = "WEBP"
+    ICO = "ICO"
+    PDF = "PDF"
+    EPS = "EPS"
+    SVG = "SVG"
+    PSD = "PSD"
+    XCF = "XCF"
+    HEIF = "HEIF"
+    AVIF = "AVIF"
