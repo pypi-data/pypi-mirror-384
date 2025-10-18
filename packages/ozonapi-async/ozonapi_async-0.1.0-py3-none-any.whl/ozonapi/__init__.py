158c356301a99ca9e72b20823816f228e55ffb28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 04:06:46 +0000

    Add package ozonapi-async/0.1.0/ozonapi_async-0.1.0-py3-none-any.whl

---
+++ packages/ozonapi-async/ozonapi_async-0.1.0-py3-none-any.whl/ozonapi/__init__.py
@@ -0,0 +1,9 @@
+"""
+Асинхронный интерфейс для взаимодействия с API маркетплейса Ozon.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Alexander Ulianov"
+__all__ = ["SellerAPI", ]
+
+from .seller import SellerAPI
