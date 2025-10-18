288a12226018c9228c73c972a3cfc65320eb553b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 06:06:19 +0000

    Add package promethium-py/1.0.0/promethium_py-1.0.0.tar.gz

---
+++ packages/promethium-py/promethium_py-1.0.0.tar.gz/promethium_py-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='promethium-py',
+    version='1.0.0',
+    author='Rohan Kishore',
+    author_email='rohankishore746@gmail.com',
+    description='A Pythonic interface to a rich, offline database of chemical information.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rohankishore/promethium',
+    packages=find_packages(),
+    package_data={
+        'promethium': ['data/*.csv'],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=[
+        'pandas',
+        'numpy',
+    ],
+)
