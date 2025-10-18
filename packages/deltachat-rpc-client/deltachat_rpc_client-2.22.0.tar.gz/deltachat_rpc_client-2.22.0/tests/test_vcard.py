687a0f4353725c4c2a31e5b524d796a751d3b6e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 03:15:39 +0000

    Add package deltachat-rpc-client/2.22.0/deltachat_rpc_client-2.22.0.tar.gz

---
+++ packages/deltachat-rpc-client/deltachat_rpc_client-2.22.0.tar.gz/deltachat_rpc_client-2.22.0/tests/test_vcard.py
@@ -0,0 +1,14 @@
+def test_vcard(acfactory) -> None:
+    alice, bob = acfactory.get_online_accounts(2)
+
+    alice_contact_bob = alice.create_contact(bob, "Bob")
+    alice_contact_charlie = alice.create_contact("charlie@example.org", "Charlie")
+
+    alice_chat_bob = alice_contact_bob.create_chat()
+    alice_chat_bob.send_contact(alice_contact_charlie)
+
+    event = bob.wait_for_incoming_msg_event()
+    message = bob.get_message_by_id(event.msg_id)
+    snapshot = message.get_snapshot()
+    assert snapshot.vcard_contact
+    assert snapshot.vcard_contact.addr == "charlie@example.org"
