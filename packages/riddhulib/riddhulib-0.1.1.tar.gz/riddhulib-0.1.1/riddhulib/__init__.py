416e84c6bf50db66d5cc443b06060701121095e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 06:06:21 +0000

    Add package riddhulib/0.1.1/riddhulib-0.1.1.tar.gz

---
+++ packages/riddhulib/riddhulib-0.1.1.tar.gz/riddhulib-0.1.1/riddhulib/__init__.py
@@ -0,0 +1,4 @@
+# riddhulib/__init__.py
+# This file makes riddhulib a Python package.
+
+from .addcode import add_numbers
---
+++ packages/riddhulib/riddhulib-0.1.1.tar.gz/riddhulib-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="riddhulib",
+    version="0.1.1",
+    author="Riddhesh Wagvekar",
+    description="A simple addition library by Riddhesh with comments and docstrings.",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
