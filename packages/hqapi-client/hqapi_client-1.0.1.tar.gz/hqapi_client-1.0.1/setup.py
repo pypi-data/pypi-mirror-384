9100b7f0b0bff1388699631c51412ad696437787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 04:06:42 +0000

    Add package hqapi-client/1.0.1/hqapi_client-1.0.1.tar.gz

---
+++ packages/hqapi-client/hqapi_client-1.0.1.tar.gz/hqapi_client-1.0.1/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="hqapi_client",  # must match your Python package folder
+    version="1.0.1",
+    description="Python client for the HQAPI.com",
+    author="HQAPI",
+    packages=find_packages(),  # automatically find packages
+    install_requires=[
+        "requests>=2.30.0",  # your dependencies
+    ],
+    python_requires=">=3.8",
+)
