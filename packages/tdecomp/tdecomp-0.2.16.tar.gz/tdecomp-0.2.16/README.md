962d6c63275742f0465591aec01bab58567b3ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 03:15:01 +0000

    Add package tdecomp/0.2.16/tdecomp-0.2.16.tar.gz

---
+++ packages/tdecomp/tdecomp-0.2.16.tar.gz/tdecomp-0.2.16/README.md
@@ -0,0 +1,4 @@
+# tensor-decompositions
+
+Here some resume for sketches, projections and randomized algorithms:
+https://docs.google.com/presentation/d/16Q8ZOH-01Z03sX1zv08AlPEzTnZ574DcktqmSQ4VfWA/edit?slide=id.g371a24abfab_0_11#slide=id.g371a24abfab_0_11
---
+++ packages/tdecomp/tdecomp-0.2.16.tar.gz/tdecomp-0.2.16/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# print(find_packages(exclude=["tests*", "experiments*", "docs*", "build*", "dist*"]))
+
+setup(
+    name="tdecomp",
+    version="0.2.16",
+    description="Library for tensor and matrix decompositions applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Leon Strelkov",
+    author_email="strelllleo@mail.ru",
+    url="https://github.com/leostre/tensor-decompositions.git",
+    packages=find_packages(exclude=["tests*", "experiments*", "docs*", "build*", "dist*"]),
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires=[
+        "tensorly",
+        "torch",
+    ],
+    license=' BSD License',
+    keywords=[
+        "tensor decompositions",
+        "matrix decompositions", 
+        "randomized algorithms"
+    ],
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
