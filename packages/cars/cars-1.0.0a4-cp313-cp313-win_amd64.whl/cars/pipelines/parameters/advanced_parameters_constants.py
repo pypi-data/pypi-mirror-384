0bedccb858a1489b5f3bd95ef5efdd945ab3ee14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 05:06:12 +0000

    Add package cars/1.0.0a4/cars-1.0.0a4-cp313-cp313-win_amd64.whl

---
+++ packages/cars/cars-1.0.0a4-cp313-cp313-win_amd64.whl/cars/pipelines/parameters/advanced_parameters_constants.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf8
+#
+# Copyright (c) 2020 Centre National d'Etudes Spatiales (CNES).
+#
+# This file is part of CARS
+# (see https://github.com/CNES/cars).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+This module contains the advanced parameter definitions
+"""
+
+SAVE_INTERMEDIATE_DATA = "save_intermediate_data"
+KEEP_LOW_RES_DIR = "keep_low_res_dir"
+PHASING = "phasing"
+DEBUG_WITH_ROI = "debug_with_roi"
+LAND_COVER_MAP = "land_cover_map"
+CLASSIFICATION_TO_CONFIGURATION_MAPPING = (
+    "classification_to_configuration_mapping"
+)
+
+EPIPOLAR_A_PRIORI = "epipolar_a_priori"
+RESOLUTION_A_PRIORI = "resolution_a_priori"
+GROUND_TRUTH_DSM = "ground_truth_dsm"
+
+EPIPOLAR_RESOLUTIONS = "epipolar_resolutions"
+
+DSM_MERGING_TILE_SIZE = "dsm_merging_tile_size"
+
+
+# inner epipolar a priori constants
+GRID_CORRECTION = "grid_correction"
+DISPARITY_RANGE = "disparity_range"
+REFERENCE_DEM = "reference_dem"
+
+TERRAIN_A_PRIORI = "terrain_a_priori"
+USE_ENDOGENOUS_DEM = "use_endogenous_dem"
+DEM_MEDIAN = "dem_median"
+DEM_MIN = "dem_min"
+DEM_MAX = "dem_max"
+
+# ground truth dsm
+INPUT_GROUND_TRUTH_DSM = "dsm"
+INPUT_CLASSIFICATION = "classification"
+INPUT_AUX_PATH = "auxiliary_data"
+INPUT_AUX_INTERP = "auxiliary_data_interpolation"
+INPUT_GEOID = "geoid"
+INPUT_EPSG = "epsg"
+
+PERFORMANCE_MAP_CLASSES = "performance_map_classes"
+
+TEXTURE_BANDS = "texture_bands"
+
+GEOMETRY_PLUGIN = "geometry_plugin"
+
+PIPELINE = "pipeline"
---
+++ packages/cars/cars-1.0.0a4-cp313-cp313-win_amd64.whl/cars/pipelines/parameters/sensor_loaders/basic_classif_loader.py
@@ -0,0 +1,106 @@
+# !/usr/bin/env python
+# coding: utf8
+#
+# Copyright (c) 2024 Centre National d'Etudes Spatiales (CNES).
+#
+# This file is part of CARS
+# (see https://github.com/CNES/cars).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""
+This module contains the ClassifSensorLoader class.
+"""
+
+from json_checker import Checker
+
+from cars.core import inputs
+from cars.core.utils import make_relative_path_absolute
+from cars.pipelines.parameters.sensor_loaders.pivot_classif_loader import (
+    PivotClassifSensorLoader,
+)
+from cars.pipelines.parameters.sensor_loaders.sensor_loader import SensorLoader
+from cars.pipelines.parameters.sensor_loaders.sensor_loader_template import (
+    SensorLoaderTemplate,
+)
+
+
+@SensorLoader.register("basic_classif")
+class BasicClassifSensorLoader(SensorLoaderTemplate):
+    """
+    Default sensor loader (used when no sensor loader is specified)
+    """
+
+    def check_conf(self, conf):
+        """
+        Check configuration
+
+        :param conf: configuration to check
+
+        :return: overloaded configuration
+        :rtype: dict
+        """
+        default_filling = {
+            "fill_with_geoid": "b0",
+            "interpolate_from_borders": "b1",
+            "fill_with_endogenous_dem": "b2",
+            "fill_with_exogenous_dem": "b3",
+        }
+        if isinstance(conf, str):
+            overloaded_conf = {}
+            image_path = make_relative_path_absolute(conf, self.config_dir)
+            overloaded_conf["path"] = image_path
+            overloaded_conf["loader"] = "basic_classif"
+            overloaded_conf["filling"] = default_filling
+        elif isinstance(conf, dict):
+            overloaded_conf = conf.copy()
+            image_path = make_relative_path_absolute(
+                conf["path"], self.config_dir
+            )
+            overloaded_conf["path"] = image_path
+            overloaded_conf["loader"] = conf.get("loader", "basic")
+            overloaded_conf["filling"] = conf.get("filling", default_filling)
+        else:
+            raise TypeError(f"Input {conf} is not a string ot dict")
+
+        sensor_schema = {"loader": str, "path": str, "filling": dict}
+
+        # Check conf
+        checker = Checker(sensor_schema)
+        checker.validate(overloaded_conf)
+
+        return overloaded_conf
+
+    def set_pivot_format(self):
+        """
+        Transform input configuration to pivot format and store it
+        """
+        pivot_config = {
+            "loader": "pivot_classif",
+            "main_file": self.used_config["path"],
+            "filling": self.used_config["filling"],
+        }
+        pivot_config["bands"] = {}
+        for band_id in range(
+            inputs.rasterio_get_nb_bands(self.used_config["path"])
+        ):
+            band_name = "b" + str(band_id)
+            pivot_config["bands"][band_name] = {
+                "path": self.used_config["path"],
+                "band": band_id,
+            }
+        pivot_config["texture_bands"] = None
+        pivot_sensor_loader = PivotClassifSensorLoader(
+            pivot_config, self.config_dir
+        )
+        self.pivot_format = pivot_sensor_loader.get_pivot_format()
