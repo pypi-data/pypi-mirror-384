9d553d77a4bd32a4e4ea826c4e1886b04ac7ed63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 06:06:24 +0000

    Add package pyfreeproxy/0.1.5/pyfreeproxy-0.1.5.tar.gz

---
+++ packages/pyfreeproxy/pyfreeproxy-0.1.5.tar.gz/pyfreeproxy-0.1.5/freeproxy/modules/proxies/__init__.py
@@ -0,0 +1,20 @@
+'''initialize'''
+from .base import BaseProxiedSession
+from .ip89 import IP89ProxiedSession
+from ..utils import BaseModuleBuilder
+from .zdaye import ZdayeProxiedSession
+from .ip3366 import IP3366ProxiedSession
+from .kuaidaili import KuaidailiProxiedSession
+from .proxylistplus import ProxylistplusProxiedSession
+
+
+'''ProxiedSessionBuilder'''
+class ProxiedSessionBuilder(BaseModuleBuilder):
+    REGISTERED_MODULES = {
+        'IP89ProxiedSession': IP89ProxiedSession, 'ZdayeProxiedSession': ZdayeProxiedSession, 'IP3366ProxiedSession': IP3366ProxiedSession,
+        'KuaidailiProxiedSession': KuaidailiProxiedSession, 'ProxylistplusProxiedSession': ProxylistplusProxiedSession,
+    }
+
+
+'''BuildProxiedSession'''
+BuildProxiedSession = ProxiedSessionBuilder().build
