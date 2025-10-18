2352cdece617d4837978e315e86b404436cf8b30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 03:15:42 +0000

    Add package mugeshcalcu/1.0.0/mugeshcalcu-1.0.0.tar.gz

---
+++ packages/mugeshcalcu/mugeshcalcu-1.0.0.tar.gz/mugeshcalcu-1.0.0/README.md
@@ -0,0 +1,10 @@
+# mugeshcalcu
+
+Simple Python library created by Mugesh for performing **addition** and **multiplication**.
+
+## Installation
+
+From your project folder:
+
+```bash
+pip install -e .
---
+++ packages/mugeshcalcu/mugeshcalcu-1.0.0.tar.gz/mugeshcalcu-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mugeshcalcu",
+    version="1.0.0",
+    description="Simple calculator library for addition and multiplication by Mugesh",
+    author="Mugesh",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    license="MIT",
+)
