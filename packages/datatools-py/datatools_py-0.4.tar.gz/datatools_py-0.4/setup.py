760ca6a43d5718e290f6b71b276c03420a46747b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 04:06:56 +0000

    Add package datatools-py/0.4/datatools_py-0.4.tar.gz

---
+++ packages/datatools-py/datatools_py-0.4.tar.gz/datatools_py-0.4/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+
+# Core dependencies (always required)
+CORE_DEPS = [
+    'tqdm>=4.66.1',
+    'numpy>=1.26.4',
+    'simple_parsing>=0.1.5',
+    'mosaicml-streaming>=0.7.5',
+    'sentencepiece>=0.1.99',
+    'zstandard>=0.23.0',
+    'universal-pathlib>=0.2.2',
+    'fsspec[s3]>=2023.1.0',
+    'pyarrow>=10.0.0',
+    # 'transformers==4.39.3',
+]
+
+# Optional dependencies for dataset support
+DATASET_DEPS = [
+    'datasets>=2.18.0',
+]
+
+setup(
+    name='datatools-py',
+    version='0.4',
+    packages=find_packages(),
+    install_requires=CORE_DEPS,
+    extras_require={
+        'datasets': DATASET_DEPS,
+        'full': DATASET_DEPS,  # Alias for datasets
+    },
+    author='Alexander Wettig',
+    description='Library and scripts for common LM data utilities (tokenizing, splitting, packing, ...)',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/CodeCreator/datatools',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'console_scripts': [
+            'peek=datatools.scripts.peek:main',
+            'merge_index=datatools.scripts.merge_index:main',
+            'pack=datatools.scripts.pack:main',
+            'wrangle=datatools.scripts.wrangle:main',
+            'tokenize=datatools.scripts.tokenize:main',
+        ]
+    },
+    python_requires='>=3.6',
+)
