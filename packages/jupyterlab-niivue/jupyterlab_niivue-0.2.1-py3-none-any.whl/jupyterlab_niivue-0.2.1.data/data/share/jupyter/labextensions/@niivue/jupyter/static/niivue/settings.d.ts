c5cff8d817a27ffb00a574beb53eba34e5cd81f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 05:06:47 +0000

    Add package jupyterlab-niivue/0.2.1/jupyterlab_niivue-0.2.1-py3-none-any.whl

---
+++ packages/jupyterlab-niivue/jupyterlab_niivue-0.2.1-py3-none-any.whl/jupyterlab_niivue-0.2.1.data/data/share/jupyter/labextensions/@niivue/jupyter/static/niivue/settings.d.ts
@@ -0,0 +1,10 @@
+export interface NiiVueSettings {
+    showCrosshairs: boolean;
+    interpolation: boolean;
+    colorbar: boolean;
+    radiologicalConvention: boolean;
+    zoomDragMode: boolean;
+    defaultVolumeColormap: string;
+    defaultOverlayColormap: string;
+}
+export declare const defaultSettings: NiiVueSettings;
