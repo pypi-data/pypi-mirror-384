ab73b231b9e3884b5d172d4dd64ad3429183a5d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 03:15:51 +0000

    Add package any-document-extractor/0.1.0/any_document_extractor-0.1.0.tar.gz

---
+++ packages/any-document-extractor/any_document_extractor-0.1.0.tar.gz/any_document_extractor-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='any-document-extractor',
+    version='0.1.0',
+    packages=['libs'],
+    url='',
+    license='',
+    author='yeqing',
+    author_email='215777@qq.com',
+    description='文档类数据字符串提取',
+    python_requires=">=3.9",
+)
