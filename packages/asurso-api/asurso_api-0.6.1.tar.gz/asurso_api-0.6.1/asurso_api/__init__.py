8251d3e016cf07f7530f6ebabc83b335d8dad5c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 06:07:34 +0000

    Add package asurso-api/0.6.1/asurso_api-0.6.1.tar.gz

---
+++ packages/asurso-api/asurso_api-0.6.1.tar.gz/asurso_api-0.6.1/asurso_api/__init__.py
@@ -0,0 +1,6 @@
+from .classes import ASURSO, AsyncASURSO
+from . import classes
+from . import enums
+from . import functions
+from . import exceptions
+from . import utils
---
+++ packages/asurso-api/asurso_api-0.6.1.tar.gz/asurso_api-0.6.1/asurso_api/enums/__init__.py
@@ -0,0 +1,3 @@
+from .lessons_period import LessonsPeriod
+from .mark_value import MarkValue
+from .raw import *
