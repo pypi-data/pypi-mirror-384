338be4b1adb7f8f2347fb98d0c5928671d79f0a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 04:06:46 +0000

    Add package pyside-cli/0.2.7/pyside_cli-0.2.7.tar.gz

---
+++ packages/pyside-cli/pyside_cli-0.2.7.tar.gz/pyside_cli-0.2.7/cli/builder/pyinstaller.py
@@ -0,0 +1,14 @@
+def build_pyinstaller_cmd(pyinstaller, args, options):
+    workpath = 'build/' + ('pyinstaller_onefile_build' if args.onefile else 'pyinstaller_onedir_build')
+    cmd = [
+        pyinstaller,
+        '--onefile' if args.onefile else '--onedir',
+        '--distpath', 'build',
+        '--workpath', workpath,
+        '--noconfirm',
+        '--log-level', 'DEBUG' if args.debug else 'WARN',
+        '--name', 'App', 'app/__main__.py',
+    ]
+    cmd.extend(options)
+    cmd.extend(args.backend_args)
+    return cmd
---
+++ packages/pyside-cli/pyside_cli-0.2.7.tar.gz/pyside_cli-0.2.7/cli/pyproject.py
@@ -0,0 +1,47 @@
+import sys
+from typing import Tuple, List
+
+import glom
+import toml
+
+class PyProjectConfig:
+    extra_nuitka_options_list = []
+    extra_pyinstaller_options_list = []
+    lang_list = []
+
+    def __init__(self):
+        """get and build vars from pyproject.toml
+         1. nuitka command options list
+         2. pyinstaller command options list
+         3. enabled languages list"""
+        with open("pyproject.toml") as f:
+            data = toml.load(f)
+        nuitka_config = glom.glom(data, "tool.pyside-cli", default={})
+        nuitka_platform_config = glom.glom(data, f"tool.pyside-cli.{sys.platform}", default={})
+        nuitka_config.update(nuitka_platform_config)
+
+        for k, v in nuitka_config.items():
+            if isinstance(v, list) and v:
+                cmd = f"--{k}={','.join(v)}"
+                self.extra_nuitka_options_list.append(cmd)
+            elif isinstance(v, str) and v != "":
+                cmd = f"--{k}={v}"
+                self.extra_nuitka_options_list.append(cmd)
+            elif type(v) is bool and v:
+                cmd = f"--{k}"
+                self.extra_nuitka_options_list.append(cmd)
+
+        pyinstaller_config = glom.glom(data, "tool.pyside-cli.pyinstaller", default={})
+        for k, v in pyinstaller_config.items():
+            if isinstance(v, list) and v:
+                cmd = f"--{k}={','.join(v)}"
+                self.extra_pyinstaller_options_list.append(cmd)
+            elif isinstance(v, str) and v != "":
+                cmd = f"--{k}={v}"
+                self.extra_pyinstaller_options_list.append(cmd)
+            elif type(v) is bool and v:
+                cmd = f"--{k}"
+                self.extra_pyinstaller_options_list.append(cmd)
+
+        self.lang_list = glom.glom(data, "tool.pyside-cli.i18n.languages", default=[])
+
