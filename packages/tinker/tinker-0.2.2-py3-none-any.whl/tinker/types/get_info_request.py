2b596f9097f0de4dd4936c755bce8c253d6987e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 05:06:06 +0000

    Add package tinker/0.2.2/tinker-0.2.2-py3-none-any.whl

---
+++ packages/tinker/tinker-0.2.2-py3-none-any.whl/tinker/types/get_info_request.py
@@ -0,0 +1,14 @@
+from typing import Optional
+from typing_extensions import Literal
+
+from .._compat import PYDANTIC_V2, ConfigDict
+from .._models import StrictBase
+from .model_id import ModelID
+
+__all__ = ["GetInfoRequest"]
+
+
+class GetInfoRequest(StrictBase):
+    model_id: ModelID
+
+    type: Optional[Literal["get_info"]] = None
---
+++ packages/tinker/tinker-0.2.2-py3-none-any.whl/tinker/types/model_input_chunk.py
@@ -0,0 +1,12 @@
+from typing import Union
+from typing_extensions import Annotated, TypeAlias
+
+from .._utils import PropertyInfo
+from .encoded_text_chunk import EncodedTextChunk
+from .image_asset_pointer_chunk import ImageAssetPointerChunk
+
+__all__ = ["ModelInputChunk"]
+
+ModelInputChunk: TypeAlias = Annotated[
+    Union[EncodedTextChunk, ImageAssetPointerChunk], PropertyInfo(discriminator="type")
+]
