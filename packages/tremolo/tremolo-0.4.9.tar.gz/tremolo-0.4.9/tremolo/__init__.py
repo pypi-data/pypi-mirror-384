e334aa62fef2a6c473ef2771345cb4510734841f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 03:15:56 +0000

    Add package tremolo/0.4.9/tremolo-0.4.9.tar.gz

---
+++ packages/tremolo/tremolo-0.4.9.tar.gz/tremolo-0.4.9/tremolo/__init__.py
@@ -0,0 +1,14 @@
+__version__ = '0.4.9'
+
+from .tremolo import Tremolo, Tremolo as Application  # noqa: E402,F401
+from . import exceptions  # noqa: E402,F401
+
+
+def run(app, **options):
+    if 'host' not in options:
+        options['host'] = '127.0.0.1'
+
+    if 'port' not in options:
+        options['port'] = 8000
+
+    Tremolo().run(app=app, **options)
