e1332e67d08d8446f993e0993aca8a11c656b1a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 06:06:11 +0000

    Add package blacline/0.1.1/blacline-0.1.1-py3-none-any.whl

---
+++ packages/blacline/blacline-0.1.1-py3-none-any.whl/blacline/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for blacline."""
+
+__author__ = """Muniappan Puabi"""
+__email__ = '95000am@gmail.com'
+
+from .schedule.basic_schedule import basic_schedule
+from .schedule.allocation import allocate_buses
