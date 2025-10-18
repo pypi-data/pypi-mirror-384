a8760556cf09eb69810f84bacdfc181dcff2eddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 04:06:25 +0000

    Add package pdmv-http-client/2.1.0/pdmv_http_client-2.1.0-py3-none-any.whl

---
+++ packages/pdmv-http-client/pdmv_http_client-2.1.0-py3-none-any.whl/rest/__init__.py
@@ -0,0 +1,10 @@
+"""
+Exposes the REST client for the McM application
+like the old structure used to do.
+Just to keep backward compatibility.
+"""
+
+from rest.applications.base import BaseClient
+from rest.applications.mcm.core import McM
+from rest.applications.rereco.core import ReReco
+from rest.applications.stats.core import Stats2
