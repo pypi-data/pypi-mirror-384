a2a81b5b2ad25393a7b5fb666a92dc60166e98e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 04:07:01 +0000

    Add package galileo/1.27.1/galileo-1.27.1-py3-none-any.whl

---
+++ packages/galileo/galileo-1.27.1-py3-none-any.whl/galileo/resources/models/create_scorer_request.py
@@ -0,0 +1,204 @@
+from collections.abc import Mapping
+from typing import TYPE_CHECKING, Any, TypeVar, Union, cast
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..models.model_type import ModelType
+from ..models.scorer_types import ScorerTypes
+from ..types import UNSET, Unset
+
+if TYPE_CHECKING:
+    from ..models.scorer_defaults import ScorerDefaults
+
+
+T = TypeVar("T", bound="CreateScorerRequest")
+
+
+@_attrs_define
+class CreateScorerRequest:
+    """
+    Attributes
+    ----------
+        name (str):
+        scorer_type (ScorerTypes):
+        default_version_id (Union[None, Unset, str]):
+        defaults (Union['ScorerDefaults', None, Unset]):
+        description (Union[Unset, str]):  Default: ''.
+        ground_truth (Union[None, Unset, bool]):
+        model_type (Union[ModelType, None, Unset]):
+        tags (Union[Unset, list[str]]):
+        user_prompt (Union[None, Unset, str]):
+    """
+
+    name: str
+    scorer_type: ScorerTypes
+    default_version_id: Union[None, Unset, str] = UNSET
+    defaults: Union["ScorerDefaults", None, Unset] = UNSET
+    description: Union[Unset, str] = ""
+    ground_truth: Union[None, Unset, bool] = UNSET
+    model_type: Union[ModelType, None, Unset] = UNSET
+    tags: Union[Unset, list[str]] = UNSET
+    user_prompt: Union[None, Unset, str] = UNSET
+    additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> dict[str, Any]:
+        from ..models.scorer_defaults import ScorerDefaults
+
+        name = self.name
+
+        scorer_type = self.scorer_type.value
+
+        default_version_id: Union[None, Unset, str]
+        default_version_id = UNSET if isinstance(self.default_version_id, Unset) else self.default_version_id
+
+        defaults: Union[None, Unset, dict[str, Any]]
+        if isinstance(self.defaults, Unset):
+            defaults = UNSET
+        elif isinstance(self.defaults, ScorerDefaults):
+            defaults = self.defaults.to_dict()
+        else:
+            defaults = self.defaults
+
+        description = self.description
+
+        ground_truth: Union[None, Unset, bool]
+        ground_truth = UNSET if isinstance(self.ground_truth, Unset) else self.ground_truth
+
+        model_type: Union[None, Unset, str]
+        if isinstance(self.model_type, Unset):
+            model_type = UNSET
+        elif isinstance(self.model_type, ModelType):
+            model_type = self.model_type.value
+        else:
+            model_type = self.model_type
+
+        tags: Union[Unset, list[str]] = UNSET
+        if not isinstance(self.tags, Unset):
+            tags = self.tags
+
+        user_prompt: Union[None, Unset, str]
+        user_prompt = UNSET if isinstance(self.user_prompt, Unset) else self.user_prompt
+
+        field_dict: dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update({"name": name, "scorer_type": scorer_type})
+        if default_version_id is not UNSET:
+            field_dict["default_version_id"] = default_version_id
+        if defaults is not UNSET:
+            field_dict["defaults"] = defaults
+        if description is not UNSET:
+            field_dict["description"] = description
+        if ground_truth is not UNSET:
+            field_dict["ground_truth"] = ground_truth
+        if model_type is not UNSET:
+            field_dict["model_type"] = model_type
+        if tags is not UNSET:
+            field_dict["tags"] = tags
+        if user_prompt is not UNSET:
+            field_dict["user_prompt"] = user_prompt
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: type[T], src_dict: Mapping[str, Any]) -> T:
+        from ..models.scorer_defaults import ScorerDefaults
+
+        d = dict(src_dict)
+        name = d.pop("name")
+
+        scorer_type = ScorerTypes(d.pop("scorer_type"))
+
+        def _parse_default_version_id(data: object) -> Union[None, Unset, str]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, str], data)
+
+        default_version_id = _parse_default_version_id(d.pop("default_version_id", UNSET))
+
+        def _parse_defaults(data: object) -> Union["ScorerDefaults", None, Unset]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            try:
+                if not isinstance(data, dict):
+                    raise TypeError()
+                return ScorerDefaults.from_dict(data)
+
+            except:  # noqa: E722
+                pass
+            return cast(Union["ScorerDefaults", None, Unset], data)
+
+        defaults = _parse_defaults(d.pop("defaults", UNSET))
+
+        description = d.pop("description", UNSET)
+
+        def _parse_ground_truth(data: object) -> Union[None, Unset, bool]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, bool], data)
+
+        ground_truth = _parse_ground_truth(d.pop("ground_truth", UNSET))
+
+        def _parse_model_type(data: object) -> Union[ModelType, None, Unset]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            try:
+                if not isinstance(data, str):
+                    raise TypeError()
+                return ModelType(data)
+
+            except:  # noqa: E722
+                pass
+            return cast(Union[ModelType, None, Unset], data)
+
+        model_type = _parse_model_type(d.pop("model_type", UNSET))
+
+        tags = cast(list[str], d.pop("tags", UNSET))
+
+        def _parse_user_prompt(data: object) -> Union[None, Unset, str]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, str], data)
+
+        user_prompt = _parse_user_prompt(d.pop("user_prompt", UNSET))
+
+        create_scorer_request = cls(
+            name=name,
+            scorer_type=scorer_type,
+            default_version_id=default_version_id,
+            defaults=defaults,
+            description=description,
+            ground_truth=ground_truth,
+            model_type=model_type,
+            tags=tags,
+            user_prompt=user_prompt,
+        )
+
+        create_scorer_request.additional_properties = d
+        return create_scorer_request
+
+    @property
+    def additional_keys(self) -> list[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
