6d28e107c331ea3ef4a3432837e8553ae42c35e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 03:16:07 +0000

    Add package tdecomp/0.2.18/tdecomp-0.2.18.tar.gz

---
+++ packages/tdecomp/tdecomp-0.2.18.tar.gz/tdecomp-0.2.18/tdecomp/__init__.py
@@ -0,0 +1,5 @@
+from . import grad_proj
+from . import tensor
+from . import matrix
+from . import utils
+from . import _base
---
+++ packages/tdecomp/tdecomp-0.2.18.tar.gz/tdecomp-0.2.18/tdecomp/matrix/__init__.py
@@ -0,0 +1,9 @@
+from . import decomposer
+from . import functional
+from . import random_projections
+
+__all__ = [
+    'decomposer',
+    'functional',
+    'random_projections'
+]
