90f932e5f69affd2091deccc3c1754a18642ae85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 03:16:04 +0000

    Add package checkatlas/0.6.1/checkatlas-0.6.1.tar.gz

---
+++ packages/checkatlas/checkatlas-0.6.1.tar.gz/checkatlas-0.6.1/checkatlas/metrics/cluster/davies_bouldin.py
@@ -0,0 +1,14 @@
+from sklearn.metrics import davies_bouldin_score
+
+
+def run(count_repr, annotations):
+    """
+
+
+
+    :param count_repr:
+    :param annotations:
+    :return:
+    """
+    # print(count_repr, annotations)
+    return davies_bouldin_score(count_repr, annotations)
