3b2255e6fcb0331b7812be5601fa0c495276c2ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 06:06:11 +0000

    Add package cdvl-crawler/0.4.1/cdvl_crawler-0.4.1-py3-none-any.whl

---
+++ packages/cdvl-crawler/cdvl_crawler-0.4.1-py3-none-any.whl/cdvl_crawler/__init__.py
@@ -0,0 +1,11 @@
+"""CDVL Crawler - Tools for crawling and downloading videos from cdvl.org"""
+
+import importlib.metadata
+
+from cdvl_crawler.crawler import CDVLCrawler
+from cdvl_crawler.downloader import CDVLDownloader
+from cdvl_crawler.generator import CDVLSiteGenerator
+
+__version__ = importlib.metadata.version("cdvl_crawler")
+
+__all__ = ["CDVLCrawler", "CDVLDownloader", "CDVLSiteGenerator"]
