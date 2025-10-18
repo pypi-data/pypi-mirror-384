f48a1b336b440d69a1f67821a70024f4b4f4b9c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 06:06:19 +0000

    Add package abstract-gui/0.1.63.211/abstract_gui-0.1.63.211.tar.gz

---
+++ packages/abstract-gui/abstract_gui-0.1.63.211.tar.gz/abstract_gui-0.1.63.211/src/abstract_gui/__init__.py
@@ -0,0 +1,10 @@
+from .abstract_gui import *
+from .abstract_browser import AbstractBrowser
+from .abstract_window_manager import AbstractWindowManager
+from .abstract_next_read_manager import NextReadManager
+from .abstract_window_global_bridge import WindowGlobalBridge
+from .RightClickManager import RightClickManager
+
+from .QT6 import *
+from .SIMPLEGUI import *
+from .SIMPLEGUI.initFuncGen import get_for_all_tabs
